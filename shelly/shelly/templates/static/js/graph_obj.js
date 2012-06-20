2b0dd1d0eb15119bd9714b3f78accd219533a46d
Alex Johnson <johnson.alex.c@gmail.com>
2012-06-20 20:06:03 +0000

    Graph details saved in graph div; drag data directly into a graph

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -1,217 +1,215 @@
-// ps is a single plotting session
-ps={}
-ps.randdata=[];
-for(var i=0;i<1000;i++) {ps.randdata.push(Math.random()*5-2);}
-ps.data=[
-{xy:[[-5,0],[-4,0],[-3,1],[-2,4],[-1,-2],[0,0],[1,5],[3,3],[3.5,1],[4,2],[6,3],[10,5]],
-	name:'Fred',color:'#00f'},
-{y:[4,2,6,1,0,-3,-1,0,1,1,0,0.1,0.2,0.1,0,0],x0:-6,dx:1.1,
-	name:'Ethel',color:'#f00'},
-{y:ps.randdata,x0:-100,dx:0.2,name:'Bertha',color:'#0f0'}];
+function scatter_plot(divid, data, layout) {
+	var gd=document.getElementById(divid);
 
-ps.layout={title:'Glass Washer',
-	xaxis:{range:[-10,12],tick0:0,dtick:2,ticklen:5,
-		title:'Time',unit:'hours'},
-	yaxis:{range:[-3.5,5.5],tick0:0,dtick:1,ticklen:5,
-		title:'pH',unit:''},
-	width:576,
-	height:432,
-	//width:640,
-	//height:480,
-	margin:{l:50,r:10,t:30,b:40,pad:2} };
-// TODO: add font size controls, and label positioning
-// TODO: add marker and linewidth controls
-// TODO: add legend
+	// data should be a list of dicts, either:
+	// {xy:[[x0,y0],[x1,y1],..], name:'<name>', color:'<color>'}
+	// or:
+	// {y:[y0,y1,..], x0:<#>, dx:<#>, name:'<name>', color:'<color>'}
+	gd.data=data;
 
-ps.paper=Raphael('graph',ps.layout.width,ps.layout.height);
-ps.x;
+	// make the graph container and axes, if they don't already exist
+	if(gd.layout===undefined) {new_plot(divid, layout);}
 
-ps.plotwidth=ps.layout.width-ps.layout.margin.l-ps.layout.margin.r;
-ps.plotheight=ps.layout.height-ps.layout.margin.t-ps.layout.margin.b;
-ax=ps.paper.rect(ps.layout.margin.l-ps.layout.margin.pad,
-	ps.layout.margin.t-ps.layout.margin.pad,
-	ps.plotwidth+2*ps.layout.margin.pad,
-	ps.plotheight+2*ps.layout.margin.pad);
-ps.paperDOM=document.getElementById('graph').firstChild;
-ps.paperDOM.style['background-color']='#fff';
-ps.plot=Raphael(ps.paperDOM,ps.plotwidth,ps.plotheight);
-ps.plotDOM=ps.paperDOM.childNodes[0];
-ps.plotDOM.style.position='';
-ps.plotDOM.setAttribute('x',ps.layout.margin.l);
-ps.plotDOM.setAttribute('y',ps.layout.margin.t);
-ps.plot.viewbox={x:0,y:0,w:ps.plotwidth,h:ps.plotheight,dx:0,dy:0};
-ps.plotbg=ps.plot.rect(-screen.width,-screen.height,
-	ps.plotwidth+2*screen.width,ps.plotheight+2*screen.height);
-ps.plotbg.attr({'fill':'#fff','stroke-width':0});
-ps.plotbg.drag(ps.plotdrag,ps.plotdragstart,ps.plotdragend);
+	var gl=gd.layout;
+	var vb=gd.plot.viewbox;
+	var xa=gl.xaxis;
+	var ya=gl.yaxis;
+	var x,xy,y,mx,bx;
 
-ps.plotdragstart=function(x,y) {
-	ps.plotbg.node.style.cursor='move';
-	ps.layout.xaxis.r0=[ps.layout.xaxis.range[0],ps.layout.xaxis.range[1]];
-	ps.layout.yaxis.r0=[ps.layout.yaxis.range[0],ps.layout.yaxis.range[1]];
+	for(curve in gd.data) { 
+	if('xy' in gd.data[curve]) {
+		xy=gd.data[curve].xy;
+		gd.plot.setStart();
+		for(x in xy) {if(x>0){gd.plot.path(Raphael.format('M{0},{1}L{2},{3}',
+			xa.b+xa.m*xy[x-1][0]+vb.x,ya.b+ya.m*xy[x-1][1]+vb.y,
+			xa.b+xa.m*xy[x][0]+vb.x,ya.b+ya.m*xy[x][1]+vb.y))}}
+		gd.data[curve].lines=gd.plot.setFinish();
+		gd.plot.setStart();
+		for(x in xy) {gd.plot.circle(xa.b+xa.m*xy[x][0]+vb.x,ya.b+ya.m*xy[x][1]+vb.y,3)}
+		gd.data[curve].points=gd.plot.setFinish();
+	}
+	else {
+		y=gd.data[curve].y;
+		mx=xa.m*gd.data[curve].dx;
+		bx=xa.m*gd.data[curve].x0+xa.b;
+		gd.plot.setStart();
+		for(x in y) {if(x>0){gd.plot.path(Raphael.format('M{0},{1}L{2},{3}',
+			bx+mx*(x-1),ya.b+ya.m*y[x-1],
+			bx+mx*x,ya.b+ya.m*y[x]))}}
+		gd.data[curve].lines=gd.plot.setFinish();
+		gd.plot.setStart();
+		for(x in y) {gd.plot.circle(bx+mx*x,ya.b+ya.m*y[x],3)}
+		gd.data[curve].points=gd.plot.setFinish();
+	}
+	gd.data[curve].lines.attr({'stroke':gd.data[curve].color});
+	gd.data[curve].points.attr({'fill':gd.data[curve].color,'stroke-width':0});
+	}
+	return('*ta-da*')
 }
 
-ps.plotdrag=function(dx,dy) {
-	var vb=ps.plot.viewbox;
+function new_plot(divid, layout) {
+	// Get the container div: we will store all variables as properties of this div
+	// (for extension to multiple graphs per page)
+	var gd=document.getElementById(divid);
+
+	// Get the layout info (hard code this for now)
+	gd.layout={title:'Glass Washer',
+		xaxis:{range:[-10,12],tick0:0,dtick:2,ticklen:5,
+			title:'Time',unit:'hours'},
+		yaxis:{range:[-3.5,5.5],tick0:0,dtick:1,ticklen:5,
+			title:'pH',unit:''},
+		width:400,
+		height:300,
+		margin:{l:50,r:10,t:30,b:40,pad:2},
+		paper_bgcolor:'#fff',
+		plot_bgcolor:'#fff' };
+		// TODO: add font size controls, and label positioning
+		// TODO: add marker and linewidth controls
+		// TODO: add legend
+		// TODO: use this hard coded one for defaults, take any new properties from input param
+
+	// Make the graph containers
+	// We use a hack to get two nested Raphael objects (Raphael 2.0.1)
+	// First one (paper) is for the axes
+	var gl=gd.layout;
+	gd.paper=Raphael(divid,gl.width,gl.height);
+	gd.plotwidth=gl.width-gl.margin.l-gl.margin.r;
+	gd.plotheight=gl.height-gl.margin.t-gl.margin.b;
+	gd.ax=gd.paper.rect(gl.margin.l-gl.margin.pad,
+		gl.margin.t-gl.margin.pad,
+		gd.plotwidth+2*gl.margin.pad,
+		gd.plotheight+2*gl.margin.pad);
+	gd.paperDOM=gd.firstChild;
+	gd.paperDOM.style['background-color']=gl.paper_bgcolor;
+
+	// Second one (plot) is for the data
+	gd.plot=Raphael(gd.paperDOM,gd.plotwidth,gd.plotheight);
+	gd.plotDOM=gd.paperDOM.childNodes[0];
+	gd.plotDOM.style.position='';
+	gd.plotDOM.setAttribute('x',gl.margin.l);
+	gd.plotDOM.setAttribute('y',gl.margin.t);
+	gd.plot.viewbox={x:0,y:0,w:gd.plotwidth,h:gd.plotheight,dx:0,dy:0};
+	gd.plotbg=gd.plot.rect(-screen.width,-screen.height,
+		gd.plotwidth+2*screen.width,gd.plotheight+2*screen.height);
+	gd.plotbg.attr({'fill':gl.plot_bgcolor,'stroke-width':0});
+
+	// plotted area dragging - the 'gd's at the end become 'this' in the fcns
+	gd.plotbg.drag(plotdrag,plotdragstart,plotdragend,gd,gd,gd);
+
+	// make the ticks, grids, and titles
+	doXTicks(gd);doYTicks(gd);doXGrid(gd);doYGrid(gd);
+	gl.xaxis.r0=gl.xaxis.range[0];
+	gl.yaxis.r0=gl.yaxis.range[0];
+
+	gd.xtitle=gd.paper.text((gl.width+gl.margin.l-gl.margin.r)/2,gl.height-10,
+		axtitle(gl.xaxis)).attr({'font-size':14});
+	gd.ytitle=gd.paper.text(20,(gl.height+gl.margin.t-gl.margin.b)/2,
+		axtitle(gl.yaxis)).rotate(-90).attr({'font-size':14});
+	gd.title=gd.paper.text(gl.width/2,gl.margin.t/2,gl.title).attr({'font-size':16});
+}
+
+function plotdragstart(x,y) {
+	var gl=this.layout;
+	this.plotbg.node.style.cursor='move';
+	gl.xaxis.r0=[gl.xaxis.range[0],gl.xaxis.range[1]];
+	gl.yaxis.r0=[gl.yaxis.range[0],gl.yaxis.range[1]];
+}
+
+function plotdrag(dx,dy) {
+	var gl=this.layout;
+	var vb=this.plot.viewbox;
 	vb.dx=dx;
 	vb.dy=dy;
-	ps.plot.setViewbox(vb.x-vb.dx,vb.y-vb.dy,vb.w,vb.h,false);
-	ps.layout.xaxis.range[0]=ps.layout.xaxis.r0[0]-dx/ps.layout.xaxis.m;
-	ps.layout.xaxis.range[1]=ps.layout.xaxis.r0[1]-dx/ps.layout.xaxis.m;
-	ps.layout.yaxis.range[0]=ps.layout.yaxis.r0[0]-dy/ps.layout.yaxis.m;
-	ps.layout.yaxis.range[1]=ps.layout.yaxis.r0[1]-dy/ps.layout.yaxis.m;
-	ps.doXTicks();ps.doYTicks();
+	this.plot.setViewBox(vb.x-vb.dx,vb.y-vb.dy,vb.w,vb.h,false);
+	gl.xaxis.range[0]=gl.xaxis.r0[0]-dx/gl.xaxis.m;
+	gl.xaxis.range[1]=gl.xaxis.r0[1]-dx/gl.xaxis.m;
+	gl.yaxis.range[0]=gl.yaxis.r0[0]-dy/gl.yaxis.m;
+	gl.yaxis.range[1]=gl.yaxis.r0[1]-dy/gl.yaxis.m;
+	doXTicks(this);doYTicks(this);
 }
 
-ps.plotdragend=function() {
-	var vb=ps.plot.viewbox;
+function plotdragend() {
+	var vb=this.plot.viewbox;
 	vb.x-=vb.dx;
 	vb.y-=vb.dy;
 	vb.dx=0;
 	vb.dy=0;
-	ps.plotbg.node.style.cursor='';
-	ps.doXGrid();ps.doYGrid();
-};
-
-ps.doXTicks=function() {
-	if(typeof xticks != 'undefined') {xticks.remove();xlabels.remove();}
-	a=ps.layout.xaxis;
-	ps.paper.setStart();
-	a.m=ps.plotwidth/(a.range[1]-a.range[0]);
-	a.b=-a.m*a.range[0];
-	a.tmin=Math.ceil((a.range[0]-a.tick0)/a.dtick)*a.dtick+a.tick0;
-	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
-		ps.paper.path(Raphael.format('M{0},{1}v{2}',
-			ps.layout.margin.l+a.m*x+a.b,
-			ps.layout.height-ps.layout.margin.b+ps.layout.margin.pad,
-			a.ticklen))}
-	xticks=ps.paper.setFinish();
-	ps.paper.setStart();
-	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
-		ps.paper.text(ps.layout.margin.l+a.m*x+a.b,
-			ps.layout.height-ps.layout.margin.b+ps.layout.margin.pad+a.ticklen,
-			Raphael.format('\n{0}',x))}
-	xlabels=ps.paper.setFinish();
-	xlabels.attr({'font-size':12});
-};
-
-ps.doXGrid=function() {
-	if(typeof xgrid != 'undefined') {xgrid.remove();}
-	a=ps.layout.xaxis;
-	ps.plot.setStart();
-	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
-		ps.plot.path(Raphael.format('M{0},{1}v{2}',a.m*x+a.b+ps.plot.viewbox.x,
-			ps.plot.viewbox.y-screen.height,ps.plotheight+2*screen.height))}
-	xgrid=ps.plot.setFinish();
-	xgrid.attr({'stroke':'#ccc'}).toBack().drag(ps.plotdrag,ps.plotdragstart,ps.plotdragend);
-	ps.plotbg.toBack();
-};
-
-ps.doYTicks=function() {
-	if(typeof yticks != 'undefined') {yticks.remove();ylabels.remove();}
-	a=ps.layout.yaxis;
-	ps.paper.setStart();
-	a.m=ps.plotheight/(a.range[0]-a.range[1]);
-	a.b=-a.m*a.range[1];
-	a.tmin=Math.ceil((a.range[0]-a.tick0)/a.dtick)*a.dtick+a.tick0;
-	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
-		ps.paper.path(Raphael.format('M{0},{1}h{2}',
-			ps.layout.margin.l-ps.layout.margin.pad,
-			ps.layout.margin.t+a.m*x+a.b,
-			-a.ticklen))}
-	ps.yticks=ps.paper.setFinish();
-	ps.paper.setStart();
-	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
-		ps.paper.text(ps.layout.margin.l-ps.layout.margin.pad-a.ticklen,
-			ps.layout.margin.t+a.m*x+a.b,
-			Raphael.format('{0}',x))}
-	ylabels=ps.paper.setFinish();
-	ylabels.attr({'font-size':12,'text-anchor':'end'});
-};
-
-ps.doYGrid=function() { // assumes doYticks has been called recently
-	if(typeof ygrid != 'undefined') {ygrid.remove();}
-	a=ps.layout.yaxis;
-	ps.plot.setStart();
-	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
-		ps.plot.path(Raphael.format('M{0},{1}h{2}',ps.plot.viewbox.x-screen.width,
-			a.m*x+a.b+ps.plot.viewbox.y,ps.plotwidth+2*screen.width))}
-	ygrid=ps.plot.setFinish();
-	ygrid.attr({'stroke':'#ccc'}).toBack().drag(ps.plotdrag,ps.plotdragstart,ps.plotdragend);
-	ps.plotbg.toBack();
+	this.plotbg.node.style.cursor='';
+	doXGrid(this);doYGrid(this);
 }
 
-ps.doXTicks();ps.doYTicks();ps.doXGrid();ps.doYGrid();
-ps.layout.xaxis.r0=ps.layout.xaxis.range[0];
-ps.layout.yaxis.r0=ps.layout.yaxis.range[0];
+function doXTicks(gd) {
+	if(typeof gd.xticks != 'undefined') {gd.xticks.remove();gd.xlabels.remove();}
+	var gl=gd.layout;
+	var a=gl.xaxis;
+	gd.paper.setStart();
+	a.m=gd.plotwidth/(a.range[1]-a.range[0]);
+	a.b=-a.m*a.range[0];
+	a.tmin=Math.ceil((a.range[0]-a.tick0)/a.dtick)*a.dtick+a.tick0;
+	for(var x=a.tmin;x<=a.range[1];x+=a.dtick){
+		gd.paper.path(Raphael.format('M{0},{1}v{2}',
+			gl.margin.l+a.m*x+a.b,
+			gl.height-gl.margin.b+gl.margin.pad,
+			a.ticklen))}
+	gd.xticks=gd.paper.setFinish();
+	gd.paper.setStart();
+	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
+		gd.paper.text(gl.margin.l+a.m*x+a.b,
+			gl.height-gl.margin.b+gl.margin.pad+a.ticklen,
+			Raphael.format('\n{0}',x))}
+	gd.xlabels=gd.paper.setFinish();
+	gd.xlabels.attr({'font-size':12});
+}
 
-ps.axtitle=function(axis) {
-	if(axis.unit=='') return axis.title;
+function doXGrid(gd) {
+	if(typeof gd.xgrid != 'undefined') {gd.xgrid.remove();}
+	var a=gd.layout.xaxis;
+	gd.plot.setStart();
+	for(var x=a.tmin;x<=a.range[1];x+=a.dtick){
+		gd.plot.path(Raphael.format('M{0},{1}v{2}',a.m*x+a.b+gd.plot.viewbox.x,
+			gd.plot.viewbox.y-screen.height,gd.plotheight+2*screen.height))}
+	gd.xgrid=gd.plot.setFinish();
+	gd.xgrid.attr({'stroke':'#ccc'}).toBack().drag(plotdrag,plotdragstart,plotdragend,gd,gd,gd);
+	gd.plotbg.toBack();
+}
+
+function doYTicks(gd) {
+	if(typeof gd.yticks != 'undefined') {gd.yticks.remove();gd.ylabels.remove();}
+	var gl=gd.layout;
+	var a=gl.yaxis;
+	gd.paper.setStart();
+	a.m=gd.plotheight/(a.range[0]-a.range[1]);
+	a.b=-a.m*a.range[1];
+	a.tmin=Math.ceil((a.range[0]-a.tick0)/a.dtick)*a.dtick+a.tick0;
+	for(var x=a.tmin;x<=a.range[1];x+=a.dtick){
+		gd.paper.path(Raphael.format('M{0},{1}h{2}',
+			gl.margin.l-gl.margin.pad,
+			gl.margin.t+a.m*x+a.b,
+			-a.ticklen))}
+	gd.yticks=gd.paper.setFinish();
+	gd.paper.setStart();
+	for(x=a.tmin;x<=a.range[1];x+=a.dtick){
+		gd.paper.text(gl.margin.l-gl.margin.pad-a.ticklen,
+			gl.margin.t+a.m*x+a.b,
+			Raphael.format('{0}',x))}
+	gd.ylabels=gd.paper.setFinish();
+	gd.ylabels.attr({'font-size':12,'text-anchor':'end'});
+}
+
+function doYGrid(gd) { // assumes doYticks has been called recently
+	if(typeof gd.ygrid != 'undefined') {gd.ygrid.remove();}
+	var a=gd.layout.yaxis;
+	gd.plot.setStart();
+	for(var x=a.tmin;x<=a.range[1];x+=a.dtick){
+		gd.plot.path(Raphael.format('M{0},{1}h{2}',gd.plot.viewbox.x-screen.width,
+			a.m*x+a.b+gd.plot.viewbox.y,gd.plotwidth+2*screen.width))}
+	gd.ygrid=gd.plot.setFinish();
+	gd.ygrid.attr({'stroke':'#ccc'}).toBack().drag(plotdrag,plotdragstart,plotdragend,gd,gd,gd);
+	gd.plotbg.toBack();
+}
+
+function axtitle(axis) {
+	if((axis.unit=='')||(axis.unit==undefined)) return axis.title;
 	else return axis.title+' ('+axis.unit+')';
-};
+}
 
-ps.lnplt=function(res) {
-    //var ps.paper=Raphael('graph',ps.layout.width,ps.layout.height);
-    xtitle=ps.paper.text((ps.layout.width+ps.layout.margin.l-ps.layout.margin.r)/2,ps.layout.height-10,
-	ps.axtitle(ps.layout.xaxis)).attr({'font-size':14});
-    ytitle=ps.paper.text(20,(ps.layout.height+ps.layout.margin.t-ps.layout.margin.b)/2,
-	ps.axtitle(ps.layout.yaxis)).rotate(-90).attr({'font-size':14});
-    title=ps.paper.text(ps.layout.width/2,ps.layout.margin.t/2,ps.layout.title).attr({'font-size':16});
-
-    //ps.data=[
-    //{xy:[[-5,0],[-4,0],[-3,1],[-2,4],[-1,-2],[0,0],[1,5],[3,3],[3.5,1],[4,2],[6,3],[10,5]],
-    // name:'Fred',color:'#00f'},
-    //{y:[4,2,6,1,0,-3,-1,0,1,1,0,0.1,0.2,0.1,0,0],x0:-6,dx:1.1,
-    // name:'Ethel',color:'#f00'},
-    //{y:ps.randdata,x0:-100,dx:0.2,name:'Bertha',color:'#0f0'}];
-
-    // x, y strings
-    x=res.x.replace(/^\s+|\s+$/g, '').replace('[','').replace(']','').split(',');
-    y=res.y.replace(/^\s+|\s+$/g, '').replace('[','').replace(']','').split(',');
-    ps.data=[]
-    dictdata={}
-    arraydata=[]
-    dictdata['name']='Fred'
-    dictdata['color']='#00f'
-    for(var i=0;i<x.length;i++){
-	var pt=[];
-	pt[0]=x[i];
-	pt[1]=y[i];
-	arraydata[i]=pt;
-    }
-    dictdata['xy']=arraydata;
-    ps.data[0]=dictdata;
- 
-    var xa=ps.layout.xaxis;
-    var ya=ps.layout.yaxis;
-    var xy,y,mx,bx;
-    for(curve in ps.data) { 
-	if('xy' in ps.data[curve]) {
-	    xy=ps.data[curve].xy;
-	    ps.plot.setStart();
-	    for(x in xy) {if(x>0){ps.plot.path(Raphael.format('M{0},{1}L{2},{3}',
-		xa.b+xa.m*xy[x-1][0],ya.b+ya.m*xy[x-1][1],
-		xa.b+xa.m*xy[x][0],ya.b+ya.m*xy[x][1]))}}
-	    ps.data[curve].lines=ps.plot.setFinish();
-	    ps.plot.setStart();
-	    for(x in xy) {ps.plot.circle(xa.b+xa.m*xy[x][0],ya.b+ya.m*xy[x][1],3)}
-	    ps.data[curve].points=ps.plot.setFinish();
-	}
-	else {
-	    y=ps.data[curve].y;
-	    mx=xa.m*ps.data[curve].dx;
-	    bx=xa.m*ps.data[curve].x0+xa.b;
-	    ps.plot.setStart();
-	    for(x in y) {if(x>0){ps.plot.path(Raphael.format('M{0},{1}L{2},{3}',
-		bx+mx*(x-1),ya.b+ya.m*y[x-1],
-		bx+mx*x,ya.b+ya.m*y[x]))}}
-	    ps.data[curve].lines=ps.plot.setFinish();
-	    ps.plot.setStart();
-	    for(x in y) {ps.plot.circle(bx+mx*x,ya.b+ya.m*y[x],3)}
-	    ps.data[curve].points=ps.plot.setFinish();
-	}
-	ps.data[curve].lines.attr({'stroke':ps.data[curve].color});
-	ps.data[curve].points.attr({'fill':ps.data[curve].color,'stroke-width':0});
-    }
-    return('*ta-da*')
-};
