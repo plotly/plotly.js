306ab563c1ffd1ec622fbdff786f670c97c6bcbe
jack <jparmer09@gmail.com>
2013-04-12 12:12:52 +0000

    file well sprucing

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -2675,12 +2675,29 @@ function killspin(){
             gettab().spinner.stop();
         }
     }
-    //$('#'+gettab().id+' .spinner').remove();
     $('.spinner').remove();
 }
 
-function startspin(){
-    var gd=gettab();
-    var spinner=new Spinner(opts).spin(gd);
-    gd.spinner=spinner;
+function startspin(parent){
+    if(parent===undefined){ var parent=gettab(); }
+    // big spinny
+    var opts = {
+        lines: 17, // The number of lines to draw
+        length: 30, // The length of each line _30
+        width: 6, // The line thickness
+        radius: 37, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        color: '#000', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+    var spinner=new Spinner(opts).spin(parent);
+    parent.spinner=spinner;
 }
