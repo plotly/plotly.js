5fa577efa57d72abb50c15ddb3024a9a8dfc7538
Alex Johnson <alex@plot.ly>
2013-10-30 06:16:48 +0000

    autosize for plotly.js, and bugfix with plot canvas and area plots

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -1128,6 +1128,7 @@ Plotly.relayout = function(gd,astr,val) {
 };
 
 function setGraphContainerHeight(gd) {
+    if(!gd.mainsite) { return; }
     $gd = $(gd);
     var graphContainerHeight = $gd.innerHeight() - $gd.find('.tool-menu').innerHeight(),
         $themebar = $gd.find('.themebar'),
@@ -1144,6 +1145,7 @@ function setGraphContainerHeight(gd) {
 }
 
 function setGraphContainerScroll(gd) {
+    if(!gd.mainsite) { return; }
     var $graphContainer = $(gd).find('.graph-container'),
         isGraphWiderThanContainer = gd.layout.width > parseInt($graphContainer.css('width'),10);
 
@@ -1159,11 +1161,18 @@ function setGraphContainerScroll(gd) {
 }
 
 function plotAutoSize(gd, aobj) {
-    if(!gd.mainsite) { delete aobj.autosize; return aobj; }
-    setGraphContainerHeight(gd);
-    var gdBB = gd.graphContainer.node().getBoundingClientRect();
-    var newheight = Math.round(gdBB.height*0.9);
-    var newwidth = Math.round(gdBB.width*0.9);
+    var newheight, newwidth;
+    if(gd.mainsite) {
+        setGraphContainerHeight(gd);
+        var gdBB = gd.graphContainer.node().getBoundingClientRect();
+        newheight = Math.round(gdBB.height*0.9);
+        newwidth = Math.round(gdBB.width*0.9);
+    }
+    else {
+        newheight = $(gd).height() || gd.layout.height || defaultLayout().height;
+        newwidth = $(gd).width() || gd.layout.width || defaultLayout().width;
+        // delete aobj.autosize;
+    }
 
     if(Math.abs(gd.layout.width - newwidth) > 1 || Math.abs(gd.layout.height - newheight) > 1) {
         gd.layout.height = newheight;
