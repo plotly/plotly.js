585cabca45c01366011a81fb257b4dd41ab520e3
Chris Viau <chrisv@plot.ly>
2013-11-01 20:35:19 +0000

    simplified example list generation; micropolar adapter

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -364,7 +364,7 @@ Plotly.plot = function(gd, data, layout) {
     gd.mainsite=Boolean($('#plotlyMainMarker').length);
 
     if(data[0] && data[0].type.substr(0, 5) === 'Polar'){
-        Plotly.plotPolar(gd, data, layout);
+        micropolar.adapter.plotly(gd, data, layout);
         return null;
     }
 
@@ -567,92 +567,6 @@ Plotly.plot = function(gd, data, layout) {
         }
     },1000);
     Plotly.Lib.markTime('done plot');
-};
-
-Plotly.plotPolar = function(gd, data, layout){
-
-    gd.data=data;
-    gd.layout=layout;
-
-    var container = d3.select('.svg-container').html('');
-
-    var micropolarData = data.map(function(d, i){
-        return d3.zip(d.x, d.y);
-    });
-
-    var micropolarGeometry = data.map(function(d, i){
-        return micropolar[d.type.substr(5)]().config({stroke: d.line.color});
-    });
-
-    // Mic patterns example
-    ////////////////////////////////////////
-
-    var colors = ['peru', 'darkviolet', 'deepskyblue', 'orangered', 'green'];
-
-    var linePlot1 = micropolar.LinePlot().config({stroke: colors[0]});
-    var linePlot2 = micropolar.LinePlot().config({stroke: colors[1]});
-    var linePlot3 = micropolar.LinePlot().config({stroke: colors[2]});
-    var linePlot4 = micropolar.LinePlot().config({stroke: colors[3]});
-    var linePlot5 = micropolar.LinePlot().config({stroke: colors[4]});
-
-    function generateData(equation){
-        var step = 6;
-        var data = d3.range(0, 360 + step, step).map(function(deg, index){
-            var theta = deg * Math.PI / 180;
-            var radius = equation(theta);
-            return [deg, radius];
-        });
-        return data;
-    }
-
-    var data1 = generateData(function(theta){ return Math.abs(Math.cos(theta)); });
-    var data2 = generateData(function(theta){ return Math.abs(0.5 + 0.5 * Math.cos(theta)); });
-    var data3 = generateData(function(theta){ return Math.abs(0.25 + 0.75 * Math.cos(theta)); });
-    var data4 = generateData(function(theta){ return Math.abs(0.7 + 0.3 * Math.cos(theta)); });
-    var data5 = generateData(function(theta){ return Math.abs(0.37 + 0.63 * Math.cos(theta)); });
-
-    var legend = micropolar.legend()
-        .config({
-            data: ['Figure 8', 'Cardioid', 'Hypercardioid', 'Subcardioid', 'Supercardioid'],
-            colors: colors
-        });
-
-    var config = {
-        geometry: [linePlot1, linePlot2, linePlot3, linePlot4, linePlot5],
-        data: [data1, data2, data3, data4, data5],
-        legend: legend,
-        title: 'Microphone Patterns',
-        height: 300,
-        width: 300,
-        angularTicksStep: 30,
-        angularTicksSuffix: 'º',
-        minorTicks: 1,
-        flip: true,
-        originTheta: -90,
-        radialAxisTheta: -90,
-        radialTicksSuffix: 'dB',
-        containerSelector: container
-    };
-
-    var polarAxis = micropolar.Axis().config(config);
-    polarAxis();
-
-    function titleLayout(){
-        var bg = svg.select('.background-circle');
-        this.call(d3.plugly.convertToTspans)
-            .call(d3.plugly.alignSVGWith(bg, 'center'))
-        return this;
-    }
-    var svg = container.select('svg');
-    var title = svg.select('text.title')
-        .call(titleLayout)
-        .call(d3.plugly.makeEditable)
-        .on('edit', function(context){
-            this.call(titleLayout);
-        });
-
-    gd.paper = container.select('svg');
-
 };
 
 // setStyles: translate styles from gd.data to gd.calcdata,
