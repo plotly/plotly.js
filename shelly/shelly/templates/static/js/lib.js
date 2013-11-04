a4ab34e13f03c559bf71ed8fd0568f91dff8f6f6
Ben Postlethwaite <post.ben.here@gmail.com>
2013-11-04 22:22:38 +0000

    just broke plotly, whooo\!

--- shelly/shelly/templates/static/js/lib.js
+++ shelly/shelly/templates/static/js/lib.js
@@ -328,4 +328,43 @@ lib.bBoxIntersect = function(a,b){
 // minor performance booster for d3...
 lib.identity = function(d){ return d; };
 
+
+// random string generator
+lib.randstr = function randstr(bits, base, existing) {
+    /*
+     * Include number of bits, the base of the string you want
+     * and an optional array of existing strings to avoid.
+     */
+    if (!base) base = 16;
+    if (bits === undefined) bits = 128;
+    if (bits <= 0) return '0';
+
+    var digits = Math.log(Math.pow(2, bits)) / Math.log(base);
+    for (var i = 2; digits === Infinity; i *= 2) {
+        digits = Math.log(Math.pow(2, bits / i)) / Math.log(base) * i;
+    }
+
+    var rem = digits - Math.floor(digits);
+
+    var res = '';
+
+    for (var i = 0; i < Math.floor(digits); i++) {
+        var x = Math.floor(Math.random() * base).toString(base);
+        res = x + res;
+    }
+
+    if (rem) {
+        var b = Math.pow(base, rem);
+        var x = Math.floor(Math.random() * b).toString(base);
+        res = x + res;
+    }
+
+    var parsed = parseInt(res, base);
+    if ( (existing && (existing.indexOf(res) > -1)) ||
+         (parsed !== Infinity && parsed >= Math.pow(2, bits)) ) {
+        return randstr(bits, base, existing)
+    }
+    else return res;
+};
+
 }()); // end Lib object definition
