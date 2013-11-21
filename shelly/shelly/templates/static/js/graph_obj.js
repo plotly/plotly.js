a432570580db999cac287b6dc6860bd925de4bca
Chris Viau <chrisv@plot.ly>
2013-11-21 16:37:08 +0000

    mathjax: partial integration

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -1517,7 +1517,6 @@ plots.titles = function(gd,title) {
                 if(!text) setPlaceholder();
                 else if(text != 'Click to enter '+name+' title') opacity = 1;
                 this.call(titleLayout);
-
                 var property = Plotly.Lib.nestedProperty(gl,{X:'xaxis.title', Y:'yaxis.title', P:'title'}[name.charAt(0)]);
                 var update = {};
                 update[property.astr] = text;
