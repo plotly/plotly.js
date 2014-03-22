31ac9313236f7faea1df3f060652e3fbdff82510
Alex Johnson <alex@plot.ly>
2014-03-22 01:28:01 +0000

    remove useless obj graphInfo from graph_obj

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -219,29 +219,29 @@ function defaultLayout(){
 // how to display each type of graph
 // AJ 3/4/13: I'm envisioning a lot of stuff that's hardcoded into plot,
 // setStyles etc will go here to make multiple graph types easier to manage
-var graphInfo = {
-    scatter:{
-        framework:makePlotFramework
-    },
-    bar:{
-        framework:makePlotFramework
-    },
-    heatmap:{
-        framework:makePlotFramework
-    },
-    histogramx:{
-        framework:makePlotFramework
-    },
-    histogramy:{
-        framework:makePlotFramework
-    },
-    histogram2d:{
-        framework:makePlotFramework
-    },
-    box:{
-        framework:makePlotFramework
-    }
-};
+// var graphInfo = {
+//     scatter:{
+//         framework:makePlotFramework
+//     },
+//     bar:{
+//         framework:makePlotFramework
+//     },
+//     heatmap:{
+//         framework:makePlotFramework
+//     },
+//     histogramx:{
+//         framework:makePlotFramework
+//     },
+//     histogramy:{
+//         framework:makePlotFramework
+//     },
+//     histogram2d:{
+//         framework:makePlotFramework
+//     },
+//     box:{
+//         framework:makePlotFramework
+//     }
+// };
 
 var BARTYPES = ['bar','histogramx','histogramy'];
 plots.isBar = function(type) { return BARTYPES.indexOf(type)!=-1; };
@@ -505,15 +505,15 @@ Plotly.plot = function(gd, data, layout) {
     //  but if there's no data there yet, it's just a placeholder...
     //  then it should destroy and remake the plot
     if (gd.data && gd.data.length > 0) {
-        var framework = graphInfo[gd.data[0].type || 'scatter'].framework,
-            subplots = plots.getSubplots(gd).join(''),
+        var subplots = plots.getSubplots(gd).join(''),
             oldSubplots = ((gd.layout && gd.layout._plots) ? Object.keys(gd.layout._plots) : []).join('');
-        if(!gd.framework || gd.framework!=framework || !gd.layout || graphwasempty || (oldSubplots!=subplots)) {
-            gd.framework = framework;
-            framework(gd,layout);
+        if(!gd.framework || gd.framework!=makePlotFramework || !gd.layout || graphwasempty || (oldSubplots!=subplots)) {
+            gd.framework = makePlotFramework;
+            makePlotFramework(gd,layout);
         }
     }
     else if((typeof gd.layout==='undefined')||graphwasempty) { makePlotFramework(gd, layout); }
+
 
     // enable or disable formatting buttons
     $(gd).find('.data-only').attr('disabled', !gd.data || gd.data.length===0);
