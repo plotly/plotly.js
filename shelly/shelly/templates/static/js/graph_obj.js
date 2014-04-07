ac7b9454365482c4b81ae73e50eede6172367f34
Alex Johnson <alex@plot.ly>
2014-04-07 17:00:50 +0000

    remove useless drag elements from export, occasionally they caused errors

--- shelly/shelly/templates/static/js/graph_obj.js
+++ shelly/shelly/templates/static/js/graph_obj.js
@@ -1698,7 +1698,7 @@ function makePlotFramework(divid, layout) {
     Plotly.Axes.doTicks(gd,'redraw');
 
     // make the axis drag objects and hover effects
-    Plotly.Fx.init(gd);
+    if(!gl._forexport) { Plotly.Fx.init(gd); }
 }
 
 // called by legend and colorbar routines to see if we need to expand the margins to show them
