0b794c506b6bb085ceba37f96ca15d3ccf18d161
ben postlethwaite <post.ben.here@gmail.com>
2014-10-17 17:22:46 -0400

    prep surface js

---
+++ gl3d/surface.js
@@ -0,0 +1,20 @@
+module.exports = Surface;
+
+
+function Surface (config) {
+
+    this.config = config;
+
+}
+
+var proto = Surface.prototype;
+
+proto.attributes = {
+    x: {type: 'data_array'},
+    y: {type: 'data_array'},
+    z: {type: 'data_array'},
+    scene: {
+        type: 'sceneid',
+        dflt: 'scene'
+    }
+};
