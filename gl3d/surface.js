d66bdca5c975353caa11f28df992d6ca13912045
ben postlethwaite <post.ben.here@gmail.com>
2014-10-19 14:48:59 -0400

    surface.js applyDefaults

--- gl3d/surface.js
+++ gl3d/surface.js
@@ -1,11 +1,12 @@
-module.exports = Surface;
-
+'use strict';
 
 function Surface (config) {
 
     this.config = config;
 
 }
+
+module.exports = Surface;
 
 var proto = Surface.prototype;
 
@@ -17,5 +18,30 @@ proto.attributes = {
         type: 'sceneid',
         dflt: 'scene'
     },
-    color: {}
+    colorscale: {
+        type: 'colorscale'
+    }
+};
+
+
+
+proto.supplyDefaults = function (traceIn, traceOut, defaultColor, layout) {
+    var _this = this;
+    var Plotly = this.config.Plotly;
+
+    function coerce(attr, dflt) {
+        return Plotly.Lib.coerce(traceIn, traceOut, _this.attributes, attr, dflt);
+    }
+
+
+    var z = coerce('z');
+    if(!z) {
+        traceOut.visible = false;
+        return;
+    }
+    coerce('x');
+    coerce('y');
+
+    coerce('colorscale');
+    coerce('scene');
 };
