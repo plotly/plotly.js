cf4482729782d599be687d0b1cc86b7d786893a4
etpinard <etienne.t.pinard@gmail.com>
2014-11-28 18:04:37 -0500

    clean up variable names in modebar.js

--- shelly/shelly/static/js/modebar.js
+++ shelly/shelly/static/js/modebar.js
@@ -148,7 +148,7 @@ ModeBar.prototype.getLogo = function(){
     group.appendChild(a);
     group.classList.add('btn-group--logo');
     return group;
-}
+};
 
 /**
  * Apply D3 cartesian mode attributes to layout to update hover functionality
@@ -158,15 +158,15 @@ function handleCartesian (ev) {
     var button = ev.currentTarget,
         astr = button.getAttribute('data-attr'),
         val = button.getAttribute('data-val') || true,
-        aobj = {},
         graphInfo = this.graphInfo,
-        layout = this.graphInfo._fullLayout,
+        fullLayout = this.graphInfo._fullLayout,
         Plotly = this.Plotly,
-        _this = this;
+        _this = this,
+        aobj = {};
 
     if(astr === 'zoom') {
-        var xr = layout.xaxis.range,
-            yr = layout.yaxis.range,
+        var xr = fullLayout.xaxis.range,
+            yr = fullLayout.yaxis.range,
             mag = (val==='in') ? 0.5 : 2,
             r0 = (1+mag)/2, r1 = (1-mag)/2;
         aobj = {
@@ -179,7 +179,7 @@ function handleCartesian (ev) {
 
     // if ALL traces have orientation 'h', 'hovermode': 'x' otherwise: 'y'
     if (astr==='hovermode' && (val==='x' || val==='y')) {
-        val = layout._isHoriz ? 'y' : 'x';
+        val = fullLayout._isHoriz ? 'y' : 'x';
         button.setAttribute('data-val', val);
     }
 
@@ -189,7 +189,7 @@ function handleCartesian (ev) {
         _this.updateActiveButton();
         if(astr === 'dragmode') {
             Plotly.Fx.setCursor(
-                layout._paper.select('.nsewdrag'),
+                fullLayout._paper.select('.nsewdrag'),
                 {pan:'move', zoom:'crosshair'}[val]
             );
         }
@@ -201,28 +201,28 @@ function handleCartesian (ev) {
  * @Param {object} ev event object
  */
 function handleHover3d (ev) {
-    var _this = this,
-        button = ev.currentTarget,
+    var button = ev.currentTarget,
         attr = button.getAttribute('data-attr'),
-        val = button.getAttribute('data-val') || true,
-        layoutUpdate = {},
+        _this = this,
+        Plotly = this.Plotly,
         graphInfo = this.graphInfo,
-        layout = graphInfo._fullLayout,
-        scenes = Object.keys(layout).filter(function(k){
-            return k.match(/^scene[0-9]*$/);
-        }).map( function (sceneKey) {
-            return layout[sceneKey];
-        });
+        fullLayout = graphInfo._fullLayout,
+        sceneLayouts = Plotly.Lib.getSceneLayouts(fullLayout),
+        layoutUpdate = {};
 
+    // 3D has only 1 hover mode; toggle it
+    var val = fullLayout[attr]!=='closest' ? 'closest' : '';
     layoutUpdate[attr] = val;
 
-    scenes.forEach( function (sceneLayout) {
-        sceneLayout._scene.spikeEnable = !sceneLayout._scene.spikeEnable;
+    // Apply to all scenes
+    sceneLayouts.forEach( function (sceneLayout) {
+        var scene = sceneLayout._scene;
+        scene.spikeEnable = !scene.spikeEnable;
+        scene.container.focus();
     });
 
-    this.Plotly.relayout(graphInfo, layoutUpdate).then( function() {
+    Plotly.relayout(graphInfo, layoutUpdate).then( function() {
         _this.updateActiveButton();
-        scenes[0]._container.focus();
     });
 }
 
@@ -231,32 +231,30 @@ function handleHover3d (ev) {
  * @Param {object} ev event object
  */
 function handleDrag3d (ev) {
-    var _this = this,
-        button = ev.currentTarget,
+    var button = ev.currentTarget,
         attr = button.getAttribute('data-attr'),
         val = button.getAttribute('data-val') || true,
-        layoutUpdate = {},
+        _this = this,
+        Plotly = this.Plotly,
         graphInfo = this.graphInfo,
-        layout = graphInfo._fullLayout,
-        scenes = Object.keys(layout).filter(function(k){
-            return k.match(/^scene[0-9]*$/);
-        }).map( function (sceneKey) {
-            return layout[sceneKey];
-        });
+        fullLayout = graphInfo._fullLayout,
+        sceneLayouts = Plotly.Lib.getSceneLayouts(fullLayout),
+        layoutUpdate = {};
 
     // set dragmode to given value
     layoutUpdate[attr] = val;
 
-    // update the webgl3D key binding
-    scenes.forEach( function (sceneLayout) {
-        if ('_scene' in sceneLayout && 'camera' in sceneLayout._scene) {
-            sceneLayout._scene.camera.keyBindingMode = val;
+    // Update the webgl3D key binding of all scenes
+    sceneLayouts.forEach( function (sceneLayout) {
+        var scene = sceneLayout._scene;
+        if ('camera' in scene) {
+            scene.camera.keyBindingMode = val;
         }
     });
+            scene.container.focus();
 
-    this.Plotly.relayout(graphInfo, layoutUpdate).then( function() {
+    Plotly.relayout(graphInfo, layoutUpdate).then( function() {
         _this.updateActiveButton();
-        scenes[0]._container.focus();
     });
 }
 
