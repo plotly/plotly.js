75b3ea896bfffc84c24503222078a8e7e492bddc
etpinard <etienne.t.pinard@gmail.com>
2014-12-04 18:31:50 -0500

    rewrite SceneFrame.createScene():
    - create iframe dom element
    - instantiated Scene
    - (that's it)

--- gl3d/scene-framer.js
+++ gl3d/scene-framer.js
@@ -25,61 +25,26 @@ module.exports = new SceneFrame();
 
 var proto = SceneFrame.prototype;
 
-proto.createScene = function (opts) {
-    opts = opts || {};
-    var container = opts.container || document.body;
-    var newIframe = document.createElement('iframe');
-    var glOptions = opts.glOptions || {};
-    var _this = this;
-    newIframe.width = opts.width || '100%';
-    newIframe.height = opts.height || '100%';
-    newIframe.style.zIndex = '' + (opts.zIndex || '1000');
-    newIframe.frameBorder = '0';
-    newIframe.src = opts.baseurl + '/glcontext.html';
+proto.createScene = function (options) {
+    var container = options.container || document.body;
 
-    newIframe.id = opts.id || ('scene-'+ this.ID);
-    this.ID++;
+    // Make iframe dom element (the onload callback is in Scene)
+    function makeIframe(options) {
+        var newIframe = document.createElement('iframe');
+        newIframe.width = options.width || '100%';
+        newIframe.height = options.height || '100%';
+        newIframe.style.zIndex = '1000';
+        newIframe.frameBorder = '0';
+        newIframe.src = options.baseurl + '/glcontext.html';
+        newIframe.id = options.sceneKey;
+        return newIframe;
+    }
 
-    container.appendChild(newIframe);
+    var newIframe = makeIframe(options);
+    container.appendChild(newIframe);    // append svg container
 
-    newIframe.onload = function () {
+    var sceneOptions = options;
+    sceneOptions.container = newIframe;  // the iframe is the scene container
 
-    //Create canvas
-    var canvas = document.createElement('canvas');
-
-    //Try initializing WebGL
-    var gl = canvas.getContext('webgl', glOptions) ||
-             canvas.getContext('experimental-webgl', glOptions);
-
-        if(gl){
-
-            var shell = newIframe.contentWindow.glnow({
-                clearFlags: 0,
-                glOptions: glOptions,
-                tickRate: 3
-            });
-
-            // contain all events within the iframe, necessary to
-            // contain zoom events to prevent parent window scrolling.
-            shell.preventDefaults = true;
-            shell.stopPropagation = true;
-
-            // Once the shell has initialized create and pass a new scene to the user.
-            // set the container of the shell to be the new iframe
-            shell.once('gl-init', function () {
-                opts.container = newIframe;
-                var scene = new Scene(opts, shell);
-            });
-
-        }else{
-            opts.container = newIframe;
-            var scene = new Scene(opts, null);
-            newIframe.contentDocument.getElementById('no3D').style.display= 'block';
-            _this.emit('scene-error', scene);
-        }
-
-    canvas = null;
-    gl = null;
-
-    };
+    return new Scene(sceneOptions);
 };
