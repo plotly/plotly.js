ef993728c3292d7c9928e1764cbde5e4dfe4e8d6
etpinard <etienne.t.pinard@gmail.com>
2015-02-26 10:35:58 -0500

    add lightLine anc background color default in Color

--- shelly/plotlyjs/static/plotlyjs/src/color.js
+++ shelly/plotlyjs/static/plotlyjs/src/color.js
@@ -35,6 +35,8 @@
     ];
 
     color.defaultLine = '#444';
+    color.lightLine = '#eee';
+    color.background = '#fff';
 
     color.scales = {
         'Greys':[[0,'rgb(0,0,0)'],[1,'rgb(255,255,255)']],
