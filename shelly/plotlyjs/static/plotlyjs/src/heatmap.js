e7348b0686a8e7b0dec0f5399ca4d32da9237fad
alexcjohnson <alex@plot.ly>
2015-04-19 00:57:53 -0400

    lint heatmap.plot

--- shelly/plotlyjs/static/plotlyjs/src/heatmap.js
+++ shelly/plotlyjs/static/plotlyjs/src/heatmap.js
@@ -576,11 +576,11 @@ function plotOne(gd, plotinfo, cd) {
         id = 'hm' + uid,
         cbId = 'cb' + uid;
 
-    fullLayout._paper.selectAll('.contour'+uid).remove(); // in case this used to be a contour map
+    fullLayout._paper.selectAll('.contour' + uid).remove(); // in case this used to be a contour map
 
     if(trace.visible !== true) {
-        fullLayout._paper.selectAll('.'+id).remove();
-        fullLayout._paper.selectAll('.'+cbId).remove();
+        fullLayout._paper.selectAll('.' + id).remove();
+        fullLayout._paper.selectAll('.' + cbId).remove();
         return;
     }
 
@@ -615,36 +615,36 @@ function plotOne(gd, plotinfo, cd) {
     // TODO: use low-resolution images outside the visible plot for panning
     // these while loops find the first and last brick bounds that are defined
     // (in case of log of a negative)
-    i=0;
-    while(left===undefined && i<x.length-1) {
-        left=xa.c2p(x[i]);
+    i = 0;
+    while(left === undefined && i < x.length - 1) {
+        left = xa.c2p(x[i]);
         i++;
     }
-    i=x.length-1;
-    while(right===undefined && i>0) {
-        right=xa.c2p(x[i]);
+    i = x.length - 1;
+    while(right === undefined && i > 0) {
+        right = xa.c2p(x[i]);
         i--;
     }
 
-    if(right<left) {
+    if(right < left) {
         temp = right;
         right = left;
         left = temp;
         xrev = true;
     }
 
-    i=0;
-    while(top===undefined && i<y.length-1) {
-        top=ya.c2p(y[i]);
+    i = 0;
+    while(top === undefined && i < y.length - 1) {
+        top = ya.c2p(y[i]);
         i++;
     }
-    i=y.length-1;
-    while(bottom===undefined && i>0) {
-        bottom=ya.c2p(y[i]);
+    i = y.length - 1;
+    while(bottom === undefined && i > 0) {
+        bottom = ya.c2p(y[i]);
         i--;
     }
 
-    if(bottom<top) {
+    if(bottom < top) {
         temp = top;
         top = bottom;
         bottom = temp;
@@ -666,28 +666,28 @@ function plotOne(gd, plotinfo, cd) {
     // about this, because zooming doesn't increase number of pixels
     // if zsmooth is best, don't include anything off screen because it takes too long
     if(zsmooth !== 'fast') {
-        var extra = zsmooth==='best' ? 0 : 0.5;
-        left = Math.max(-extra*xa._length,left);
-        right = Math.min((1+extra)*xa._length,right);
-        top = Math.max(-extra*ya._length,top);
-        bottom = Math.min((1+extra)*ya._length,bottom);
+        var extra = zsmooth === 'best' ? 0 : 0.5;
+        left = Math.max(-extra * xa._length, left);
+        right = Math.min((1 + extra) * xa._length, right);
+        top = Math.max(-extra * ya._length, top);
+        bottom = Math.min((1 + extra) * ya._length, bottom);
     }
 
-    var wd=Math.round(right-left),
-        ht=Math.round(bottom-top);
+    var imageWidth = Math.round(right - left),
+        imageHeight = Math.round(bottom - top);
 
     // now redraw
 
     // if image is entirely off-screen, don't even draw it
-    if(wd<=0 || ht<=0) return;
+    if(imageWidth <= 0 || imageHeight <= 0) return;
 
     var canvasW, canvasH;
     if(zsmooth === 'fast') {
         canvasW = n;
         canvasH = m;
     } else {
-        canvasW = wd;
-        canvasH = ht;
+        canvasW = imageWidth;
+        canvasH = imageHeight;
     }
 
     var canvas = document.createElement('canvas');
@@ -696,6 +696,7 @@ function plotOne(gd, plotinfo, cd) {
     var context = canvas.getContext('2d');
 
     // interpolate for color scale
+    // use an array instead of color strings, so we preserve alpha
     var s = d3.scale.linear()
         .domain(scl.map(function(si){ return si[0]; }))
         .range(scl.map(function(si){
@@ -705,26 +706,38 @@ function plotOne(gd, plotinfo, cd) {
         .clamp(true);
 
     // map brick boundaries to image pixels
-    var xpx,ypx;
+    var xpx,
+        ypx;
     if(zsmooth === 'fast') {
-        xpx = xrev ? function(index){ return n-1-index; } : Plotly.Lib.identity;
-        ypx = yrev ? function(index){ return m-1-index; } : Plotly.Lib.identity;
+        xpx = xrev ?
+            function(index) { return n - 1 - index; } :
+            Plotly.Lib.identity;
+        ypx = yrev ?
+            function(index) { return m - 1 - index; } :
+            Plotly.Lib.identity;
     }
     else {
-        xpx = function(index){ return Plotly.Lib.constrain(Math.round(xa.c2p(x[index])-left),0,wd); };
-        ypx = function(index){ return Plotly.Lib.constrain(Math.round(ya.c2p(y[index])-top),0,ht); };
+        xpx = function(index){
+            return Plotly.Lib.constrain(Math.round(xa.c2p(x[index]) - left),
+                0, imageWidth);
+        };
+        ypx = function(index){
+            return Plotly.Lib.constrain(Math.round(ya.c2p(y[index]) - top),
+                0, imageHeight);
+        };
     }
 
     // get interpolated bin value. Returns {bin0:closest bin, frac:fractional dist to next, bin1:next bin}
-    function findInterp(pixel,pixArray) {
-        var maxbin = pixArray.length-2,
-            bin = Plotly.Lib.constrain(Plotly.Lib.findBin(pixel,pixArray),0,maxbin),
+    function findInterp(pixel, pixArray) {
+        var maxbin = pixArray.length - 2,
+            bin = Plotly.Lib.constrain(Plotly.Lib.findBin(pixel, pixArray), 0, maxbin),
             pix0 = pixArray[bin],
-            pix1 = pixArray[bin+1],
-            interp = Plotly.Lib.constrain(bin+(pixel-pix0)/(pix1-pix0)-0.5,0,maxbin),
+            pix1 = pixArray[bin + 1],
+            interp = Plotly.Lib.constrain(bin + (pixel - pix0) / (pix1 - pix0) - 0.5, 0, maxbin),
             bin0 = Math.round(interp),
-            frac = Math.abs(interp-bin0);
-        if(!interp || interp===maxbin || !frac) {
+            frac = Math.abs(interp - bin0);
+
+        if(!interp || interp === maxbin || !frac) {
             return {
                 bin0: bin0,
                 bin1: bin0,
@@ -734,7 +747,7 @@ function plotOne(gd, plotinfo, cd) {
         return {
             bin0: bin0,
             frac: frac,
-            bin1: Math.round(bin0+frac/(interp-bin0))
+            bin1: Math.round(bin0 + frac / (interp - bin0))
         };
     }
 
@@ -784,7 +797,7 @@ function plotOne(gd, plotinfo, cd) {
         else if(z10 === undefined) dxy = (2 * z11 - z01 - z00) * 2/3;
         else dxy = (z11 + z00 - z01 - z10);
 
-        setColor(z00 + xinterp.frac * dx + yinterp.frac*(dy + xinterp.frac * dxy));
+        setColor(z00 + xinterp.frac * dx + yinterp.frac * (dy + xinterp.frac * dxy));
     }
 
     Plotly.Lib.markTime('done init png');
@@ -804,31 +817,30 @@ function plotOne(gd, plotinfo, cd) {
         j,
         xi,
         v,
-        row,
-        c;
+        row;
 
     if(zsmooth === 'best') {
-        var xPixArray = [],
-            yPixArray = [],
-            xinterpArray = [],
+        var xPixArray = new Array(x.length),
+            yPixArray = new Array(y.length),
+            xinterpArray = new Array(imageWidth),
             yinterp,
             r0,
             r1;
 
         // first make arrays of x and y pixel locations of brick boundaries
-        for(i = 0; i < x.length; i++) xPixArray.push(Math.round(xa.c2p(x[i]) - left));
-        for(i = 0; i < y.length; i++) yPixArray.push(Math.round(ya.c2p(y[i]) - top));
+        for(i = 0; i < x.length; i++) xPixArray[i] = Math.round(xa.c2p(x[i]) - left);
+        for(i = 0; i < y.length; i++) yPixArray[i] = Math.round(ya.c2p(y[i]) - top);
 
         // then make arrays of interpolations
         // (bin0=closest, bin1=next, frac=fractional dist.)
-        for(i = 0; i < wd; i++) xinterpArray.push(findInterp(i, xPixArray));
+        for(i = 0; i < imageWidth; i++) xinterpArray[i] = findInterp(i, xPixArray);
 
         // now do the interpolations and fill the png
-        for(j = 0; j < ht; j++) {
+        for(j = 0; j < imageHeight; j++) {
             yinterp = findInterp(j, yPixArray);
             r0 = z[yinterp.bin0];
             r1 = z[yinterp.bin1];
-            for(i = 0; i < wd; i++) {
+            for(i = 0; i < imageWidth; i++) {
                 interpColor(r0, r1, xinterpArray[i], yinterp);
                 context.fillRect(i, j, 1, 1);
             }
@@ -884,13 +896,13 @@ function plotOne(gd, plotinfo, cd) {
         .classed(id, true)
         .datum(cd[0])
         .attr({
-            xmlns:'http://www.w3.org/2000/svg',
+            xmlns: 'http://www.w3.org/2000/svg',
             'xlink:xlink:href': canvas.toDataURL('image/png'), // odd d3 quirk, need namespace twice
-            height:ht,
-            width:wd,
-            x:left,
-            y:top,
-            preserveAspectRatio:'none'
+            height: imageHeight,
+            width: imageWidth,
+            x: left,
+            y: top,
+            preserveAspectRatio: 'none'
         });
 
     Plotly.Lib.markTime('done showing png');
