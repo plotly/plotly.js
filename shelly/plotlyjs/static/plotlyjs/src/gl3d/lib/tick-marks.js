4ee6241928bb54c52a7a198e0b9af78563e10e8a
bpostlethwaite <post.ben.here@gmail.com>
2015-04-24 14:06:02 -0400

    a little too much regex replace

--- shelly/plotlyjs/static/plotlyjs/src/gl3d/lib/tick-marks.js
+++ shelly/plotlyjs/static/plotlyjs/src/gl3d/lib/tick-marks.js
@@ -25,7 +25,7 @@ function contourLevelsFromTicks(ticks) {
 function computeTickMarks(scene) {
     var axesOptions = scene.axesOptions;
     var glRange     = scene.glplot.axesPixels;
-    var sceneLayout = scene.glplotLayout;
+    var sceneLayout = scene.sceneLayout;
 
     var ticks = [[],[],[]];
 
