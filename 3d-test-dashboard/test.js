9d99b5e3c887742b89d074ab938088a17c1a268e
mikolalysenko <mikolalysenko@gmail.com>
2015-05-14 14:47:21 -0400

    basic mesh 3d integration

--- 3d-test-dashboard/test.js
+++ 3d-test-dashboard/test.js
@@ -167,5 +167,7 @@ plots['opacity-surface'] = require('./testplots/opacity-surface.json');
 plots['projection-traces'] = require('./testplots/projection-traces.json');
 plots['opacity-scaling-spikes'] = require('./testplots/opacity-scaling-spikes.json');
 plots['text-weirdness'] = require('./testplots/text-weirdness.json');
+plots['triangle-mesh3d'] = require('./testplots/triangle.json');
+plots['snowden'] = require('./testplots/snowden.json');
 
 plotButtons(plots);
---
+++ shelly/plotlyjs/static/plotlyjs/src/gl3d/convert/mesh.js
@@ -0,0 +1,225 @@
+'use strict';
+
+var createMesh = require('gl-mesh3d'),
+    str2RgbaArray = require('../lib/str2rgbarray'),
+    tinycolor = require('tinycolor2');
+
+function Mesh3DTrace(scene, mesh, uid) {
+  this.scene    = scene;
+  this.uid      = uid;
+  this.mesh     = mesh;
+  this.name     = '';
+  this.data     = null;
+  this.showContour = false;
+}
+
+var proto = Mesh3DTrace.prototype;
+
+proto.handlePick = function(selection) {
+  if(selection.object === this.mesh) {
+    return true;
+  }
+};
+
+/*
+function parseColorScale (colorscale, alpha) {
+    if (alpha === undefined) alpha = 1;
+
+    return colorscale.map( function (elem) {
+        var index = elem[0];
+        var color = tinycolor(elem[1]);
+        var rgb = color.toRgb();
+        return {
+            index: index,
+            rgb: [rgb.r, rgb.g, rgb.b, alpha]
+        };
+    });
+}
+*/
+
+function zip3(x, y, z) {
+  var result = new Array(x.length);
+  for(var i=0; i<x.length; ++i) {
+    result[i] = [x[i], y[i], z[i]];
+  }
+  return result;
+}
+
+
+
+proto.update = function(data) {
+
+    var scene = this.scene,
+        layout = scene.fullSceneLayout;
+
+    //Unpack position data
+    function toDataCoords(axis, coord) {
+      return coord.map(function(x) {
+        return axis.d2l(x);
+      });
+    }
+    var positions = zip3(
+      toDataCoords(layout.xaxis, data.x),
+      toDataCoords(layout.yaxis, data.y),
+      toDataCoords(layout.zaxis, data.z));
+
+    //Unpack cell data
+    var cells = zip3(data.i, data.j, data.k);
+
+    //Update mesh
+    this.mesh.update({
+      positions: positions,
+      cells:     cells
+    });
+
+
+  /*
+    var i,
+        scene = this.scene,
+        sceneLayout = scene.fullSceneLayout,
+        surface = this.surface,
+        alpha = data.opacity,
+        colormap = parseColorScale(data.colorscale, alpha),
+        z = data.z,
+        x = data.x,
+        y = data.y,
+        xaxis = sceneLayout.xaxis,
+        yaxis = sceneLayout.yaxis,
+        zaxis = sceneLayout.zaxis,
+        xlen = z[0].length,
+        ylen = z.length,
+        coords = [
+            ndarray(new Float32Array(xlen * ylen), [xlen, ylen]),
+            ndarray(new Float32Array(xlen * ylen), [xlen, ylen]),
+            ndarray(new Float32Array(xlen * ylen), [xlen, ylen])
+        ],
+        xc = coords[0],
+        yc = coords[1],
+        contourLevels = scene.contourLevels;
+
+    fill(coords[2], function(row, col) {
+        return zaxis.d2l(z[col][row]);
+    });
+
+    // coords x
+    if (Array.isArray(x[0])) {
+        fill(xc, function(row, col) {
+            return xaxis.d2l(x[col][row]);
+        });
+    } else {
+        // ticks x
+        fill(xc, function(row) {
+            return xaxis.d2l(x[row]);
+        });
+    }
+
+    // coords y
+    if (Array.isArray(y[0])) {
+        fill(yc, function(row, col) {
+            return yaxis.d2l(y[col][row]);
+        });
+    } else {
+        // ticks y
+        fill(yc, function(row, col) {
+            return yaxis.d2l(y[col]);
+        });
+    }
+
+    //Refine if necessary
+    refine(coords);
+
+    var params = {
+        colormap:       colormap,
+        levels:         [[], [], []],
+        showContour:    [ true, true, true ],
+        contourProject: [ [ false, false, false ],
+                          [ false, false, false ],
+                          [ false, false, false ] ],
+        contourWidth:   [ 1, 1, 1 ],
+        contourColor:   [ [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1] ],
+        contourTint:    [ 1, 1, 1 ],
+        dynamicColor:   [ [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1] ],
+        dynamicWidth:   [ 1, 1, 1 ],
+        dynamicTint:    [ 1, 1, 1 ],
+        opacity:        1
+    };
+
+
+    if('opacity' in data) {
+        if(data.opacity < 1) {
+            params.opacity = 0.25 * data.opacity;
+        }
+    }
+
+    var highlightEnable            = [ true, true, true ];
+    var contourEnable              = [ true, true, true ];
+    var axis                       = [ 'x', 'y', 'z' ];
+
+    for(i = 0; i < 3; ++i) {
+        var contourParams          = data.contours[axis[i]];
+        highlightEnable[i]         = contourParams.highlight;
+        contourEnable[i]           = contourParams.show;
+
+        params.showContour[i]      = contourParams.show || contourParams.highlight;
+        if (!params.showContour[i]) {
+            continue;
+        }
+
+        params.contourProject[i]   = [
+            contourParams.project.x,
+            contourParams.project.y,
+            contourParams.project.z ];
+
+        if (contourParams.show) {
+            this.showContour = true;
+            params.levels[i]       = contourLevels[i];
+            params.contourColor[i] = str2RgbaArray(contourParams.color);
+            params.contourWidth[i] = contourParams.width;
+        } else {
+            this.showContour = false;
+        }
+
+        if (contourParams.highlight) {
+            params.dynamicColor[i] = str2RgbaArray(contourParams.highlightColor);
+            params.dynamicWidth[i] = contourParams.highlightWidth;
+        }
+    }
+
+    params.coords = coords;
+    surface.update(params);
+
+    surface.highlightEnable  = highlightEnable;
+    surface.contourEnable    = contourEnable;
+    surface.visible          = data.visible;
+
+    if ('lighting' in data) {
+        surface.ambientLight   = data.lighting.ambient;
+        surface.diffuseLight   = data.lighting.diffuse;
+        surface.specularLight  = data.lighting.specular;
+        surface.roughness      = data.lighting.roughness;
+        surface.fresnel        = data.lighting.fresnel;
+    }
+
+    if (alpha && alpha < 1) {
+        surface.supportsTransparency = true;
+    }
+    */
+};
+
+proto.dispose = function() {
+  this.glplot.remove(this.mesh);
+  this.mesh.dispose();
+};
+
+function createMesh3DTrace(scene, data) {
+  var gl = scene.glplot.gl;
+  var mesh = createMesh({
+    gl: gl
+  });
+  var result = new Mesh3DTrace(scene, mesh, data.uid);
+  result.update(data);
+  scene.glplot.add(mesh);
+  return result;
+}
+
+module.exports = createMesh3DTrace;
