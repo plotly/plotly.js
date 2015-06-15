ca9921ab4b624cb8af9da90ffc2abcffa3cb8269
etpinard <etienne.t.pinard@gmail.com>
2015-06-15 13:10:27 -0400

    add geo test plot list and JSONs

---
+++ 3d-test-dashboard/test-geo.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var plotButtons = require('./buttons');
+
+var plots = {};
+
+plots['first'] = require('./testplots-geo/first.json');
+plots['second'] = require('./testplots-geo/second.json');
+plots['kavrayskiy7'] = require('./testplots-geo/kavrayskiy7.json');
+plots['custom-colorscale'] = require('./testplots-geo/custom-colorscale.json');
+plots['scattergeo-locations'] = require('./testplots-geo/scattergeo-locations.json');
+plots['multi-geos'] = require('./testplots-geo/multi-geos.json');
+plots['usa-states'] = require('./testplots-geo/usa-states.json');
+plots['legendonly'] = require('./testplots-geo/legendonly.json');
+plots['europe-bubbles'] = require('./testplots-geo/europe-bubbles.json');
+plots['orthographic'] = require('./testplots-geo/orthographic.json');
+
+plotButtons(plots, '/testplots-geo/');
