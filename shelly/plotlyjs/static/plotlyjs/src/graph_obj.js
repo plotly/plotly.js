c67e4fe30098cec94881738a8e9c913c58f2bc30
etpinard <etienne.t.pinard@gmail.com>
2015-07-02 14:27:03 -0400

    [lint] sub if ( -> if(

--- shelly/plotlyjs/static/plotlyjs/src/graph_obj.js
+++ shelly/plotlyjs/static/plotlyjs/src/graph_obj.js
@@ -1437,21 +1437,21 @@ plots.supplyDataDefaults = function(traceIn, i, layout) {
     // this is necessary otherwise we lose references to scene objects when
     // the traces of a scene are invisible. Also we handle visible/unvisible
     // differently for 3D cases.
-    if (plots.traceIs(traceOut, 'gl3d')) scene = coerce('scene');
+    if(plots.traceIs(traceOut, 'gl3d')) scene = coerce('scene');
 
-    if (plots.traceIs(traceOut, 'geo')) scene = coerce('geo');
+    if(plots.traceIs(traceOut, 'geo')) scene = coerce('geo');
 
     // module-specific attributes --- note: we need to send a trace into
     // the 3D modules to have it removed from the webgl context.
-    if (visible || scene) {
+    if(visible || scene) {
         module = plots.getModule(traceOut);
         traceOut._module = module;
     }
 
-    if (module && visible) module.supplyDefaults(traceIn, traceOut, defaultColor, layout);
+    if(module && visible) module.supplyDefaults(traceIn, traceOut, defaultColor, layout);
 
     if(visible) {
-        coerce('name', 'trace '+i);
+        coerce('name', 'trace ' + i);
 
         coerce('hoverinfo');
 
