7c58a215c14967854b180ca77d7a1d437c7fb4ed
etpinard <etienne.t.pinard@gmail.com>
2015-07-30 13:51:10 -0400

    add 'doubleClick' plot config

--- shelly/plotlyjs/static/plotlyjs/src/graph_obj.js
+++ shelly/plotlyjs/static/plotlyjs/src/graph_obj.js
@@ -205,21 +205,38 @@ function opaqueSetBackground(gd, bgColor) {
 // the defaults are the appropriate settings for plotly.js,
 // so we get the right experience without any config argument
 plots.defaultConfig = {
-    staticPlot: false, // no interactivity, for export or image generation
-    workspace: false, // we're in the workspace, so need toolbar etc TODO describe functionality instead?
-    editable: false, // we can edit titles, move annotations, etc
-    autosizable: false, // plot will respect layout.autosize=true and infer its container size
-    fillFrame: false, // if we DO autosize, do we fill the container or the screen?
-    scrollZoom: false, // mousewheel or two-finger scroll zooms the plot
-    showTips: true, // new users see some hints about interactivity
-    showLink: true, // link to open this plot in plotly
-    sendData: true, // if we show a link, does it contain data or just link to a plotly file?
-    linkText: 'Edit chart', // text appearing in the sendData link
-    displayModeBar: 'hover', // display the modebar (true, false, or 'hover')
-    displaylogo: true, // add the plotly logo on the end of the modebar
-    plot3dPixelRatio: 2, // increase the pixel ratio for 3D plot images
-    setBackground: defaultSetBackground // fn to add the background color to a different container
-                                        // or 'opaque' to ensure there's white behind it
+    // no interactivity, for export or image generation
+    staticPlot: false,
+    // we're in the workspace, so need toolbar etc
+    // TODO describe functionality instead?
+    workspace: false,
+    // we can edit titles, move annotations, etc
+    editable: false,
+    // plot will respect layout.autosize=true and infer its container size
+    autosizable: false,
+    // if we DO autosize, do we fill the container or the screen?
+    fillFrame: false,
+    // mousewheel or two-finger scroll zooms the plot
+    scrollZoom: false,
+    // double click interaction (false, 'reset', 'autosize' or 'reset+autosize')
+    doubleClick: 'reset+autosize',
+    // new users see some hints about interactivity
+    showTips: true,
+    // link to open this plot in plotly
+    showLink: true,
+    // if we show a link, does it contain data or just link to a plotly file?
+    sendData: true,
+    // text appearing in the sendData link
+    linkText: 'Edit chart',
+    // display the modebar (true, false, or 'hover')
+    displayModeBar: 'hover',
+    // add the plotly logo on the end of the modebar
+    displaylogo: true,
+    // increase the pixel ratio for 3D plot images
+    plot3dPixelRatio: 2,
+    // fn to add the background color to a different container
+    // or 'opaque' to ensure there's white behind it
+    setBackground: defaultSetBackground
 };
 
 function setPlotContext(gd, config) {
@@ -248,6 +265,7 @@ function setPlotContext(gd, config) {
         context.editable = false;
         context.autosizable = false;
         context.scrollZoom = false;
+        context.doubleClick = false;
         context.showTips = false;
         context.showLink = false;
         context.displayModeBar = false;
