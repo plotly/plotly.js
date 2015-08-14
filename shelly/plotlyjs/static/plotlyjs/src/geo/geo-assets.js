15aaab883cc07f12abc79979d3bdc494c42efb69
etpinard <etienne.t.pinard@gmail.com>
2015-08-14 11:35:10 -0400

    fix dump typo in geo-assets file

--- shelly/plotlyjs/static/plotlyjs/src/geo/geo-assets.js
+++ shelly/plotlyjs/static/plotlyjs/src/geo/geo-assets.js
@@ -1,8 +1,7 @@
 exports.topojsons = {};
 
 exports.topojsons['world_110m'] = require('./topojson/world_110m.json');
-
-exports.topojsons['africa_10m'] = require('./topojson/africa_110m.json');
+exports.topojsons['africa_110m'] = require('./topojson/africa_110m.json');
 exports.topojsons['asia_110m'] = require('./topojson/asia_110m.json');
 exports.topojsons['europe_110m'] = require('./topojson/europe_110m.json');
 exports.topojsons['north-america_110m'] = require('./topojson/north-america_110m.json');
@@ -10,7 +9,7 @@ exports.topojsons['south-america_110m'] = require('./topojson/south-america_110m
 exports.topojsons['usa_110m'] = require('./topojson/usa_110m');
 
 exports.topojsons['world_50m'] = require('./topojson/world_50m.json');
-exports.topojsons['africa_10m'] = require('./topojson/africa_50m.json');
+exports.topojsons['africa_50m'] = require('./topojson/africa_50m.json');
 exports.topojsons['asia_50m'] = require('./topojson/asia_50m.json');
 exports.topojsons['europe_50m'] = require('./topojson/europe_50m.json');
 exports.topojsons['north-america_50m'] = require('./topojson/north-america_50m.json');
