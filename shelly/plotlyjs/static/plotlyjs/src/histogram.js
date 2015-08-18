1b222e3a2692593e94bbe104b93580b6278e7493
etpinard <etienne.t.pinard@gmail.com>
2015-08-18 15:22:46 -0400

    add description to histogram.js

--- shelly/plotlyjs/static/plotlyjs/src/histogram.js
+++ shelly/plotlyjs/static/plotlyjs/src/histogram.js
@@ -3,7 +3,6 @@
 var histogram = module.exports = {},
     Plotly = require('./plotly'),
     isNumeric = require('./isnumeric'),
-    scatterAttrs = Plotly.Scatter.attributes,
     barAttrs = Plotly.Bars.attributes;
 
 /**
@@ -22,25 +21,52 @@ Plotly.Plots.register(Plotly.Heatmap, 'histogram2d',
 // but uses Bars.plot to display
 // and Bars.setPositions for stacking and grouping
 
-var defaultBins = {
-    start: {
-        type: 'number',
-        dflt: 0
-    },
-    end: {
-        type: 'number',
-        dflt: 1
-    },
-    size: {
-        type: 'any', // for date axes
-        dflt: 1
-    }
-};
+function makeBinsAttr(axLetter) {
+    return {
+        start: {
+            type: 'number',
+            dflt: 0,
+            description: [
+                'Sets the starting value for the', axLetter,
+                'axis bins.'
+            ].join(' ')
+        },
+        end: {
+            type: 'number',
+            dflt: 1,
+            description: [
+                'Sets the end value for the', axLetter,
+                'axis bins.'
+            ].join(' ')
+        },
+        size: {
+            type: 'any', // for date axes
+            dflt: 1,
+            description: [
+                'Sets the step in-between value each', axLetter,
+                'axis bin.'
+            ].join(' ')
+        }
+    };
+}
 
 histogram.attributes = {
-    z: {type: 'data_array'},
-    x: scatterAttrs.x,
-    y: scatterAttrs.y,
+    x: {
+        type: 'data_array',
+        description: [
+            'Sets the sample data to be binned on the x axis.'
+        ].join(' ')
+    },
+    y: {
+        type: 'data_array',
+        description: [
+            'Sets the sample data to be binned on the y axis.'
+        ].join(' ')
+    },
+    z: {
+        type: 'data_array',
+        description: 'Sets the aggregation data.'
+    },
     marker: {
         color: {type: 'data_array'}
     },
@@ -48,33 +74,72 @@ histogram.attributes = {
     histfunc: {
         type: 'enumerated',
         values: ['count', 'sum', 'avg', 'min', 'max'],
-        dflt: 'count'
+        dflt: 'count',
+        description: [
+            'Specifies the binning function used for this histogram trace.',
+
+            'If *count*, the histogram values are computed by counting the',
+            'number of values lying inside each bin.',
+
+            'If *sum*, *avg*, *min*, *max*,',
+            'the histogram values are computed using',
+            'the sum, the average, the minimum or the maximum',
+            'of the values lying inside each bin respectively.'
+        ].join(' ')
     },
     histnorm: {
         type: 'enumerated',
         values: ['', 'percent', 'probability', 'density', 'probability density'],
-        dflt: ''
+        dflt: '',
+        description: [
+            'Specifies the type of normalization used for this histogram trace.',
+
+            'If **, the span of each bar corresponds to the number of',
+            'occurrences (i.e. the number of data points lying inside the bins).',
+
+            'If *percent*, the span of each bar corresponds to the percentage',
+            'of occurrences with respect to the total number of sample points',
+            '(here, the sum of all bin area equals 100%)',
+
+            'If *density*, the span of each bar corresponds to the number of',
+            'occurrences in a bin divided by the size of the bin interval',
+            '(here, the sum of all bin area equals the total number of sample points)',
+
+            'If *probability density*, the span of each bar corresponds to the',
+            'probability that an event will fall into the corresponding bin',
+            '(here, the sum of all bin area equals 1)'
+        ].join(' ')
     },
     autobinx: {
         type: 'boolean',
-        dflt: true
+        dflt: true,
+        description: [
+            'Determines whether or not the x axis bin attributes are picked',
+            'by an algorithm.'
+        ].join(' ')
     },
     nbinsx: {
         type: 'integer',
         min: 0,
-        dflt: 0
+        dflt: 0,
+        description: 'Sets the number of x axis bins.'
     },
-    xbins: defaultBins,
+    xbins: makeBinsAttr('x'),
     autobiny: {
         type: 'boolean',
-        dflt: true
+        dflt: true,
+        description: [
+            'Determines whether or not the y axis bin attributes are picked',
+            'by an algorithm.'
+        ].join(' ')
     },
     nbinsy: {
         type: 'integer',
         min: 0,
-        dflt: 0
+        dflt: 0,
+        description: 'Sets the number of y axis bins.'
     },
-    ybins: defaultBins
+    ybins: makeBinsAttr('y')
 };
 
 histogram.supplyDefaults = function(traceIn, traceOut) {
