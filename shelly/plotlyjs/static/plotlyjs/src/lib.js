57ffe045fac23f3f15248ebc0c9f32a02ff4e1ae
etpinard <etienne.t.pinard@gmail.com>
2015-08-21 17:29:57 -0400

    expose valObjects in Lib to define value types:
    - put old coerceIt content in valObject[valType].coerceFunction
    - add 'description' to each valType
    - add 'requiredOpts' and 'otherOpts' to list required and
      valid options for each valType

--- shelly/plotlyjs/static/plotlyjs/src/lib.js
+++ shelly/plotlyjs/static/plotlyjs/src/lib.js
@@ -371,7 +371,7 @@ function lessOrEqual(a, b) { return a <= b; }
 function greaterThan(a, b) { return a > b; }
 function greaterOrEqual(a, b) { return a >= b; }
 
-lib.sorterAsc = function(a, b) { return a - b; }
+lib.sorterAsc = function(a, b) { return a - b; };
 
 /**
  * find distinct values in an array, lumping together ones that appear to
@@ -1384,133 +1384,262 @@ function getFontAttrsArrayOk() {
    return fontAttrsArrayOk;
 }
 
-var coerceIt = {
-    data_array: function(v, propOut, dflt) {
-        /**
-         * data_array: value MUST be an array, or we ignore it
-         * you can use dflt=[] to force said array to exist though
-         */
-        if(Array.isArray(v)) propOut.set(v);
-        else if(dflt!==undefined) propOut.set(dflt);
-    },
-    enumerated: function(v, propOut, dflt, opts) {
-        if(opts.coerceNumber) v = +v;
-        if(opts.values.indexOf(v)===-1) propOut.set(dflt);
-        else propOut.set(v);
-    },
-    boolean: function(v, propOut, dflt) {
-        if(v===true || v===false) propOut.set(v);
-        else propOut.set(dflt);
-    },
-    number: function(v, propOut, dflt, opts) {
-        if(!isNumeric(v) ||
-                (opts.min!==undefined && v<opts.min) ||
-                (opts.max!==undefined && v>opts.max)) {
-            propOut.set(dflt);
-        }
-        else propOut.set(+v);
-    },
-    integer: function(v, propOut, dflt, opts) {
-        if(v%1 || !isNumeric(v) ||
-                (opts.min!==undefined && v<opts.min) ||
-                (opts.max!==undefined && v>opts.max)) {
-            propOut.set(dflt);
-        }
-        else propOut.set(+v);
-    },
-    string: function(v, propOut, dflt, opts) {
-        if(opts.strict===true && typeof v !== 'string') {
-            propOut.set(dflt);
-            return;
-        }
+var colorscaleNames = Object.keys(require('./colorscale').scales);
 
-        var s = String(v);
-        if(v===undefined || (opts.noBlank===true && !s)) {
+lib.valObjects = {
+    data_array: {
+        // You can use *dflt=[] to force said array to exist though.
+        description: [
+            'An {array} of data.',
+            'The value MUST be an {array}, or we ignore it.'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(Array.isArray(v)) propOut.set(v);
+            else if(dflt!==undefined) propOut.set(dflt);
+        }
+    },
+    enumerated: {
+        description: [
+            'Enumerated value type. The available values are listed',
+            'in `values`.'
+        ].join(' '),
+        requiredOpts: ['values'],
+        otherOpts: ['dflt', 'coerceNumber', 'arrayOk'],
+        coerceFunction: function(v, propOut, dflt, opts) {
+            if(opts.coerceNumber) v = +v;
+            if(opts.values.indexOf(v)===-1) propOut.set(dflt);
+            else propOut.set(v);
+        }
+    },
+    boolean: {
+        description: 'A boolean (true/false) value.',
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(v===true || v===false) propOut.set(v);
+            else propOut.set(dflt);
+        }
+    },
+    number: {
+        description: [
+            'A number or a numeric value',
+            '(e.g. a number inside a string).',
+            'When applicable, values greater (less) than `max` (`min`)',
+            'are coerced to the `dflt`.'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt', 'min', 'max', 'arrayOk'],
+        coerceFunction: function(v, propOut, dflt, opts) {
+            if(!isNumeric(v) ||
+                    (opts.min!==undefined && v<opts.min) ||
+                    (opts.max!==undefined && v>opts.max)) {
+                propOut.set(dflt);
+            }
+            else propOut.set(+v);
+        }
+    },
+    integer: {
+        description: [
+            'An integer or an integer inside a string.',
+            'When applicable, values greater (less) than `max` (`min`)',
+            'are coerced to the `dflt`.'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt', 'min', 'max'],
+        coerceFunction: function(v, propOut, dflt, opts) {
+            if(v%1 || !isNumeric(v) ||
+                    (opts.min!==undefined && v<opts.min) ||
+                    (opts.max!==undefined && v>opts.max)) {
+                propOut.set(dflt);
+            }
+            else propOut.set(+v);
+        }
+    },
+    string: {
+        description: [
+            'A string value.',
+            'Numbers are converted to strings except for attributes with',
+            '`strict` set to true.'
+        ].join(' '),
+        requiredOpts: [],
+        // TODO 'values shouldn't be in there (edge case: 'dash' in Scatter)
+        otherOpts: ['dflt', 'noBlank', 'strict', 'values'],
+        coerceFunction: function(v, propOut, dflt, opts) {
+            if(opts.strict===true && typeof v !== 'string') {
+                propOut.set(dflt);
+                return;
+            }
+
+            var s = String(v);
+            if(v===undefined || (opts.noBlank===true && !s)) {
+                propOut.set(dflt);
+            }
+            else propOut.set(s);
+        }
+    },
+    color: {
+        description: [
+            'A string describing color.',
+            'Supported formats:',
+            '- hex (e.g. \'#d3d3d3\')',
+            '- rgb (e.g. \'rgb(255, 0, 0)\')',
+            '- rgba (e.g. \'rgb(255, 0, 0, 0.5)\')',
+            '- hsl (e.g. \'hsl(0, 100%, 50%)\')',
+            '- hsv (e.g. \'hsv(0, 100%, 100%)\')',
+            '- named colors (full list: http://www.w3.org/TR/css3-color/#svg-color)'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt', 'arrayOk'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(tinycolor(v).isValid()) propOut.set(v);
+            else propOut.set(dflt);
+        }
+    },
+    colorscale: {
+        description: [
+            'A Plotly colorscale either picked by a name:',
+            '(any of', colorscaleNames.join(', '), ')',
+            'customized as an {array} of 2-element {arrays} where',
+            'the first element is the normalized color level value and the,',
+            'second item is a valid color string.'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            propOut.set(Plotly.Colorscale.getScale(v, dflt));
+        }
+    },
+    font: {
+        description: [
+            'An {object} describing a font.',
+            'The valid inner keys are:',
+            '- \'size\' (a number greater than *0*)',
+            '- \'color\' (a color string)',
+            '- \'family\' (a CSS font family name).'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt', 'arrayOk'],
+        coerceFunction: function(v, propOut, dflt, opts) {
+            if(!v) v = {};
+            var vOut = {},
+                _fontAttrs = (opts && opts.arrayOk) ?
+                    getFontAttrsArrayOk() : fontAttrs;
+
+            lib.coerce(v, vOut, _fontAttrs, 'size', dflt.size);
+            lib.coerce(v, vOut, _fontAttrs, 'color', dflt.color);
+            lib.coerce(v, vOut, _fontAttrs, 'family', dflt.family);
+
+            propOut.set(vOut);
+        }
+    },
+    angle: {
+        description: [
+            'A number (in degree) between -180 and 180.'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(v==='auto') propOut.set('auto');
+            else if(!isNumeric(v)) propOut.set(dflt);
+            else {
+                if(Math.abs(v)>180) v -= Math.round(v/360)*360;
+                propOut.set(+v);
+            }
+        }
+    },
+    axisid: {
+        description: [
+            'An axis id string (e.g. \'x\', \'x2\', \'x3\', ...).'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(typeof v === 'string' && v.charAt(0)===dflt) {
+                var axnum = Number(v.substr(1));
+                if(axnum%1 === 0 && axnum>1) {
+                    propOut.set(v);
+                    return;
+                }
+            }
             propOut.set(dflt);
         }
-        else propOut.set(s);
     },
-    color: function(v, propOut, dflt) {
-        if(tinycolor(v).isValid()) propOut.set(v);
-        else propOut.set(dflt);
-    },
-    colorscale: function(v, propOut, dflt) {
-        propOut.set(Plotly.Colorscale.getScale(v, dflt));
-    },
-    font: function(v, propOut, dflt, opts) {
-        if(!v) v = {};
-        var vOut = {},
-            _fontAttrs = (opts && opts.arrayOk) ?
-                getFontAttrsArrayOk() : fontAttrs;
-
-        lib.coerce(v, vOut, _fontAttrs, 'size', dflt.size);
-        lib.coerce(v, vOut, _fontAttrs, 'color', dflt.color);
-        lib.coerce(v, vOut, _fontAttrs, 'family', dflt.family);
-
-        propOut.set(vOut);
-    },
-    angle: function(v, propOut, dflt) {
-        if(v==='auto') propOut.set('auto');
-        else if(!isNumeric(v)) propOut.set(dflt);
-        else {
-            if(Math.abs(v)>180) v -= Math.round(v/360)*360;
-            propOut.set(+v);
+    sceneid: {
+        description: [
+            'A scene id string (e.g. \'scene\', \'scene2\', \'scene3\', ...).'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(typeof v === 'string' && v.substr(0,5)===dflt) {
+                var scenenum = Number(v.substr(5));
+                if(scenenum%1 === 0 && scenenum>1) {
+                    propOut.set(v);
+                    return;
+                }
+            }
+            propOut.set(dflt);
         }
     },
-    axisid: function(v, propOut, dflt) {
-        if(typeof v === 'string' && v.charAt(0)===dflt) {
-            var axnum = Number(v.substr(1));
-            if(axnum%1 === 0 && axnum>1) {
+    geoid: {
+        description: [
+            'A geo id string (e.g. \'geo\', \'geo2\', \'geo3\', ...).'
+        ].join(' '),
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(typeof v === 'string' && v.substr(0,3)===dflt) {
+                var geonum = Number(v.substr(3));
+                if(geonum%1 === 0 && geonum>1) {
+                    propOut.set(v);
+                    return;
+                }
+            }
+            propOut.set(dflt);
+        }
+    },
+    flaglist: {
+        description: [
+            'A string representing a combination of flags',
+            '(other does not matter here).',
+            'Combine any of the available `flags` with *+*.',
+            '(e.g. (\'lines+markers\')).',
+            'Values in `extras` cannot be combined.'
+        ].join(' '),
+        requiredOpts: ['flags'],
+        otherOpts: ['dflt', 'extras'],
+        coerceFunction: function(v, propOut, dflt, opts) {
+            if(typeof v !== 'string') {
+                propOut.set(dflt);
+                return;
+            }
+            if(opts.extras.indexOf(v)!==-1) {
                 propOut.set(v);
                 return;
             }
-        }
-        propOut.set(dflt);
-    },
-    sceneid: function(v, propOut, dflt) {
-        if(typeof v === 'string' && v.substr(0,5)===dflt) {
-            var scenenum = Number(v.substr(5));
-            if(scenenum%1 === 0 && scenenum>1) {
-                propOut.set(v);
-                return;
+            var vParts = v.split('+'),
+                i = 0;
+            while(i<vParts.length) {
+                var vi = vParts[i];
+                if(opts.flags.indexOf(vi)===-1 || vParts.indexOf(vi)<i) {
+                    vParts.splice(i,1);
+                }
+                else i++;
             }
+            if(!vParts.length) propOut.set(dflt);
+            else propOut.set(vParts.join('+'));
         }
-        propOut.set(dflt);
     },
-    geoid: function(v, propOut, dflt) {
-        if(typeof v === 'string' && v.substr(0,3)===dflt) {
-            var geonum = Number(v.substr(3));
-            if(geonum%1 === 0 && geonum>1) {
-                propOut.set(v);
-                return;
-            }
+    any: {
+        description: 'Any type.',
+        requiredOpts: [],
+        otherOpts: ['dflt'],
+        coerceFunction: function(v, propOut, dflt) {
+            if(v===undefined) propOut.set(dflt);
+            else propOut.set(v);
         }
-        propOut.set(dflt);
-    },
-    flaglist: function(v, propOut, dflt, opts) {
-        if(typeof v !== 'string') {
-            propOut.set(dflt);
-            return;
-        }
-        if(opts.extras.indexOf(v)!==-1) {
-            propOut.set(v);
-            return;
-        }
-        var vParts = v.split('+'),
-            i = 0;
-        while(i<vParts.length) {
-            var vi = vParts[i];
-            if(opts.flags.indexOf(vi)===-1 || vParts.indexOf(vi)<i) {
-                vParts.splice(i,1);
-            }
-            else i++;
-        }
-        if(!vParts.length) propOut.set(dflt);
-        else propOut.set(vParts.join('+'));
-    },
-    any: function(v, propOut, dflt) {
-        if(v===undefined) propOut.set(dflt);
-        else propOut.set(v);
     }
 };
 
@@ -1545,7 +1674,7 @@ lib.coerce = function(containerIn, containerOut, attributes, attribute, dflt) {
         return v;
     }
 
-    coerceIt[opts.valType](v, propOut, dflt, opts);
+    lib.valObjects[opts.valType].coerceFunction(v, propOut, dflt, opts);
 
     return propOut.get();
 };
