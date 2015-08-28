50d040bdaf2839517cf0a2dd04b3421aa443f89a
etpinard <etienne.t.pinard@gmail.com>
2015-08-28 14:21:13 -0400

    add 'role' to list of compatible options

--- shelly/plotlyjs/static/plotlyjs/tests/plotschema_test.js
+++ shelly/plotlyjs/static/plotlyjs/tests/plotschema_test.js
@@ -58,7 +58,7 @@ describe('plot schema', function() {
             var valObject = valObjects[attr.valType],
                 opts = valObject.requiredOpts
                     .concat(valObject.otherOpts)
-                    .concat(['valType', 'description']);
+                    .concat(['valType', 'description', 'role']);
 
             Object.keys(attr).forEach(function(key) {
                 expect(opts.indexOf(key) !== -1).toBe(true);
