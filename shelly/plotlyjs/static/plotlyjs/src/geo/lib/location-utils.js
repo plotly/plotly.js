486d27b0a2c634a7e90289686cc9773a4fef25a3
etpinard <etienne.t.pinard@gmail.com>
2015-09-09 19:25:41 -0400

    modif countryNameToISO3 for new country-name_to_iso3.json struct

--- shelly/plotlyjs/static/plotlyjs/src/geo/lib/location-utils.js
+++ shelly/plotlyjs/static/plotlyjs/src/geo/lib/location-utils.js
@@ -3,7 +3,11 @@
 var locationUtils = module.exports = {};
 
 var Plotly = require('../../plotly'),
+    // an hash object iso3 to regex string
     countryNameData = require('../raw/country-name_to_iso3.json');
+
+// make list of all country iso3 ids from at runtime
+var countryIds = Object.keys(countryNameData);
 
 var locationmodeToIdFinder = {
     'ISO-3': Plotly.Lib.identity,
@@ -18,13 +22,13 @@ locationUtils.locationToId = function(locationmode, location) {
 
 
 function countryNameToISO3(countryName) {
-    var item, regex;
+    var iso3, regex;
 
-    for(var i = 0; i < countryNameData.length; i++) {
-        item = countryNameData[i];
-        regex = new RegExp(item.regex);
+    for(var i = 0; i < countryIds.length; i++) {
+        iso3 = countryIds[i];
+        regex = new RegExp(countryNameData[iso3]);
 
-        if(regex.test(countryName.toLowerCase())) return item.iso3;
+        if(regex.test(countryName.toLowerCase())) return iso3;
     }
 
     console.warn('unrecognized country name ' + countryName);
