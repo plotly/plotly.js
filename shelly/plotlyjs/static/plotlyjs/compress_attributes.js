1e1430f1897d3a283ce26d0a9d165b8dfaf7cec3
etpinard <etienne.t.pinard@gmail.com>
2015-09-10 17:29:31 -0400

    add regex generator for object to delete (e.g. '_deprecated': {})

--- shelly/plotlyjs/static/plotlyjs/compress_attributes.js
+++ shelly/plotlyjs/static/plotlyjs/compress_attributes.js
@@ -7,18 +7,30 @@
 
 var through = require('through2');
 
-var attributeNamesToRemove = ['description', 'requiredOpts', 'otherOpts', 'hrName', 'role'],
-    regexStr = '';
+var attributeNamesToRemove = [
+    'description', 'requiredOpts', 'otherOpts', 'hrName', 'role'
+];
+
+var objectNamesToRemove = ['_deprecated'];
 
 // ref: http://www.regexr.com/3bj6p
+var regexStr = '';
 attributeNamesToRemove.forEach(function(attr, i) {
     // one line string with or without trailing comma
     regexStr += attr + ': \'.*\'' + ',?' + '|';
-    // array of strings with or without trailing comma
-    regexStr += attr + ': \\[[\\s\\S]*?\\].*' + ',?';
 
-    if(i !== attributeNamesToRemove.length-1) regexStr += '|';
+    // array (of strings) with or without trailing comma
+    regexStr += attr + ': \\[[\\s\\S]*?\\].*' + ',?' + '|';
 });
+
+// ref: http://www.regexr.com/3bor2
+objectNamesToRemove.forEach(function(obj) {
+    // object with '// to delete following trailling '}'
+    regexStr += obj + ': {[\\s\\S]*?}' + ',? ?\\/\\/ ?to delete' + '|';
+});
+
+// remove trailing '|'
+regexStr = regexStr.substring(0, regexStr.length-1);
 
 var regex = new RegExp(regexStr, 'g');
 
