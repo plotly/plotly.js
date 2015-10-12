6303fa690f55f8ad78b2b016c3b276787c83df07
chriddyp <chris@plot.ly>
2015-10-12 00:08:42 -0400

    explain fonts, and font support.

--- shelly/plotlyjs/static/plotlyjs/src/graph_obj.js
+++ shelly/plotlyjs/static/plotlyjs/src/graph_obj.js
@@ -1563,7 +1563,18 @@ plots.fontAttrs = {
         valType: 'string',
         role: 'style',
         noBlank: true,
-        strict: true
+        strict: true,
+        description: [
+            'HTML font family - the typeface that will be applied by the web browser.',
+            'The web browser will only be able to apply a font if it is available on the system',
+            'which it operates. Provide multiple font families, separated by commas, to indicate',
+            'the preference in which to apply fonts if they aren\'t available on the system.',
+            'Plotly\'s image generation is performed on a server, where only a select number of',
+            'fonts are installed and supported.',
+            'These include *Arial*, *Balto*, *Courier New*, *Droid Sans*,, *Droid Serif*,',
+            '*Droid Sans Mono*, *Gravitas One*, *Old Standard TT*, *Open Sans*, *Overpass*,'
+            '*PT Sans Narrow*, *Raleway*, *Times New Roman*.'
+        ].join(' ')
     },
     size: {
         valType: 'number',
