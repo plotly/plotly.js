4572f8d88e1144118e78ca06c3b8639ada49c51c
etpinard <etienne.t.pinard@gmail.com>
2015-10-14 14:26:15 -0400

    update scattergl attribute to reflect version 1.0 integration choices

--- shelly/plotlyjs/static/plotlyjs/src/gl2d/scattergl/attributes.js
+++ shelly/plotlyjs/static/plotlyjs/src/gl2d/scattergl/attributes.js
@@ -1,37 +1,56 @@
 'use strict';
 
-var Plotly = require('../../plotly');
+var Plotly = require('../../plotly'),
+    extendFlat = Plotly.Lib.extendFlat;
 
 var scatterAttrs = Plotly.Scatter.attributes,
     scatterLineAttrs = scatterAttrs.line,
     scatterMarkerAttrs = scatterAttrs.marker,
-    scatterMarkerLineAttrs = scatterMarkerAttrs.line,
-    extendFlat = Plotly.Lib.extendFlat;
+    scatterMarkerLineAttrs = scatterMarkerAttrs.line;
 
 module.exports = {
-    x: {
-        valType: 'data_array',
-        description: 'Sets the x coordinates.'
+    x: scatterAttrs.x,
+    y: scatterAttrs.y,
+    text: extendFlat({}, scatterAttrs.text, {
+        description: [
+            'Sets text elements associated with each (x,y) pair to appear on hover.',
+            'If a single string, the same string appears over',
+            'all the data points.',
+            'If an array of string, the items are mapped in order to the',
+            'this trace\'s (x,y) coordinates.'
+        ].join(' ')
+    }),
+    mode: {
+        valType: 'flaglist',
+        flags: ['lines', 'markers'],
+        extras: ['none'],
+        role: 'info',
+        description: [
+            'Determines the drawing mode for this scatter trace.'
+        ].join(' ')
     },
-    y: {
-        valType: 'data_array',
-        description: 'Sets the y coordinates.'
-    },
-    text: {
-        valType: 'data_array',
-        description: 'Sets text elements associated with each (x,y,z) triplet.'
-    },
-    mode: extendFlat(scatterAttrs.mode,
-        {dflt: 'lines+markers'}),
     line: {
         color: scatterLineAttrs.color,
         width: scatterLineAttrs.width,
-        dash: scatterLineAttrs.dash
+        dash: {
+            valType: 'enumerated',
+            values: Object.keys(require('../lib/dashes.json')),
+            dftl: 'solid',
+            role: 'style',
+            description: 'Sets the style of the lines.'
+        }
     },
     marker: {
         color: scatterMarkerAttrs.color,
-        symbol: scatterMarkerAttrs.symbol,
-        size: extendFlat({}, scatterMarkerAttrs.size, {dflt: 8}),
+        symbol: {
+            valType: 'enumerated',
+            values: Object.keys(require('../../gl3d/lib/markers.json')),
+            dflt: 'circle',
+            arrayOk: true,
+            role: 'style',
+            description: 'Sets the marker symbol type.'
+        },
+        size: scatterMarkerAttrs.size,
         sizeref: scatterMarkerAttrs.sizeref,
         sizemin: scatterMarkerAttrs.sizemin,
         sizemode: scatterMarkerAttrs.sizemode,
@@ -54,10 +73,10 @@ module.exports = {
             reversescale: scatterMarkerLineAttrs.reversescale
         }
     },
-    fill: scatterAttrs.fill,
+    fill: extendFlat({}, scatterAttrs.fill, {
+        values: ['none', 'tozeroy', 'tozerox']
+    }),
     fillcolor: scatterAttrs.fillcolor,
-    textposition: extendFlat({}, scatterAttrs.textposition, {dflt: 'top center'}),
-    textfont: scatterAttrs.textfont,
     _nestedModules: {
         'error_x': 'ErrorBars',
         'error_y': 'ErrorBars',
