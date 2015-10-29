6c32645f817410402ca454337d04c95aee79167b
etpinard <etienne.t.pinard@gmail.com>
2015-10-28 23:23:40 -0400

    expose getDelay and getRedrawFunc in Plotly.Snapshot,
    - share them in snapshot routine and image server main.js

--- shelly/plotlyjs/static/plotlyjs/src/snapshot/snapshot.js
+++ shelly/plotlyjs/static/plotlyjs/src/snapshot/snapshot.js
@@ -1,4 +1,31 @@
+'use strict';
+
+var Plotly = require('../plotly');
+
+function getDelay(fullLayout) {
+    return (fullLayout._hasGL3D || fullLayout._hasGL2D) ? 500 : 0;
+}
+
+function getRedrawFunc(gd) {
+    return function() {
+        var fullLayout = gd._fullLayout;
+
+        // doesn't work presently (and not needed) for polar or 3d
+        if(fullLayout._hasGL3D || fullLayout._hasGL2D || fullLayout._hasPolar) {
+            return;
+        }
+
+        Plotly.Annotations.drawAll(gd);
+        Plotly.Legend.draw(gd, fullLayout.showlegend);
+        (gd.calcdata || []).forEach(function(d) {
+            if(d[0] && d[0].t && d[0].t.cb) d[0].t.cb();
+        });
+    };
+}
+
 var Snapshot = {
+    getDelay: getDelay,
+    getRedrawFunc: getRedrawFunc,
     clone: require('./cloneplot'),
     toSVG: require('./tosvg'),
     svgToImg: require('./svgtoimg'),
