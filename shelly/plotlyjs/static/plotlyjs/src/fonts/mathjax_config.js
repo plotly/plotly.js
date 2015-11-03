f7be0223c65a66289d066b1a197d505114ed258e
etpinard <etienne.t.pinard@gmail.com>
2015-11-02 21:35:20 -0500

    mv mathjax config to fonts/

---
+++ shelly/plotlyjs/static/plotlyjs/src/fonts/mathjax_config.js
@@ -0,0 +1,21 @@
+/* global MathJax:false */
+
+/**
+ * Check and configure MathJax
+ */
+if (typeof MathJax !== 'undefined'){
+    exports.MathJax = true;
+
+    MathJax.Hub.Config({
+        messageStyle: 'none',
+        skipStartupTypeset: true,
+        displayAlign: 'left',
+        tex2jax: {
+            inlineMath: [['$','$'],['\\(','\\)']]
+        }
+    });
+
+    MathJax.Hub.Configured();
+} else {
+    exports.MathJax = false;
+}
