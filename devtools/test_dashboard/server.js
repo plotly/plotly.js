eeb1b26063ed4ad74912e05b15dccfb1a318757e
etpinard <etienne.t.pinard@gmail.com>
2015-11-10 13:51:37 -0500

    update test_dashboard server file (re-use watch_plotly.js!)

--- devtools/test_dashboard/server.js
+++ devtools/test_dashboard/server.js
@@ -1,89 +1,53 @@
-var http = require('http');
-var ecstatic = require('ecstatic');
-var browserify = require('browserify');
-var open = require('open');
 var fs = require('fs');
-var watchify = require('watchify');
+var http = require('http');
 var path = require('path');
-var outpipe = require('outpipe');
-var outfile = path.join(__dirname, '../shelly/plotlyjs/static/plotlyjs/build/plotlyjs-bundle.js');
 
-var testFile = './test';
+var browserify = require('browserify');
+var ecstatic = require('ecstatic');
+var _open = require('open');
 
+var makeWatchifiedBundle = require('../../tasks/watch_plotly');
+var shortcutPaths = require('../../tasks/util/shortcut_paths');
+var constants = require('../../tasks/util/constants');
+
+
+// TODO make this an optional argument
+var PORT = '8080';
+
+var testFile;
 switch(process.argv[2]) {
-  case 'geo':
-    testFile = './test-geo';
-  break;
-  case '2d':
-    testFile = './test-2d';
-  break;
+    case 'geo':
+        testFile = './test-geo';
+    break;
+    case '2d':
+        testFile = './test-2d';
+    break;
+    default:
+        testFile = './test-3d';
 }
 
-console.log('using ' + testFile);
+console.log('Using ' + testFile);
+console.log('Listening on :' + PORT + '\n');
 
-var b = browserify(path.join(__dirname, '../shelly/plotlyjs/static/plotlyjs/src/plotly.js'), {
-  debug: true,
-  verbose: true,
-  standalone: 'Plotly',
-  transform: path.join(__dirname, '../shelly/plotlyjs/static/plotlyjs/compress_attributes.js'),
-  cache: {},
-  packageCache: {}
+// watch plotly.js
+var watchifiedBundle = makeWatchifiedBundle(function onFirstBundleCallback() {
+    _open('http://localhost:' + PORT + '/devtools/test_dashboard');
 });
+watchifiedBundle();
 
-
-var w = watchify(b);
-
-var bytes, time;
-w.on('bytes', function (b) { bytes = b });
-w.on('time', function (t) { time = t });
-
-w.on('update', bundle);
-bundle();
-
-var firstBundle = true;
-
-function bundle () {
-    var didError = false;
-    var outStream = process.platform === 'win32'
-        ? fs.createWriteStream(outfile)
-        : outpipe(outfile);
-
-    var wb = w.bundle();
-    wb.on('error', function (err) {
-        console.error(String(err));
-        didError = true;
-        outStream.end('console.error('+JSON.stringify(String(err))+');');
-    });
-    wb.pipe(outStream);
-
-    outStream.on('error', function (err) {
-        console.error(err);
-    });
-    outStream.on('close', function () {
-        if (!didError) {
-            console.error(bytes + ' bytes written to ' + outfile
-                + ' (' + (time / 1000).toFixed(2) + ' seconds)'
-            );
-            if(firstBundle) {
-              open('http://localhost:8080/test-dashboard');
-              firstBundle = false;
-            }
-        }
-    });
-}
-
-////// build the test examples
-
-fs.unlink('./test-bundle.js', function(error) {
-    browserify({
+// build the test examples
+fs.unlink(constants.pathToTestDashboardBundle, function() {
+    browserify(path.join(__dirname, testFile), {
         debug: true,
-        verbose: true
-    }).add(testFile).bundle()
-        .pipe(fs.createWriteStream('test-bundle.js'));
+        transform: [shortcutPaths]
+    })
+    .bundle(function(err) {
+        if(err) throw err;
+    })
+    .pipe(fs.createWriteStream(constants.pathToTestDashboardBundle));
 });
 
+// boot up server
 http.createServer(
-  ecstatic({ root: '../.'  })
-).listen(8080);
-
-console.log('Listening on :8080');
+    ecstatic({ root: constants.pathToRoot })
+).listen(PORT);
