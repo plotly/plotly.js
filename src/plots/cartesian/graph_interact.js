769f6dd5d6d99c9fc52d3d14487a0e625f9390f4
etpinard <etienne.t.pinard@gmail.com>
2015-11-11 15:40:22 -0500

    put graph_interact in plots/cartesian

--- src/plots/cartesian/graph_interact.js
+++ src/plots/cartesian/graph_interact.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var Plotly = require('./plotly');
+var Plotly = require('../../plotly');
 var d3 = require('d3');
 var tinycolor = require('tinycolor2');
 var isNumeric = require('fast-isnumeric');
-var Events = require('./events');
+var Events = require('../../lib/events');
 
 var fx = module.exports = {};
 
