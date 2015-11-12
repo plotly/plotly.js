d3b9f1fdd5ad232f3b5c23febf2ab3b6787684cb
etpinard <etienne.t.pinard@gmail.com>
2015-11-12 18:01:05 -0500

    rm un-finished scripts (for now)

--- tasks/test.js
+++
@@ -1,6 +0,0 @@
-// npm test : run all test
-// npm test-unit : run the unit tests
-// npm test-image : run the image comparison tests
-// npm test-image {image-name} : run image comparison test for {image-name}
-//
-// npm test
