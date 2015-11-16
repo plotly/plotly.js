93d1be66c8f734453577a265f7731e192afd3898
etpinard <etienne.t.pinard@gmail.com>
2015-11-16 15:39:32 -0500

    add make baseline script

--- test/image/make_baseline.js
+++ test/image/make_baseline.js
@@ -1,35 +1,34 @@
-'use strict';
-
-var request = require('request');
-var getOptions = require('./get-options');
 var fs = require('fs');
 var path = require('path');
-var ProgressBar = require('progress');
-var bar;
 
-var root = __dirname;
+var constants = require('../../tasks/util/constants');
+var getOptions = require('../../tasks/util/get_image_request_options');
+
+// packages inside the image server docker
+var ProgressBar = require('progress');
+var request = require('request');
 
 var userFileName = process.argv[2];
+var bar;
 
 
 if (!userFileName) {
-    fs.readdir(root + '/../mocks', function (err, files) {
+    fs.readdir(constants.pathToTestImageMocks, function (err, files) {
         console.log('#######  ' + files.length + ' total baseline images to build  #######');
         bar = new ProgressBar('processing [:bar] [:current / :total]', { total: files.length, width: 30 });
         if (err) return console.log(err);
         files.forEach(createBaselineImage);
     });
-} else {
+}
+else {
     createBaselineImage(userFileName);
 }
-
-
 
 
 function createBaselineImage (fileName) {
     if (path.extname(fileName) !== '.json') return;
 
-    var figure = require('../mocks/' + fileName);
+    var figure = require(path.join(constants.pathToTestImageMocks, fileName));
     var bodyMock = {
         figure: figure,
         format: 'png',
@@ -37,7 +36,7 @@ function createBaselineImage (fileName) {
     };
 
     var imageFileName = fileName.split('.')[0] + '.png';
-    var savedImagePath = root + '/../test-images-baseline/' + imageFileName;
+    var savedImagePath = path.join(constants.pathToTestImageBaselines, imageFileName);
     var savedImageStream = fs.createWriteStream(savedImagePath);
     var options = getOptions(bodyMock, 'http://localhost:9010/');
 
