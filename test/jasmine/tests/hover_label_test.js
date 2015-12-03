8e363e5b2ea73565533afd835b87b22f72d76224
Chelsea <cldougl@gmail.com>
2015-12-03 16:57:30 -0500

    more hover tests

--- test/jasmine/tests/hover_label_test.js
+++ test/jasmine/tests/hover_label_test.js
@@ -9,13 +9,13 @@ var destroyGraphDiv = require('../assets/destroy_graph_div');
 
 describe('hover info', function() {
     'use strict';
-    
+
     var mock = require('@mocks/14.json'),
         evt = {
                 clientX: mock.layout.width/ 2,
                 clientY: mock.layout.height / 2
         };
-    
+
     afterEach(destroyGraphDiv);
 
     describe('hover info', function() {
@@ -23,14 +23,14 @@ describe('hover info', function() {
 
         beforeEach(function(done) {
             Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
-        }); 
+        });
 
         it('responds to hover', function() {
             var gd = document.getElementById('graph');
             Fx.hover('graph', evt, 'xy');
-            
+
             var hoverTrace = gd._hoverdata[0];
-            
+
             expect(hoverTrace.curveNumber).toEqual(0);
             expect(hoverTrace.pointNumber).toEqual(17);
             expect(hoverTrace.x).toEqual(0.388);
@@ -50,14 +50,14 @@ describe('hover info', function() {
 
         beforeEach(function(done) {
             Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
-        }); 
+        });
 
         it('responds to hover x', function() {
             var gd = document.getElementById('graph');
             Fx.hover('graph', evt, 'xy');
-            
+
             var hoverTrace = gd._hoverdata[0];
-            
+
             expect(hoverTrace.curveNumber).toEqual(0);
             expect(hoverTrace.pointNumber).toEqual(17);
             expect(hoverTrace.x).toEqual(0.388);
@@ -76,14 +76,14 @@ describe('hover info', function() {
 
         beforeEach(function(done) {
             Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
-        }); 
+        });
 
         it('responds to hover y', function() {
             var gd = document.getElementById('graph');
             Fx.hover('graph', evt, 'xy');
-            
+
             var hoverTrace = gd._hoverdata[0];
-            
+
             expect(hoverTrace.curveNumber).toEqual(0);
             expect(hoverTrace.pointNumber).toEqual(17);
             expect(hoverTrace.x).toEqual(0.388);
@@ -92,6 +92,148 @@ describe('hover info', function() {
             expect(d3.selectAll('g.axistext').size()).toEqual(0);
             expect(d3.selectAll('g.hovertext').size()).toEqual(1);
             expect(d3.selectAll('g.hovertext').select('text').html()).toEqual('1');
+        });
+    });
+
+    describe('hover info text', function() {
+        var mockCopy = Lib.extendDeep({}, mock);
+
+        mockCopy.data[0].text = []
+        mockCopy.data[0].text[17] = 'hover text'
+        mockCopy.data[0].hoverinfo = 'text';
+
+        beforeEach(function(done) {
+            Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
+        });
+
+        it('responds to hover text', function() {
+            var gd = document.getElementById('graph');
+            Fx.hover('graph', evt, 'xy');
+
+            var hoverTrace = gd._hoverdata[0];
+
+            expect(hoverTrace.curveNumber).toEqual(0);
+            expect(hoverTrace.pointNumber).toEqual(17);
+            expect(hoverTrace.x).toEqual(0.388);
+            expect(hoverTrace.y).toEqual(1);
+
+            expect(d3.selectAll('g.axistext').size()).toEqual(0);
+            expect(d3.selectAll('g.hovertext').size()).toEqual(1);
+            expect(d3.selectAll('g.hovertext').select('text').html()).toEqual('hover text');
+        });
+    });
+
+    describe('hover info all', function() {
+        var mockCopy = Lib.extendDeep({}, mock);
+
+        mockCopy.data[0].text = []
+        mockCopy.data[0].text[17] = 'hover text'
+        mockCopy.data[0].hoverinfo = 'all';
+
+        beforeEach(function(done) {
+            Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
+        });
+
+        it('responds to hover all', function() {
+            var gd = document.getElementById('graph');
+            Fx.hover('graph', evt, 'xy');
+
+            var hoverTrace = gd._hoverdata[0];
+
+            expect(hoverTrace.curveNumber).toEqual(0);
+            expect(hoverTrace.pointNumber).toEqual(17);
+            expect(hoverTrace.x).toEqual(0.388);
+            expect(hoverTrace.y).toEqual(1);
+
+            expect(d3.selectAll('g.axistext').size()).toEqual(1);
+            expect(d3.selectAll('g.hovertext').size()).toEqual(1);
+            expect(d3.selectAll('g.axistext').select('text').html()).toEqual('0.388');
+            expect(d3.selectAll('g.hovertext').select('text').html()).toEqual('<tspan class="line" dy="0em" x="9" y="-3.9453125">1</tspan><tspan class="line" dy="1.3em" x="9" y="-3.9453125">hover text</tspan>');
+        });
+    });
+
+    describe('hover info y+text', function() {
+        var mockCopy = Lib.extendDeep({}, mock);
+
+        mockCopy.data[0].text = []
+        mockCopy.data[0].text[17] = 'hover text'
+        mockCopy.data[0].hoverinfo = 'y+text';
+
+        beforeEach(function(done) {
+            Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
+        });
+
+        it('responds to hover y+text', function() {
+            var gd = document.getElementById('graph');
+            Fx.hover('graph', evt, 'xy');
+
+            var hoverTrace = gd._hoverdata[0];
+
+            expect(hoverTrace.curveNumber).toEqual(0);
+            expect(hoverTrace.pointNumber).toEqual(17);
+            expect(hoverTrace.x).toEqual(0.388);
+            expect(hoverTrace.y).toEqual(1);
+
+            expect(d3.selectAll('g.axistext').size()).toEqual(0);
+            expect(d3.selectAll('g.hovertext').size()).toEqual(1);
+            expect(d3.selectAll('g.hovertext').select('text').html()).toEqual('<tspan class="line" dy="0em" x="9" y="-3.9453125">1</tspan><tspan class="line" dy="1.3em" x="9" y="-3.9453125">hover text</tspan>');
+        });
+    });
+
+    describe('hover info x+text', function() {
+        var mockCopy = Lib.extendDeep({}, mock);
+
+        mockCopy.data[0].text = []
+        mockCopy.data[0].text[17] = 'hover text'
+        mockCopy.data[0].hoverinfo = 'x+text';
+
+        beforeEach(function(done) {
+            Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
+        });
+
+        it('responds to hover x+text', function() {
+            var gd = document.getElementById('graph');
+            Fx.hover('graph', evt, 'xy');
+
+            var hoverTrace = gd._hoverdata[0];
+
+            expect(hoverTrace.curveNumber).toEqual(0);
+            expect(hoverTrace.pointNumber).toEqual(17);
+            expect(hoverTrace.x).toEqual(0.388);
+            expect(hoverTrace.y).toEqual(1);
+
+            expect(d3.selectAll('g.axistext').size()).toEqual(1);
+            expect(d3.selectAll('g.hovertext').size()).toEqual(1);
+            expect(d3.selectAll('g.axistext').select('text').html()).toEqual('0.388');
+            expect(d3.selectAll('g.hovertext').select('text').html()).toEqual('hover text');
+        });
+    });
+
+    describe('hover info text with html', function() {
+        var mockCopy = Lib.extendDeep({}, mock);
+
+        mockCopy.data[0].text = []
+        mockCopy.data[0].text[17] = 'hover<br>text'
+        mockCopy.data[0].hoverinfo = 'text';
+
+        beforeEach(function(done) {
+            Plotly.plot(createGraphDiv(), mockCopy.data, mockCopy.layout).then(done);
+        });
+
+        it('responds to hover text with html', function() {
+            var gd = document.getElementById('graph');
+            Fx.hover('graph', evt, 'xy');
+
+            var hoverTrace = gd._hoverdata[0];
+
+            expect(hoverTrace.curveNumber).toEqual(0);
+            expect(hoverTrace.pointNumber).toEqual(17);
+            expect(hoverTrace.x).toEqual(0.388);
+            expect(hoverTrace.y).toEqual(1);
+
+            expect(d3.selectAll('g.axistext').size()).toEqual(0);
+            expect(d3.selectAll('g.hovertext').size()).toEqual(1);
+            expect(d3.selectAll('g.hovertext').select('text').html()).toEqual('<tspan class="line" dy="0em" x="9" y="-3.9453125">hover</tspan><tspan class="line" dy="1.3em" x="9" y="-3.9453125">text</tspan>');
         });
     });
 });
