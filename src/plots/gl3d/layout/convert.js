dcbad32a4f1465014ebf9d2da90f38e3171f0437
Chelsea <cldougl@gmail.com>
2015-12-15 18:44:20 -0500

    rm extended spacing pattern

--- src/plots/gl3d/layout/convert.js
+++ src/plots/gl3d/layout/convert.js
@@ -18,43 +18,43 @@ var arrayCopy1D = arrtools.copy1D;
 var AXES_NAMES = ['xaxis', 'yaxis', 'zaxis'];
 
 function AxesOptions() {
-    this.bounds         = [ [-10, -10, -10],
-                            [ 10,  10,  10] ];
+    this.bounds = [ [-10, -10, -10],
+                    [ 10,  10,  10] ];
 
-    this.ticks          = [ [], [], [] ];
-    this.tickEnable     = [ true, true, true ];
-    this.tickFont       = [ 'sans-serif', 'sans-serif', 'sans-serif' ];
-    this.tickSize       = [ 12, 12, 12 ];
-    this.tickAngle      = [ 0, 0, 0 ];
-    this.tickColor      = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
-    this.tickPad        = [ 18, 18, 18 ];
+    this.ticks = [ [], [], [] ];
+    this.tickEnable = [ true, true, true ];
+    this.tickFont = [ 'sans-serif', 'sans-serif', 'sans-serif' ];
+    this.tickSize = [ 12, 12, 12 ];
+    this.tickAngle = [ 0, 0, 0 ];
+    this.tickColor = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
+    this.tickPad = [ 18, 18, 18 ];
 
-    this.labels         = [ 'x', 'y', 'z' ];
-    this.labelEnable    = [ true, true, true ];
-    this.labelFont      = ['Open Sans','Open Sans','Open Sans'];
-    this.labelSize      = [ 20, 20, 20 ];
-    this.labelAngle     = [ 0, 0, 0 ];
-    this.labelColor     = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
-    this.labelPad       = [ 30, 30, 30 ];
+    this.labels = [ 'x', 'y', 'z' ];
+    this.labelEnable = [ true, true, true ];
+    this.labelFont = ['Open Sans','Open Sans','Open Sans'];
+    this.labelSize = [ 20, 20, 20 ];
+    this.labelAngle = [ 0, 0, 0 ];
+    this.labelColor = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
+    this.labelPad = [ 30, 30, 30 ];
 
-    this.lineEnable     = [ true, true, true ];
-    this.lineMirror     = [ false, false, false ];
-    this.lineWidth      = [ 1, 1, 1 ];
-    this.lineColor      = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
+    this.lineEnable = [ true, true, true ];
+    this.lineMirror = [ false, false, false ];
+    this.lineWidth = [ 1, 1, 1 ];
+    this.lineColor = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
 
     this.lineTickEnable = [ true, true, true ];
     this.lineTickMirror = [ false, false, false ];
     this.lineTickLength = [ 10, 10, 10 ];
-    this.lineTickWidth  = [ 1, 1, 1 ];
-    this.lineTickColor  = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
+    this.lineTickWidth = [ 1, 1, 1 ];
+    this.lineTickColor = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
 
-    this.gridEnable     = [ true, true, true ];
-    this.gridWidth      = [ 1, 1, 1 ];
-    this.gridColor      = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
+    this.gridEnable = [ true, true, true ];
+    this.gridWidth = [ 1, 1, 1 ];
+    this.gridColor = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
 
-    this.zeroEnable     = [ true, true, true ];
-    this.zeroLineColor  = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
-    this.zeroLineWidth  = [ 2, 2, 2 ];
+    this.zeroEnable = [ true, true, true ];
+    this.zeroLineColor = [ [0,0,0,1], [0,0,0,1], [0,0,0,1] ];
+    this.zeroLineWidth = [ 2, 2, 2 ];
 
     this.backgroundEnable = [ true, true, true ];
     this.backgroundColor  = [ [0.8, 0.8, 0.8, 0.5],
@@ -62,9 +62,9 @@ function AxesOptions() {
                               [0.8, 0.8, 0.8, 0.5] ];
 
     // some default values are stored for applying model transforms
-    this._defaultTickPad         = arrayCopy1D(this.tickPad);
-    this._defaultLabelPad        = arrayCopy1D(this.labelPad);
-    this._defaultLineTickLength  = arrayCopy1D(this.lineTickLength);
+    this._defaultTickPad = arrayCopy1D(this.tickPad);
+    this._defaultLabelPad = arrayCopy1D(this.labelPad);
+    this._defaultLineTickLength = arrayCopy1D(this.lineTickLength);
 }
 
 var proto = AxesOptions.prototype;
@@ -78,22 +78,23 @@ proto.merge = function(sceneLayout) {
         opts.labels[i] = convertHTML(axes.title);
         if ('titlefont' in axes) {
             if (axes.titlefont.color)  opts.labelColor[i] = str2RgbaArray(axes.titlefont.color);
-            if (axes.titlefont.family) opts.labelFont[i]  = axes.titlefont.family;
-            if (axes.titlefont.size)   opts.labelSize[i]  = axes.titlefont.size;
+            if (axes.titlefont.family) opts.labelFont[i] = axes.titlefont.family;
+            if (axes.titlefont.size)   opts.labelSize[i] = axes.titlefont.size;
         }
 
         /////// LINES ////////
         if ('showline' in axes)  opts.lineEnable[i] = axes.showline;
-        if ('linecolor' in axes) opts.lineColor[i]  = str2RgbaArray(axes.linecolor);
-        if ('linewidth' in axes) opts.lineWidth[i]  = axes.linewidth;
+        if ('linecolor' in axes) opts.lineColor[i] = str2RgbaArray(axes.linecolor);
+        if ('linewidth' in axes) opts.lineWidth[i] = axes.linewidth;
 
         if ('showgrid' in axes)  opts.gridEnable[i] = axes.showgrid;
-        if ('gridcolor' in axes) opts.gridColor[i]  = str2RgbaArray(axes.gridcolor);
-        if ('gridwidth' in axes) opts.gridWidth[i]  = axes.gridwidth;
+        if ('gridcolor' in axes) opts.gridColor[i] = str2RgbaArray(axes.gridcolor);
+        if ('gridwidth' in axes) opts.gridWidth[i] = axes.gridwidth;
 
-        // Remove zeroline if axis type is log //
-        if (axes.type === 'log') opts.zeroEnable[i]        = false;
-        else if ('zeroline' in axes) opts.zeroEnable[i]    = axes.zeroline;
+        // Remove zeroline if axis type is log
+        // otherwise the zeroline is incorrectly drawn at 1 on log axes
+        if (axes.type === 'log') opts.zeroEnable[i] = false;
+        else if ('zeroline' in axes) opts.zeroEnable[i] = axes.zeroline;
         if ('zerolinecolor' in axes) opts.zeroLineColor[i] = str2RgbaArray(axes.zerolinecolor);
         if ('zerolinewidth' in axes) opts.zeroLineWidth[i] = axes.zerolinewidth;
 
@@ -113,9 +114,9 @@ proto.merge = function(sceneLayout) {
         //// tick labels
         if ('showticklabels' in axes) opts.tickEnable[i] = axes.showticklabels;
         if ('tickfont' in axes) {
-            if (axes.tickfont.color)  opts.tickColor[i]  = str2RgbaArray(axes.tickfont.color);
-            if (axes.tickfont.family) opts.tickFont[i]   = axes.tickfont.family;
-            if (axes.tickfont.size)   opts.tickSize[i]   = axes.tickfont.size;
+            if (axes.tickfont.color) opts.tickColor[i] = str2RgbaArray(axes.tickfont.color);
+            if (axes.tickfont.family) opts.tickFont[i] = axes.tickfont.family;
+            if (axes.tickfont.size) opts.tickSize[i] = axes.tickfont.size;
         }
 
         if ('mirror' in axes) {
@@ -133,8 +134,8 @@ proto.merge = function(sceneLayout) {
 
         ////// grid background
         if ('showbackground' in axes && axes.showbackground !== false) {
-            opts.backgroundEnable[i]    = true;
-            opts.backgroundColor[i]     = str2RgbaArray(axes.backgroundcolor);
+            opts.backgroundEnable[i] = true;
+            opts.backgroundColor[i] = str2RgbaArray(axes.backgroundcolor);
         } else opts.backgroundEnable[i] = false;
     }
 };
