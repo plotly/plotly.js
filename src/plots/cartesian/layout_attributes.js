851ad6330be8c6bc0006a13bd64a203bf6831213
Miklós Tusz <mdtusz@gmail.com>
2015-12-21 18:27:07 -0500

    remove numeric enums for exponentbase (all strings now)

--- src/plots/cartesian/layout_attributes.js
+++ src/plots/cartesian/layout_attributes.js
@@ -290,8 +290,8 @@ module.exports = {
     },
     exponentbase: {
         valType: 'enumerated',
-        values: [2, '2', 'e', 8, '8', 10, '10', 16, '16'],
-        dflt: 10,
+        values: ['2', 'e', '8', '10', '16'],
+        dflt: '10',
         coerceNumber: true,
         role: 'style',
         description: [
