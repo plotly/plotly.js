5b1bdc47b720f724f343eca812917980406e025c
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-01-19 17:46:02 -0500

    add xmlns namespaces constant file

---
+++ src/constants/xmlns_namespaces.js
@@ -0,0 +1,22 @@
+/**
+* Copyright 2012-2016, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+
+'use strict';
+
+
+exports.xmlns = 'http://www.w3.org/2000/xmlns/';
+exports.svg = 'http://www.w3.org/2000/svg';
+exports.xlink = 'http://www.w3.org/1999/xlink';
+
+// the 'old' d3 quirk got fix in v3.5.7
+// https://github.com/mbostock/d3/commit/a6f66e9dd37f764403fc7c1f26be09ab4af24fed
+exports.svgAttrs = {
+    xmlns: exports.svg,
+    'xmlns:xlink': exports.xlink
+};
