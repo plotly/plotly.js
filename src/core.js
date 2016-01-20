60793e79ebf4ccb7f00e0e9c57af62c87acd24bb
Miklós Tusz <mdtusz@gmail.com>
2016-01-20 14:22:56 -0500

    Expose trace modularity for users

---
+++ src/core.js
@@ -0,0 +1,45 @@
+/**
+* Copyright 2012-2016, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+'use strict';
+
+/*
+ * Export the plotly.js API methods.
+ */
+
+var Plotly = require('./plotly');
+
+// export the version found in the package.json
+exports.version = require('../package.json').version;
+
+// plot api
+exports.plot = Plotly.plot;
+exports.newPlot = Plotly.newPlot;
+exports.restyle = Plotly.restyle;
+exports.relayout = Plotly.relayout;
+exports.redraw = Plotly.redraw;
+exports.extendTraces = Plotly.extendTraces;
+exports.prependTraces = Plotly.prependTraces;
+exports.addTraces = Plotly.addTraces;
+exports.deleteTraces = Plotly.deleteTraces;
+exports.moveTraces = Plotly.moveTraces;
+exports.setPlotConfig = require('./plot_api/set_plot_config');
+exports.register = Plotly.register;
+
+// plot icons
+exports.Icons = require('../build/ploticon');
+
+// unofficial 'beta' plot methods, use at your own risk
+exports.Plots = Plotly.Plots;
+exports.Fx = Plotly.Fx;
+exports.Snapshot = Plotly.Snapshot;
+exports.PlotSchema = Plotly.PlotSchema;
+exports.Queue = Plotly.Queue;
+
+// export d3 used in the bundle
+exports.d3 = require('d3');
