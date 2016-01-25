c384cf4ba838be9db7e846ad9601736dd1fcb251
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-01-25 17:35:47 -0500

    add scattergeo and choropleth click and hover event tests

--- test/jasmine/tests/geo_interact_test.js
+++ test/jasmine/tests/geo_interact_test.js
@@ -14,14 +14,24 @@ describe('Test geo interactions', function() {
 
     describe('mock geo_first.json', function() {
         var mock = require('@mocks/geo_first.json');
+        var gd;
+
+        function mouseEventScatterGeo(type) {
+            mouseEvent(type, 300, 235);
+        }
+
+        function mouseEventChoropleth(type) {
+            mouseEvent(type, 400, 160);
+        }
 
         beforeEach(function(done) {
-            Plotly.plot(createGraphDiv(), mock.data, mock.layout).then(done);
+            gd = createGraphDiv();
+            Plotly.plot(gd, mock.data, mock.layout).then(done);
         });
 
-        describe('scattegeo hover labels', function() {
+        describe('scattergeo hover labels', function() {
             beforeEach(function() {
-                mouseEvent('mouseover', 300, 235);
+                mouseEventScatterGeo('mouseover');
             });
 
             it('should show one hover text group', function() {
@@ -41,9 +51,63 @@ describe('Test geo interactions', function() {
             });
         });
 
+        describe('scattergeo hover events', function() {
+            var ptData;
+
+            beforeEach(function() {
+                gd.on('plotly_hover', function(eventData) {
+                    ptData = eventData.points[0];
+                });
+
+                mouseEventScatterGeo('mouseover');
+            });
+
+            it('should contain the correct fields', function() {
+                expect(Object.keys(ptData)).toEqual([
+                    'data', 'fullData', 'curveNumber', 'pointNumber',
+                    'lon', 'lat', 'location'
+                ]);
+            });
+
+            it('should show the correct point data', function() {
+                expect(ptData.lon).toEqual(0);
+                expect(ptData.lat).toEqual(0);
+                expect(ptData.location).toBe(null);
+                expect(ptData.curveNumber).toEqual(0);
+                expect(ptData.pointNumber).toEqual(0);
+            });
+        });
+
+        describe('scattergeo click events', function() {
+            var ptData;
+
+            beforeEach(function() {
+                gd.on('plotly_click', function(eventData) {
+                    ptData = eventData.points[0];
+                });
+
+                mouseEventScatterGeo('click');
+            });
+
+            it('should contain the correct fields', function() {
+                expect(Object.keys(ptData)).toEqual([
+                    'data', 'fullData', 'curveNumber', 'pointNumber',
+                    'lon', 'lat', 'location'
+                ]);
+            });
+
+            it('should show the correct point data', function() {
+                expect(ptData.lon).toEqual(0);
+                expect(ptData.lat).toEqual(0);
+                expect(ptData.location).toBe(null);
+                expect(ptData.curveNumber).toEqual(0);
+                expect(ptData.pointNumber).toEqual(0);
+            });
+        });
+
         describe('choropleth hover labels', function() {
             beforeEach(function() {
-                mouseEvent('mouseover', 400, 160);
+                mouseEventChoropleth('mouseover');
             });
 
             it('should show one hover text group', function() {
@@ -61,6 +125,58 @@ describe('Test geo interactions', function() {
                 var node = d3.selectAll('g.hovertext').selectAll('text')[0][0];
 
                 expect(node.innerHTML).toEqual('trace 1');
+            });
+        });
+
+        describe('choropleth hover events', function() {
+            var ptData;
+
+            beforeEach(function() {
+                gd.on('plotly_hover', function(eventData) {
+                    ptData = eventData.points[0];
+                });
+
+                mouseEventChoropleth('mouseover');
+            });
+
+            it('should contain the correct fields', function() {
+                expect(Object.keys(ptData)).toEqual([
+                    'data', 'fullData', 'curveNumber', 'pointNumber',
+                    'location', 'z'
+                ]);
+            });
+
+            it('should show the correct point data', function() {
+                expect(ptData.location).toBe('RUS');
+                expect(ptData.z).toEqual(10);
+                expect(ptData.curveNumber).toEqual(1);
+                expect(ptData.pointNumber).toEqual(2);
+            });
+        });
+
+        describe('choropleth click events', function() {
+            var ptData;
+
+            beforeEach(function() {
+                gd.on('plotly_click', function(eventData) {
+                    ptData = eventData.points[0];
+                });
+
+                mouseEventChoropleth('click');
+            });
+
+            it('should contain the correct fields', function() {
+                expect(Object.keys(ptData)).toEqual([
+                    'data', 'fullData', 'curveNumber', 'pointNumber',
+                    'location', 'z'
+                ]);
+            });
+
+            it('should show the correct point data', function() {
+                expect(ptData.location).toBe('RUS');
+                expect(ptData.z).toEqual(10);
+                expect(ptData.curveNumber).toEqual(1);
+                expect(ptData.pointNumber).toEqual(2);
             });
         });
 
