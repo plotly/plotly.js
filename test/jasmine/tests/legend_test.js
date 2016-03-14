b3af44533c503b84b503dbf5e207824ba29121ef
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-03-14 14:53:59 -0400

    move auto-anchor logic to own module + add tests

--- test/jasmine/tests/legend_test.js
+++ test/jasmine/tests/legend_test.js
@@ -2,6 +2,7 @@ var Legend = require('@src/components/legend');
 var Plots = require('@src/plots/plots');
 
 var helpers = require('@src/components/legend/helpers');
+var anchorUtils = require('@src/components/legend/anchor_utils');
 describe('Test legend:', function() {
     'use strict';
 
@@ -358,6 +359,92 @@ describe('Test legend:', function() {
             expect(isReversed({ traceorder: 'reversed+grouped' })).toBe(true);
             expect(isReversed({ traceorder: 'grouped+reversed' })).toBe(true);
             expect(isReversed({ traceorder: 'reversed' })).toBe(true);
+        });
+    });
+
+    describe('isRightAnchor anchor util', function() {
+        var isRightAnchor = anchorUtils.isRightAnchor;
+        var threshold = 2/3;
+
+        it('should return true when \'xanchor\' is set to \'right\'', function() {
+            expect(isRightAnchor({ xanchor: 'left' })).toBe(false);
+            expect(isRightAnchor({ xanchor: 'center' })).toBe(false);
+            expect(isRightAnchor({ xanchor: 'right' })).toBe(true);
+        });
+
+        it('should return true when \'xanchor\' is set to \'auto\' and \'x\' >= 2/3', function() {
+            var opts = { xanchor: 'auto' };
+
+            [0, 0.4, 0.7, 1].forEach(function(v) {
+                opts.x = v;
+                expect(isRightAnchor(opts))
+                    .toBe(v > threshold, 'case ' + v);
+            });
+        });
+    });
+
+    describe('isCenterAnchor anchor util', function() {
+        var isCenterAnchor = anchorUtils.isCenterAnchor;
+        var threshold0 = 1/3;
+        var threshold1 = 2/3;
+
+        it('should return true when \'xanchor\' is set to \'center\'', function() {
+            expect(isCenterAnchor({ xanchor: 'left' })).toBe(false);
+            expect(isCenterAnchor({ xanchor: 'center' })).toBe(true);
+            expect(isCenterAnchor({ xanchor: 'right' })).toBe(false);
+        });
+
+        it('should return true when \'xanchor\' is set to \'auto\' and 1/3 < \'x\' < 2/3', function() {
+            var opts = { xanchor: 'auto' };
+
+            [0, 0.4, 0.7, 1].forEach(function(v) {
+                opts.x = v;
+                expect(isCenterAnchor(opts))
+                    .toBe(v > threshold0 && v < threshold1, 'case ' + v);
+            });
+        });
+    });
+
+    describe('isBottomAnchor anchor util', function() {
+        var isBottomAnchor = anchorUtils.isBottomAnchor;
+        var threshold = 1/3;
+
+        it('should return true when \'yanchor\' is set to \'right\'', function() {
+            expect(isBottomAnchor({ yanchor: 'top' })).toBe(false);
+            expect(isBottomAnchor({ yanchor: 'middle' })).toBe(false);
+            expect(isBottomAnchor({ yanchor: 'bottom' })).toBe(true);
+        });
+
+        it('should return true when \'yanchor\' is set to \'auto\' and \'y\' <= 1/3', function() {
+            var opts = { yanchor: 'auto' };
+
+            [0, 0.4, 0.7, 1].forEach(function(v) {
+                opts.y = v;
+                expect(isBottomAnchor(opts))
+                    .toBe(v < threshold, 'case ' + v);
+            });
+        });
+    });
+
+    describe('isMiddleAnchor anchor util', function() {
+        var isMiddleAnchor = anchorUtils.isMiddleAnchor;
+        var threshold0 = 1/3;
+        var threshold1 = 2/3;
+
+        it('should return true when \'yanchor\' is set to \'center\'', function() {
+            expect(isMiddleAnchor({ yanchor: 'top' })).toBe(false);
+            expect(isMiddleAnchor({ yanchor: 'middle' })).toBe(true);
+            expect(isMiddleAnchor({ yanchor: 'bottom' })).toBe(false);
+        });
+
+        it('should return true when \'yanchor\' is set to \'auto\' and 1/3 < \'y\' < 2/3', function() {
+            var opts = { yanchor: 'auto' };
+
+            [0, 0.4, 0.7, 1].forEach(function(v) {
+                opts.y = v;
+                expect(isMiddleAnchor(opts))
+                    .toBe(v > threshold0 && v < threshold1, 'case ' + v);
+            });
         });
     });
 
