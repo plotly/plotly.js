6611d1ea3750a52d83121c38e3e9d801c4a8488f
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-03-30 15:23:00 -0400

    add makeColorMap tests

--- test/jasmine/tests/contour_test.js
+++ test/jasmine/tests/contour_test.js
@@ -1,5 +1,7 @@
 var Plots = require('@src/plots/plots');
 var Contour = require('@src/traces/contour');
+var makeColorMap = require('@src/traces/contour/make_color_map');
+var colorScales = require('@src/components/colorscale/scales');
 
 
 describe('Test contour', function() {
@@ -45,6 +47,89 @@ describe('Test contour', function() {
             };
             supplyDefaults(traceIn, traceOut, defaultColor, layout);
             expect(traceOut.autocontour).toBe(true);
+        });
+    });
+
+    describe('makeColorMap', function() {
+        it('should make correct color map function (\'fill\' coloring case)', function() {
+            var trace = {
+                contours: {
+                    coloring: 'fill',
+                    start: -1.5,
+                    size: 0.5,
+                    end: 2.005
+                },
+                colorscale: [[
+                    0, 'rgb(12,51,131)'
+                ], [
+                    0.25, 'rgb(10,136,186)'
+                ], [
+                    0.5, 'rgb(242,211,56)'
+                ], [
+                    0.75, 'rgb(242,143,56)'
+                ], [
+                    1, 'rgb(217,30,30)'
+                ]]
+            };
+
+            var colorMap = makeColorMap(trace);
+
+            expect(colorMap.domain()).toEqual(
+                [-1.75, -0.75, 0.25, 1.25, 2.25]
+            );
+
+            expect(colorMap.range()).toEqual([
+                'rgb(12,51,131)', 'rgb(10,136,186)', 'rgb(242,211,56)',
+                'rgb(242,143,56)', 'rgb(217,30,30)'
+            ]);
+        });
+
+        it('should make correct color map function (\'heatmap\' coloring case)', function() {
+            var trace = {
+                contours: {
+                    coloring: 'heatmap',
+                    start: 1.5,
+                    size: 0.5,
+                    end: 5.505
+                },
+                colorscale: colorScales.RdBu,
+                zmin: 1,
+                zmax: 6
+            };
+
+            var colorMap = makeColorMap(trace);
+
+            expect(colorMap.domain()).toEqual(
+               [1, 2.75, 3.5, 4, 4.5, 6]
+            );
+
+            expect(colorMap.range()).toEqual([
+                'rgb(5,10,172)', 'rgb(106,137,247)', 'rgb(190,190,190)',
+                'rgb(220,170,132)', 'rgb(230,145,90)', 'rgb(178,10,28)'
+            ]);
+        });
+
+        it('should make correct color map function (\'lines\' coloring case)', function() {
+            var trace = {
+                contours: {
+                    coloring: 'lines',
+                    start: 1.5,
+                    size: 0.5,
+                    end: 5.505
+                },
+                colorscale: colorScales.RdBu
+            };
+
+            var colorMap = makeColorMap(trace);
+
+            expect(colorMap.domain()).toEqual(
+                [1.5, 2.9, 3.5, 3.9, 4.3, 5.5]
+            );
+
+            expect(colorMap.range()).toEqual([
+                'rgb(5,10,172)', 'rgb(106,137,247)', 'rgb(190,190,190)',
+                'rgb(220,170,132)', 'rgb(230,145,90)', 'rgb(178,10,28)'
+            ]);
         });
     });
 });
