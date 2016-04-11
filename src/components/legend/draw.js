cedabd385c8043449e33d45504983dba4d87a87a
Nicolas Riesco <enquiries@nicolasriesco.net>
2016-04-11 12:00:16 +0100

    Do not round off the legend location
    * Commented out the statements to round off the legend location, in
      order to reproduce the baseline images.

--- src/components/legend/draw.js
+++ src/components/legend/draw.js
@@ -195,8 +195,8 @@ module.exports = function draw(gd) {
         ly -= opts.height / 2;
     }
 
-    lx = Math.round(lx);
-    ly = Math.round(ly);
+    //lx = Math.round(lx);
+    //ly = Math.round(ly);
 
     // Make sure the legend top and bottom are visible
     // (legends with a scroll bar are not allowed to stretch beyond the extended
