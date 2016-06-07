49d9c7e09d660dd2bba00ed52bbdcc2be2e10bd7
MFed <dev.Federici.Michael@dev.bwater.com>
2016-06-06 23:59:10 -0400

    Trying to fix tests for absolutetail.

--- test/jasmine/tests/annotations_test.js
+++ test/jasmine/tests/annotations_test.js
@@ -1,6 +1,4 @@
-var Plots = require('@src/plots/plots');
 var Annotations = require('@src/components/annotations');
-
 
 describe('Test annotations', function() {
     'use strict';
@@ -8,7 +6,7 @@ describe('Test annotations', function() {
     describe('supplyLayoutDefaults', function() {
         it('should default to not use absolute arrow tail', function() {
             var annotationDefaults = {};
-            annotationDefaults._has = Plots._hasPlotType.bind(annotationDefaults);
+            annotationDefaults._has = function() { return false };
 
             Annotations.supplyLayoutDefaults({ annotations: [{ showarrow: true, arrowhead: 2}] }, annotationDefaults);
 
@@ -17,7 +15,7 @@ describe('Test annotations', function() {
 
         it('should convert ax/ay date coordinates to milliseconds if absolutetail is true', function() {
             var annotationOut = { xaxis: { type: 'date', range: ['2000-01-01', '2016-01-01'] }};
-            annotationOut._has = Plots._hasPlotType.bind(annotationOut);
+            annotationOut._has = function() { return false };
 
             var annotationIn = {
                 annotations: [{ showarrow: true, absolutetail: true, x: '2008-07-01', ax: '2004-07-01', y: 0, ay: 50}]
