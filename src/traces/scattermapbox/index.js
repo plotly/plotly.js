39684178450ca1a25671bd2d24b0f0ad82e8eb94
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-06-10 15:29:33 -0400

    introducting the 'scattermapbox' trace module

---
+++ src/traces/scattermapbox/index.js
@@ -0,0 +1,34 @@
+/**
+* Copyright 2012-2016, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+'use strict';
+
+
+var ScatterMapbox = {};
+
+ScatterMapbox.attributes = require('./attributes');
+ScatterMapbox.supplyDefaults = require('./defaults');
+ScatterMapbox.colorbar = require('../scatter/colorbar');
+ScatterMapbox.calc = require('./calc');
+ScatterMapbox.hoverPoints = require('./hover');
+ScatterMapbox.plot = require('./plot');
+
+ScatterMapbox.moduleType = 'trace';
+ScatterMapbox.name = 'scattermapbox';
+ScatterMapbox.basePlotModule = require('../../plots/mapbox');
+ScatterMapbox.categories = ['mapbox', 'gl', 'symbols', 'markerColorscale', 'showLegend'];
+ScatterMapbox.meta = {
+    hrName: 'scatter_mapbox',
+    description: [
+        'The data visualized as scatter point, lines or marker symbols',
+        'on a Mapbox GL geographic map',
+        'is provided by longitude/latitude pairs in `lon` and `lat`.'
+    ].join(' ')
+};
+
+module.exports = ScatterMapbox;
