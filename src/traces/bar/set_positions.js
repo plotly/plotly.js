d2797165438f99b5615b9b95e1ec741e61fb298d
keeganmccallum <keeganmccallum3@gmail.com>
2016-06-23 18:40:47 -0400

    fix formatting

--- src/traces/bar/set_positions.js
+++ src/traces/bar/set_positions.js
@@ -149,7 +149,7 @@ module.exports = function setPositions(gd, plotinfo) {
 
                     sv = Math.round(ti[j].p / sumround);
                     // store the negative sum value for p at the same key, with sign flipped
-                    if(relative && ti[j].s < 0) sv = '-'+sv;  // use string to ensure -0 !== 0
+                    if(relative && ti[j].s < 0) sv = '-' + sv;  // use string to ensure -0 !== 0
                     var previousSum = sums[sv] || 0;
                     if(stack || relative) ti[j].b = previousSum;
                     barEnd = ti[j].b + ti[j].s;
@@ -178,7 +178,7 @@ module.exports = function setPositions(gd, plotinfo) {
                     for(j = 0; j < ti.length; j++) {
                         relAndNegative = relative && ti[j].s < 0;
                         sv = Math.round(ti[j].p / sumround);
-                        if(relAndNegative) sv = -sv;  // locate negative sum amount for this p val
+                        if(relAndNegative) sv = '-' + sv;  // locate negative sum amount for this p val
                         scale = top / sums[sv];
                         if(relAndNegative) scale *= -1; // preserve sign if negative
                         ti[j].b *= scale;
