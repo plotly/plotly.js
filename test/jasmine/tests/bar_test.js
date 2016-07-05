36216f944073f80554108f6f19abae7b9bcdcc34
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-07-05 14:04:10 -0400

    lint: flatten bar test suite

--- test/jasmine/tests/bar_test.js
+++ test/jasmine/tests/bar_test.js
@@ -1,60 +1,70 @@
 var Bar = require('@src/traces/bar');
 
-
-describe('Test bar', function() {
+describe('bar supplyDefaults', function() {
     'use strict';
 
-    describe('supplyDefaults', function() {
-        var traceIn,
-            traceOut;
+    var traceIn,
+        traceOut;
 
-        var defaultColor = '#444';
+    var defaultColor = '#444';
 
-        var supplyDefaults = Bar.supplyDefaults;
+    var supplyDefaults = Bar.supplyDefaults;
 
-        beforeEach(function() {
-            traceOut = {};
+    beforeEach(function() {
+        traceOut = {};
+    });
+
+    it('should set visible to false when x and y are empty', function() {
+        traceIn = {};
+        supplyDefaults(traceIn, traceOut, defaultColor);
+        expect(traceOut.visible).toBe(false);
+
+        traceIn = {
+            x: [],
+            y: []
+        };
+        supplyDefaults(traceIn, traceOut, defaultColor);
+        expect(traceOut.visible).toBe(false);
+    });
+
+    it('should set visible to false when x or y is empty', function() {
+        traceIn = {
+            x: []
+        };
+        supplyDefaults(traceIn, traceOut, defaultColor);
+        expect(traceOut.visible).toBe(false);
+
+        traceIn = {
+            x: [],
+            y: [1, 2, 3]
+        };
+        supplyDefaults(traceIn, traceOut, defaultColor);
+        expect(traceOut.visible).toBe(false);
+
+        traceIn = {
+            y: []
+        };
+        supplyDefaults(traceIn, traceOut, defaultColor);
+        expect(traceOut.visible).toBe(false);
+
+        traceIn = {
+            x: [1, 2, 3],
+            y: []
+        };
+        supplyDefaults(traceIn, traceOut, defaultColor);
+        expect(traceOut.visible).toBe(false);
+    });
+});
+
+    'use strict';
+
+
+
+
         });
 
-        it('should set visible to false when x and y are empty', function() {
-            traceIn = {};
-            supplyDefaults(traceIn, traceOut, defaultColor);
-            expect(traceOut.visible).toBe(false);
-
-            traceIn = {
-                x: [],
-                y: []
-            };
-            supplyDefaults(traceIn, traceOut, defaultColor);
-            expect(traceOut.visible).toBe(false);
         });
 
-        it('should set visible to false when x or y is empty', function() {
-            traceIn = {
-                x: []
-            };
-            supplyDefaults(traceIn, traceOut, defaultColor);
-            expect(traceOut.visible).toBe(false);
-
-            traceIn = {
-                x: [],
-                y: [1, 2, 3]
-            };
-            supplyDefaults(traceIn, traceOut, defaultColor);
-            expect(traceOut.visible).toBe(false);
-
-            traceIn = {
-                y: []
-            };
-            supplyDefaults(traceIn, traceOut, defaultColor);
-            expect(traceOut.visible).toBe(false);
-
-            traceIn = {
-                x: [1, 2, 3],
-                y: []
-            };
-            supplyDefaults(traceIn, traceOut, defaultColor);
-            expect(traceOut.visible).toBe(false);
         });
 
     });
