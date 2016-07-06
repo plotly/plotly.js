f620a2292eb48f5e9e15aa0c2783ebcc5d445e58
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-07-06 14:18:15 -0400

    validate: add support for is isSubplotObj attributes

--- test/jasmine/tests/validate_test.js
+++ test/jasmine/tests/validate_test.js
@@ -95,15 +95,41 @@ describe('Plotly.validate', function() {
                     }]
                 }
             },
+            xaxis2: {
+                type: 'date',
+                rangeselector: {
+                    buttons: [{
+                        title: '1 month'
+                    }]
+                }
+            },
             shapes: [{
                 opacity: 'none'
             }]
         });
 
-        assertErrorShape(out, [], 4);
+        assertErrorShape(out, [], 5);
         assertErrorContent(out.layout[0], 'schema', ['annotations[1]', 'arrowSymbol'], '');
         assertErrorContent(out.layout[1], 'unused', ['xaxis', 'rangeselector', 'buttons[0]', 'count'], '');
         assertErrorContent(out.layout[2], 'schema', ['xaxis', 'rangeselector', 'buttons[1]', 'title'], '');
-        assertErrorContent(out.layout[3], 'value', ['shapes[0]', 'opacity'], '');
+        assertErrorContent(out.layout[3], 'schema', ['xaxis2', 'rangeselector', 'buttons[0]', 'title'], '');
+        assertErrorContent(out.layout[4], 'value', ['shapes[0]', 'opacity'], '');
+    });
+
+    it('should work with isSubplotObj attributes', function() {
+        var out = Plotly.validate([], {
+            xaxis2: {
+                range: 30
+            },
+            scene10: {
+                bgcolor: 'red'
+            },
+            geo0: {},
+        });
+
+        assertErrorShape(out, [], 4);
+        assertErrorContent(out.layout[0], 'value', ['xaxis2', 'range'], '');
+        assertErrorContent(out.layout[1], 'unused', ['scene10'], '');
+        assertErrorContent(out.layout[2], 'schema', ['geo0'], '');
     });
 });
