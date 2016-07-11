5547bbec67f7a681185f2e9f8495791dc8d1fc0c
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-07-11 12:11:36 -0400

    1.14.2

--- src/assets/geo_assets.js
+++ src/assets/geo_assets.js
@@ -12,6 +12,6 @@ var saneTopojson = require('sane-topojson');
 
 
 // package version injected by `npm run preprocess`
-exports.version = '1.14.1';
+exports.version = '1.14.2';
 
 exports.topojson = saneTopojson;
