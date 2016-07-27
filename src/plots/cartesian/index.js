6afc3391f4c21ca5efa542b713f0daaede081dbb
Ricky Reusser <rsreusser@gmail.com>
2016-07-27 11:34:42 -0400

    Catch degenerate trace-fill-linking case

--- src/plots/cartesian/index.js
+++ src/plots/cartesian/index.js
@@ -63,10 +63,10 @@ exports.plot = function(gd, traces, transitionOpts) {
                 // traces 0 and 2. Trace 1 also needs to be updated, otherwise its fill
                 // is outdated. So this retroactively adds the previous trace if the
                 // traces are interdependent.
-                if(['tonextx', 'tonexty', 'tonext'].indexOf(trace.fill) !== -1) {
-                    if(cdSubplot.indexOf(pcd) === -1) {
-                        cdSubplot.push(pcd);
-                    }
+                if(pcd &&
+                        ['tonextx', 'tonexty', 'tonext'].indexOf(trace.fill) !== -1 &&
+                        cdSubplot.indexOf(pcd) === -1) {
+                    cdSubplot.push(pcd);
                 }
 
                 // If this trace is specifically requested, add it to the list:
