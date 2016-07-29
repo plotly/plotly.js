584e644328ff50006f3447d24bc8523d2ae8f928
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-07-29 15:39:59 -0400

    tasks: refactor pretest
    - group sub-tasks in subroutines
    - DRY up using common module
    - add image test container setup sub-task such that
      `npm run pretest` on CircleCI runs the docker container
      and sets it up once and for all

--- tasks/pretest.js
+++ tasks/pretest.js
@@ -1,50 +1,83 @@
 var fs = require('fs');
+
 var constants = require('./util/constants');
+var common = require('./util/common');
+var containerCommands = require('./util/container_commands');
+var isCI = process.env.CIRCLECI;
 
-
+// main
+makeCredentialsFile();
+makeSetPlotConfigFile();
+makeTestImageFolders();
+if(isCI) runAndSetupImageTestContainer();
 
 // Create a credentials json file,
 // to be required in jasmine test suites and test dashboard
-var credentials = JSON.stringify({
-    MAPBOX_ACCESS_TOKEN: mapboxAccessToken
-}, null, 2);
+function makeCredentialsFile() {
+    var credentials = JSON.stringify({
+        MAPBOX_ACCESS_TOKEN: constants.mapboxAccessToken
+    }, null, 2);
 
-fs.writeFile(constants.pathToCredentials, credentials, function(err) {
-    if(err) throw err;
-});
+    common.writeFile(constants.pathToCredentials, credentials);
+    logger('make build/credentials.json');
+}
 
 // Create a 'set plot config' file,
 // to be included in the image test index
-var setPlotConfig = [
-    '\'use strict\';',
-    '',
-    '/* global Plotly:false */',
-    '',
-    'Plotly.setPlotConfig({',
-    '    mapboxAccessToken: \'' + mapboxAccessToken + '\'',
-    '});',
-    ''
-].join('\n');
+function makeSetPlotConfigFile() {
+    var setPlotConfig = [
+        '\'use strict\';',
+        '',
+        '/* global Plotly:false */',
+        '',
+        'Plotly.setPlotConfig({',
+        '    mapboxAccessToken: \'' + constants.mapboxAccessToken + '\'',
+        '});',
+        ''
+    ].join('\n');
 
-fs.writeFile(constants.pathToSetPlotConfig, setPlotConfig, function(err) {
-    if(err) throw err;
-});
-
-// make artifact folders for image tests
-if(!doesDirExist(constants.pathToTestImagesDiff)) {
-    fs.mkdirSync(constants.pathToTestImagesDiff);
-}
-if(!doesDirExist(constants.pathToTestImages)) {
-    fs.mkdirSync(constants.pathToTestImages);
+    common.writeFile(constants.pathToSetPlotConfig, setPlotConfig);
+    logger('make build/set_plot_config.js');
 }
 
-function doesDirExist(dirPath) {
-    try {
-        if(fs.statSync(dirPath).isDirectory()) return true;
-    }
-    catch(e) {
-        return false;
+// Make artifact folders for image tests
+function makeTestImageFolders() {
+
+    function makeOne(folderPath, info) {
+        if(!common.doesDirExist(folderPath)) {
+            fs.mkdirSync(folderPath);
+            logger('initialize ' + info);
+        }
+        else logger(info + ' is present');
     }
 
-    return false;
+    makeOne(constants.pathToTestImages, 'test image folder');
+    makeOne(constants.pathToTestImagesDiff, 'test image diff folder');
+}
+
+// On CircleCI, run and setup image test container once an for all
+function runAndSetupImageTestContainer() {
+
+    function run() {
+        var cmd = containerCommands.dockerRun;
+        common.execCmd(cmd, function() {
+            logger('run docker container');
+            setup();
+        });
+    }
+
+    function setup() {
+        var cmd = containerCommands.getRunCmd(isCI, [
+            containerCommands.setup
+        ]);
+        common.execCmd(cmd, function() {
+            logger('setup docker container');
+        });
+    }
+
+    run();
+}
+
+function logger(task) {
+    console.log('ok ' + task);
 }
