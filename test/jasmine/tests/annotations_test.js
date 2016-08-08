050390114bce7cef7f5077f23347440b60f9e6b7
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-08-08 14:37:12 -0400

    test: add relayout tests for annotations

--- test/jasmine/tests/annotations_test.js
+++ test/jasmine/tests/annotations_test.js
@@ -1,7 +1,13 @@
-require('@src/plotly');
-var Plots = require('@src/plots/plots');
 var Annotations = require('@src/components/annotations');
+
+var Plotly = require('@lib/index');
+var Plots = require('@src/plots/plots');
+var Lib = require('@src/lib');
 var Dates = require('@src/lib/dates');
+
+var d3 = require('d3');
+var createGraphDiv = require('../assets/create_graph_div');
+var destroyGraphDiv = require('../assets/destroy_graph_div');
 
 describe('Test annotations', function() {
     'use strict';
@@ -28,6 +34,49 @@ describe('Test annotations', function() {
             Annotations.supplyLayoutDefaults(annotationIn, annotationOut);
 
             expect(annotationIn.annotations[0].ax).toEqual(Dates.dateTime2ms('2004-07-01'));
+        });
+    });
+});
+
+describe('annotations relayout', function() {
+    'use strict';
+
+    var mock = require('@mocks/annotations.json');
+    var len = mock.layout.annotations.length;
+    var gd;
+
+    beforeEach(function(done) {
+        gd = createGraphDiv();
+
+        var mockData = Lib.extendDeep([], mock.data),
+            mockLayout = Lib.extendDeep({}, mock.layout);
+
+        Plotly.plot(gd, mockData, mockLayout).then(done);
+    });
+
+    afterEach(destroyGraphDiv);
+
+    function countAnnotations() {
+        return d3.selectAll('g.annotation').size();
+    }
+
+    it('should be able to add /remove annotations', function(done) {
+        expect(countAnnotations()).toEqual(len);
+
+        var ann = { text: '' };
+
+        Plotly.relayout(gd, 'annotations[' + len + ']', ann).then(function() {
+            expect(countAnnotations()).toEqual(len + 1);
+
+            return Plotly.relayout(gd, 'annotations[' + 0 + ']', 'remove');
+        }).then(function() {
+            expect(countAnnotations()).toEqual(len);
+
+            return Plotly.relayout(gd, { annotations: [] });
+        }).then(function() {
+            expect(countAnnotations()).toEqual(0);
+
+            done();
         });
     });
 });
