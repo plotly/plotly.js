57cec8cc8966c186d76038868e22b15f9d1eaf78
Robert Monfera <monfera.robert@gmail.com>
2016-10-04 21:57:48 +0200

    PR feedback - cover the other gl2d plot types - other plot type tests

--- test/jasmine/tests/gl2d_click_test.js
+++ test/jasmine/tests/gl2d_click_test.js
@@ -17,6 +17,79 @@ describe('Test hover and click interactions', function() {
     if(!hasWebGLSupport('gl2d_click_test')) return;
 
     var mock = require('@mocks/gl2d_14.json');
+    var mock2 = require('@mocks/gl2d_pointcloud-basic.json');
+    var mock3 = {
+        'data': [
+            {
+                'type': 'contourgl',
+                'z': [
+                    [
+                        10,
+                        10.625,
+                        12.5,
+                        15.625,
+                        20
+                    ],
+                    [
+                        5.625,
+                        6.25,
+                        8.125,
+                        11.25,
+                        15.625
+                    ],
+                    [
+                        2.5,
+                        3.125,
+                        5,
+                        8.125,
+                        12.5
+                    ],
+                    [
+                        0.625,
+                        1.25,
+                        3.125,
+                        6.25,
+                        10.625
+                    ],
+                    [
+                        0,
+                        0.625,
+                        2.5,
+                        5.625,
+                        10
+                    ]
+                ],
+                'colorscale': 'Jet',
+                'contours': {
+                    'start': 2,
+                    'end': 10,
+                    'size': 1
+                },
+                'uid': 'ad5624',
+                'zmin': 0,
+                'zmax': 20
+            }
+        ],
+        'layout': {
+            'xaxis': {
+                'range': [
+                    0,
+                    4
+                ],
+                'autorange': true
+            },
+            'yaxis': {
+                'range': [
+                    0,
+                    4
+                ],
+                'autorange': true
+            },
+            'height': 450,
+            'width': 1000,
+            'autosize': true
+        }
+    };
 
     var mockCopy, gd;
 
@@ -181,6 +254,91 @@ describe('Test hover and click interactions', function() {
         });
 
 
+    });
+
+    describe('hover event is fired for other gl2d plot types', function() {
+        var futureData;
+
+        it('pointcloud', function(done) {
+
+            var modifiedMockCopy = Lib.extendDeep({}, mock2);
+
+            Plotly.plot(gd, modifiedMockCopy.data, modifiedMockCopy.layout)
+
+                .then(new Promise(function() {
+
+                    gd.on('plotly_hover', function(data) {
+                        futureData = data;
+                    });
+
+                    hover(540, 150);
+
+                    window.setTimeout(function() {
+
+                        expect(futureData.points.length).toEqual(1);
+
+                        var pt = futureData.points[0];
+
+                        expect(Object.keys(pt)).toEqual([
+                            'x', 'y', 'curveNumber', 'pointNumber', 'data', 'fullData', 'xaxis', 'yaxis'
+                        ]);
+
+                        expect(pt.x).toEqual(4.5);
+                        expect(pt.y).toEqual(9);
+                        expect(pt.curveNumber).toEqual(2);
+                        expect(pt.pointNumber).toEqual(1);
+                        expect(pt.fullData.length).toEqual(3);
+                        expect(typeof pt.data.uid).toEqual('string');
+                        expect(pt.xaxis.domain.length).toEqual(2);
+                        expect(pt.yaxis.domain.length).toEqual(2);
+
+                        done();
+                    }, 350);
+                }));
+
+
+        });
+
+        it('heatmapgl', function(done) {
+
+            var modifiedMockCopy = Lib.extendDeep({}, mock3);
+            modifiedMockCopy.data[0].type = 'heatmapgl';
+
+            Plotly.plot(gd, modifiedMockCopy.data, modifiedMockCopy.layout)
+
+                .then(new Promise(function() {
+
+                    gd.on('plotly_hover', function(data) {
+                        futureData = data;
+                    });
+
+                    hover(540, 150);
+
+                    window.setTimeout(function() {
+
+                        expect(futureData.points.length).toEqual(1);
+
+                        var pt = futureData.points[0];
+
+                        expect(Object.keys(pt)).toEqual([
+                            'x', 'y', 'curveNumber', 'pointNumber', 'data', 'fullData', 'xaxis', 'yaxis'
+                        ]);
+
+                        expect(pt.x).toEqual(2);
+                        expect(pt.y).toEqual(3);
+                        expect(pt.curveNumber).toEqual(0);
+                        expect(pt.pointNumber).toEqual([2, 3, 17]);
+                        expect(pt.fullData.length).toEqual(1);
+                        expect(typeof pt.data.uid).toEqual('string');
+                        expect(pt.xaxis.domain.length).toEqual(2);
+                        expect(pt.yaxis.domain.length).toEqual(2);
+
+                        done();
+                    }, 350);
+                }));
+
+
+        });
     });
 
     describe('click event is fired on click', function() {
