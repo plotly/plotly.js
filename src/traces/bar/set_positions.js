f63a7b7881712b2d25fa363df231761dd6b3875d
Nicolas Riesco <enquiries@nicolasriesco.net>
2016-10-10 15:04:39 +0100

    bar: refactor code into function `setBaseAndSize`

--- src/traces/bar/set_positions.js
+++ src/traces/bar/set_positions.js
@@ -54,8 +54,7 @@ function setGroupPositions(gd, pa, sa, traces) {
 
     var fullLayout = gd._fullLayout,
         pLetter = pa._id.charAt(0),
-        sLetter = sa._id.charAt(0),
-        i, j;
+        sLetter = sa._id.charAt(0);
 
     if(fullLayout.barmode === 'overlay') {
         traces.forEach(function(trace) {
@@ -64,116 +63,7 @@ function setGroupPositions(gd, pa, sa, traces) {
     }
     else setOffsetAndWidth(gd, pa, pLetter, traces);
 
-    var stack = (fullLayout.barmode === 'stack'),
-        relative = (fullLayout.barmode === 'relative'),
-        norm = fullLayout.barnorm;
-
-    // bar size range and stacking calculation
-    if(stack || relative || norm) {
-        // for stacked bars, we need to evaluate every step in every
-        // stack, because negative bars mean the extremes could be
-        // anywhere
-        // also stores the base (b) of each bar in calcdata
-        // so we don't have to redo this later
-        var sMax = sa.l2c(sa.c2l(0)),
-            sMin = sMax,
-            sums = {},
-
-            // make sure if p is different only by rounding,
-            // we still stack
-            sumround = traces[0][0].t.barwidth / 100,
-            sv = 0,
-            padded = true,
-            barEnd,
-            ti,
-            scale;
-
-        for(i = 0; i < traces.length; i++) { // trace index
-            ti = traces[i];
-
-            for(j = 0; j < ti.length; j++) {
-
-                // skip over bars with no size,
-                // so that we don't try to stack them
-                if(!isNumeric(ti[j].s)) continue;
-
-                sv = Math.round(ti[j].p / sumround);
-
-                // store the negative sum value for p at the same key,
-                // with sign flipped using string to ensure -0 !== 0.
-                if(relative && ti[j].s < 0) sv = '-' + sv;
-
-                var previousSum = sums[sv] || 0;
-                if(stack || relative) ti[j].b = previousSum;
-                barEnd = ti[j].b + ti[j].s;
-                sums[sv] = previousSum + ti[j].s;
-
-                // store the bar top in each calcdata item
-                if(stack || relative) {
-                    ti[j][sLetter] = barEnd;
-                    if(!norm && isNumeric(sa.c2l(barEnd))) {
-                        sMax = Math.max(sMax, barEnd);
-                        sMin = Math.min(sMin, barEnd);
-                    }
-                }
-            }
-        }
-
-        if(norm) {
-            var top = norm === 'fraction' ? 1 : 100,
-                relAndNegative = false,
-                tiny = top / 1e9; // in case of rounding error in sum
-
-            padded = false;
-            sMin = 0;
-            sMax = stack ? top : 0;
-
-            for(i = 0; i < traces.length; i++) { // trace index
-                ti = traces[i];
-
-                for(j = 0; j < ti.length; j++) {
-                    relAndNegative = (relative && ti[j].s < 0);
-
-                    sv = Math.round(ti[j].p / sumround);
-
-                    // locate negative sum amount for this p val
-                    if(relAndNegative) sv = '-' + sv;
-
-                    scale = top / sums[sv];
-
-                    // preserve sign if negative
-                    if(relAndNegative) scale *= -1;
-                    ti[j].b *= scale;
-                    ti[j].s *= scale;
-                    barEnd = ti[j].b + ti[j].s;
-                    ti[j][sLetter] = barEnd;
-
-                    if(isNumeric(sa.c2l(barEnd))) {
-                        if(barEnd < sMin - tiny) {
-                            padded = true;
-                            sMin = barEnd;
-                        }
-                        if(barEnd > sMax + tiny) {
-                            padded = true;
-                            sMax = barEnd;
-                        }
-                    }
-                }
-            }
-        }
-
-        Axes.expand(sa, [sMin, sMax], {tozero: true, padded: padded});
-    }
-    else {
-        // for grouped or overlaid bars, just make sure zero is
-        // included, along with the tops of each bar, and store
-        // these bar tops in calcdata
-        var fs = function(v) { v[sLetter] = v.s; return v.s; };
-
-        for(i = 0; i < traces.length; i++) {
-            Axes.expand(sa, traces[i].map(fs), {tozero: true, padded: true});
-        }
-    }
+    setBaseAndSize(gd, sa, sLetter, traces);
 }
 
 
@@ -240,6 +130,126 @@ function setOffsetAndWidth(gd, pa, pLetter, traces) {
         for(j = 0; j < trace.length; j++) {
             bar = trace[j];
             bar[pLetter] = bar.p + barCenter;
+        }
+    }
+}
+
+
+function setBaseAndSize(gd, sa, sLetter, traces) {
+    var fullLayout = gd._fullLayout,
+        i, trace,
+        j, bar;
+
+    var stack = (fullLayout.barmode === 'stack'),
+        relative = (fullLayout.barmode === 'relative'),
+        norm = fullLayout.barnorm;
+
+    // bar size range and stacking calculation
+    if(stack || relative || norm) {
+        // for stacked bars, we need to evaluate every step in every
+        // stack, because negative bars mean the extremes could be
+        // anywhere
+        // also stores the base (b) of each bar in calcdata
+        // so we don't have to redo this later
+        var sMax = sa.l2c(sa.c2l(0)),
+            sMin = sMax,
+            sums = {},
+
+            // make sure if p is different only by rounding,
+            // we still stack
+            sumround = traces[0][0].t.barwidth / 100,
+            sv = 0,
+            padded = true,
+            barEnd,
+            scale;
+
+        for(i = 0; i < traces.length; i++) { // trace index
+            trace = traces[i];
+
+            for(j = 0; j < trace.length; j++) {
+                bar = trace[j];
+
+                // skip over bars with no size,
+                // so that we don't try to stack them
+                if(!isNumeric(bar.s)) continue;
+
+                sv = Math.round(bar.p / sumround);
+
+                // store the negative sum value for p at the same key,
+                // with sign flipped using string to ensure -0 !== 0.
+                if(relative && bar.s < 0) sv = '-' + sv;
+
+                var previousSum = sums[sv] || 0;
+                if(stack || relative) bar.b = previousSum;
+                barEnd = bar.b + bar.s;
+                sums[sv] = previousSum + bar.s;
+
+                // store the bar top in each calcdata item
+                if(stack || relative) {
+                    bar[sLetter] = barEnd;
+                    if(!norm && isNumeric(sa.c2l(barEnd))) {
+                        sMax = Math.max(sMax, barEnd);
+                        sMin = Math.min(sMin, barEnd);
+                    }
+                }
+            }
+        }
+
+        if(norm) {
+            var top = norm === 'fraction' ? 1 : 100,
+                relAndNegative = false,
+                tiny = top / 1e9; // in case of rounding error in sum
+
+            padded = false;
+            sMin = 0;
+            sMax = stack ? top : 0;
+
+            for(i = 0; i < traces.length; i++) { // trace index
+                trace = traces[i];
+
+                for(j = 0; j < trace.length; j++) {
+                    bar = trace[j];
+
+                    relAndNegative = (relative && bar.s < 0);
+
+                    sv = Math.round(bar.p / sumround);
+
+                    // locate negative sum amount for this p val
+                    if(relAndNegative) sv = '-' + sv;
+
+                    scale = top / sums[sv];
+
+                    // preserve sign if negative
+                    if(relAndNegative) scale *= -1;
+                    bar.b *= scale;
+                    bar.s *= scale;
+                    barEnd = bar.b + bar.s;
+                    bar[sLetter] = barEnd;
+
+                    if(isNumeric(sa.c2l(barEnd))) {
+                        if(barEnd < sMin - tiny) {
+                            padded = true;
+                            sMin = barEnd;
+                        }
+                        if(barEnd > sMax + tiny) {
+                            padded = true;
+                            sMax = barEnd;
+                        }
+                    }
+                }
+            }
+        }
+
+        Axes.expand(sa, [sMin, sMax], {tozero: true, padded: padded});
+    }
+    else {
+        // for grouped or overlaid bars, just make sure zero is
+        // included, along with the tops of each bar, and store
+        // these bar tops in calcdata
+        var fs = function(v) { v[sLetter] = v.s; return v.s; };
+
+        for(i = 0; i < traces.length; i++) {
+            Axes.expand(sa, traces[i].map(fs), {tozero: true, padded: true});
         }
     }
 }
