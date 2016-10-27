f958b4f5dc3abc9eb5bb811fa2ec7aa2b1dbd380
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-10-27 16:02:35 -0400

    doc: improve comment in patch minified tasks util

--- tasks/util/patch_minified.js
+++ tasks/util/patch_minified.js
@@ -8,7 +8,14 @@ var BACK = '+");';
  *
  * so that plotly.min.js loads in Jupyter NBs, more info here:
  *
- * https://github.com/plotly/plotly.py/pull/545
+ * - https://github.com/plotly/plotly.py/pull/545
+ * - https://github.com/plotly/plotly.js/pull/914
+ * - https://github.com/plotly/plotly.js/pull/1094
+ *
+ * For example, this routine replaces
+ *  'require("+o(s)+")' -> 'require("+ o(s) +")'
+ *
+ * But works for any 1-letter variable that uglify-js may output.
  *
  */
 module.exports = function patchMinified(minifiedCode) {
