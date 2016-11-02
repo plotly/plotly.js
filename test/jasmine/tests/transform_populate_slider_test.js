74be1b022c037b7fed18099642a4e8b56f6a3070
Ricky Reusser <rsreusser@gmail.com>
2016-11-02 09:19:41 -0400

    Add test file

--- test/jasmine/tests/transform_populate_slider_test.js
+++ test/jasmine/tests/transform_populate_slider_test.js
@@ -1,4 +1,4 @@
-/* var Plotly = require('@lib/index');
+var Plotly = require('@lib/index');
 var Filter = require('@lib/filter');
 
 var Plots = require('@src/plots/plots');
@@ -10,6 +10,20 @@ var assertDims = require('../assets/assert_dims');
 var assertStyle = require('../assets/assert_style');
 
 describe('filter transforms defaults:', function() {
+    var gd;
 
+    beforeEach(function() {
+        gd = createGraphDiv();
+    });
+
+    afterEach(function() {
+        Plotly.purge(gd);
+        destroyGraphDiv();
+    });
+
+    it('passes', function(done) {
+        Plotly.plot(gd, [{
+            x: [1, 2, 3]
+        }]).then(done);
+    });
 });
-*/
