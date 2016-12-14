b21464d8bb8cdf73c4c3e0146587fa97519d1396
Ricky Reusser <rsreusser@gmail.com>
2016-12-14 15:38:46 -0500

    Compress away nulls when adding frames

--- test/jasmine/tests/frame_api_test.js
+++ test/jasmine/tests/frame_api_test.js
@@ -43,6 +43,13 @@ describe('Test frame api', function() {
             }).catch(fail).then(done);
         });
 
+        it('compresses nulls when adding frames', function (done) {
+            Plotly.addFrames(gd, [null, {name: 'test'}, null]).then(function () {
+                expect(Object.keys(h)).toEqual(['test']);
+                expect(f).toEqual([{name: 'test'}]);
+            }).catch(fail).then(done);
+        });
+
         it('treats a null list as a noop', function(done) {
             Plotly.addFrames(gd, null).then(function() {
                 expect(Object.keys(h)).toEqual([]);
