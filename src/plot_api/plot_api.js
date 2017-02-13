f59750abcfd4aa50d88781005c0f8afd2e601dcc
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-02-13 15:16:57 -0500

    extend input relayout & restyle objects before looping over key-vals
    - so that if key-vals need to be mapped
      (this happens for annotations relayout calls), the input
      object doesn't get mutated.

--- src/plot_api/plot_api.js
+++ src/plot_api/plot_api.js
@@ -1180,7 +1180,7 @@ Plotly.restyle = function restyle(gd, astr, val, traces) {
     if(typeof astr === 'string') aobj[astr] = val;
     else if(Lib.isPlainObject(astr)) {
         // the 3-arg form
-        aobj = astr;
+        aobj = Lib.extendFlat({}, astr);
         if(traces === undefined) traces = val;
     }
     else {
@@ -1696,7 +1696,9 @@ Plotly.relayout = function relayout(gd, astr, val) {
 
     var aobj = {};
     if(typeof astr === 'string') aobj[astr] = val;
-    else if(Lib.isPlainObject(astr)) aobj = astr;
+    else if(Lib.isPlainObject(astr)) {
+        aobj = Lib.extendFlat({}, astr);
+    }
     else {
         Lib.warn('Relayout fail.', astr, val);
         return Promise.reject();
@@ -2085,10 +2087,10 @@ Plotly.update = function update(gd, traceUpdate, layoutUpdate, traces) {
     if(Object.keys(traceUpdate).length) gd.changed = true;
     if(Object.keys(layoutUpdate).length) gd.changed = true;
 
-    var restyleSpecs = _restyle(gd, traceUpdate, traces),
+    var restyleSpecs = _restyle(gd, Lib.extendFlat({}, traceUpdate), traces),
         restyleFlags = restyleSpecs.flags;
 
-    var relayoutSpecs = _relayout(gd, layoutUpdate),
+    var relayoutSpecs = _relayout(gd, Lib.extendFlat({}, layoutUpdate)),
         relayoutFlags = relayoutSpecs.flags;
 
     // clear calcdata if required
