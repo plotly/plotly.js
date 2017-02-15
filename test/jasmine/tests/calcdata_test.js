89b56b78ac900c9ac5a94123dc583aa4511bbab4
Ricky Reusser <rsreusser@gmail.com>
2017-02-15 11:53:12 -0500

    Explicitly test passing object as customdata

--- test/jasmine/tests/calcdata_test.js
+++ test/jasmine/tests/calcdata_test.js
@@ -866,15 +866,14 @@ describe('calculated data and points', function() {
     describe('customdata', function() {
         it('should pass customdata to the calcdata points', function() {
             Plotly.plot(gd, [{
-                x: [0, 1, 2, 3],
-                y: [4, 5, 6, 7],
-                customdata: ['a', 'b', 'c', 'd']
+                x: [0, 1, 3],
+                y: [4, 5, 7],
+                customdata: ['a', 'b', {foo: 'bar'}]
             }], {});
 
             expect(gd.calcdata[0][0]).toEqual(jasmine.objectContaining({data: 'a'}));
             expect(gd.calcdata[0][1]).toEqual(jasmine.objectContaining({data: 'b'}));
-            expect(gd.calcdata[0][2]).toEqual(jasmine.objectContaining({data: 'c'}));
-            expect(gd.calcdata[0][3]).toEqual(jasmine.objectContaining({data: 'd'}));
+            expect(gd.calcdata[0][2]).toEqual(jasmine.objectContaining({data: {foo: 'bar'}}));
         });
     });
 });
