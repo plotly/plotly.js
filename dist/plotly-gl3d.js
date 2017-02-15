f334c387398a107b91599d17d74bc7f093d4f725
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-02-15 13:51:21 -0500

    1.23.2

--- dist/plotly-gl3d.js
+++ dist/plotly-gl3d.js
@@ -1,5 +1,5 @@
 /**
-* plotly.js (gl3d) v1.23.1
+* plotly.js (gl3d) v1.23.2
 * Copyright 2012-2017, Plotly, Inc.
 * All rights reserved.
 * Licensed under the MIT license
@@ -60232,7 +60232,7 @@ exports.svgAttrs = {
 var Plotly = require('./plotly');
 
 // package version injected by `npm run preprocess`
-exports.version = '1.23.1';
+exports.version = '1.23.2';
 
 // inject promise polyfill
 require('es6-promise').polyfill();
