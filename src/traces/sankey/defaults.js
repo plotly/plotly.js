315c47812850bf1f481077b571fd5fd0c56c0b60
Robert Monfera <monfera.robert@gmail.com>
2017-03-01 13:20:59 +0100

    pruning parcoords leftovers from sankey

--- src/traces/sankey/defaults.js
+++ src/traces/sankey/defaults.js
@@ -10,68 +10,6 @@
 
 var Lib = require('../../lib');
 var attributes = require('./attributes');
-var hasColorscale = require('../../components/colorscale/has_colorscale');
-var colorscaleDefaults = require('../../components/colorscale/defaults');
-
-function handleLineDefaults(traceIn, traceOut, defaultColor, layout, coerce) {
-
-    coerce('line.color', defaultColor);
-    coerce('line.colorscale');
-
-    if(hasColorscale(traceIn, 'line')) {
-        colorscaleDefaults(traceIn, traceOut, layout, coerce, {prefix: 'line.', cLetter: 'c'});
-    }
-    else {
-        coerce('line.color', defaultColor);
-    }
-}
-
-function dimensionsDefaults(traceIn, traceOut) {
-    var dimensionsIn = traceIn.dimensions || [],
-        dimensionsOut = traceOut.dimensions = [];
-
-    var dimensionIn, dimensionOut, i;
-    var commonLength = Infinity;
-
-    function coerce(attr, dflt) {
-        return Lib.coerce(dimensionIn, dimensionOut, attributes.dimensions, attr, dflt);
-    }
-
-    for(i = 0; i < dimensionsIn.length; i++) {
-        dimensionIn = dimensionsIn[i];
-        dimensionOut = {};
-
-        if(!Lib.isPlainObject(dimensionIn)) {
-            continue;
-        }
-
-        var values = coerce('values');
-        var visible = coerce('visible', values.length > 0);
-
-        if(visible) {
-            coerce('label');
-            coerce('tickvals');
-            coerce('ticktext');
-            coerce('tickformat');
-            coerce('range');
-            coerce('constraintrange');
-
-            commonLength = Math.min(commonLength, dimensionOut.values.length);
-        }
-
-        dimensionOut._index = i;
-        dimensionsOut.push(dimensionOut);
-    }
-
-    if(isFinite(commonLength)) {
-        for(i = 0; i < dimensionsOut.length; i++) {
-            dimensionOut = dimensionsOut[i];
-            if(dimensionOut.visible && dimensionOut.values.length > commonLength) {
-                dimensionOut.values = dimensionOut.values.slice(0, commonLength);
-            }
-        }
-    }
-}
 
 function nodesDefaults(traceIn, traceOut) {
     var nodesIn = traceIn.nodes || [],
@@ -137,21 +75,14 @@ function linksDefaults(traceIn, traceOut) {
 }
 
 
-module.exports = function supplyDefaults(traceIn, traceOut, defaultColor, layout) {
+module.exports = function supplyDefaults(traceIn, traceOut) {
     function coerce(attr, dflt) {
         return Lib.coerce(traceIn, traceOut, attributes, attr, dflt);
     }
 
-    dimensionsDefaults(traceIn, traceOut);
-    var nodes = nodesDefaults(traceIn, traceOut);
+    nodesDefaults(traceIn, traceOut);
     linksDefaults(traceIn, traceOut);
-
-    handleLineDefaults(traceIn, traceOut, defaultColor, layout, coerce);
 
     coerce('domain.x');
     coerce('domain.y');
-
-    if(!Array.isArray(nodes) || !nodes.length) {
-    //    traceOut.visible = false;
-    }
 };
--- src/traces/sankey/plot.js
+++ src/traces/sankey/plot.js
@@ -14,34 +14,8 @@ module.exports = function plot(gd, calcData) {
 
     var fullLayout = gd._fullLayout;
     var svg = fullLayout._paper;
-    var root = fullLayout._paperdiv;
-
-    var gdDimensions = {};
-    var gdDimensionsOriginalOrder = {};
 
     var size = fullLayout._size;
-
-    calcData.forEach(function(d, i) {
-        gdDimensions[i] = gd.data[i].dimensions;
-        gdDimensionsOriginalOrder[i] = gd.data[i].dimensions.slice();
-    });
-
-    var filterChanged = function(i, originalDimensionIndex, newRange) {
-
-        // Have updated `constraintrange` data on `gd.data` and raise `Plotly.restyle` event
-        // without having to incur heavy UI blocking due to an actual `Plotly.restyle` call
-
-        var gdDimension = gdDimensionsOriginalOrder[i][originalDimensionIndex];
-        var gdConstraintRange = gdDimension.constraintrange;
-
-        if(!gdConstraintRange || gdConstraintRange.length !== 2) {
-            gdConstraintRange = gdDimension.constraintrange = [];
-        }
-        gdConstraintRange[0] = newRange[0];
-        gdConstraintRange[1] = newRange[1];
-
-        gd.emit('plotly_restyle');
-    };
 
     var hover = function(eventData) {
         gd.emit('plotly_hover', eventData);
@@ -51,52 +25,7 @@ module.exports = function plot(gd, calcData) {
         gd.emit('plotly_unhover', eventData);
     };
 
-    var axesMoved = function(i, visibleIndices) {
-
-        // Have updated order data on `gd.data` and raise `Plotly.restyle` event
-        // without having to incur heavy UI blocking due to an actual `Plotly.restyle` call
-
-        function visible(dimension) {return !('visible' in dimension) || dimension.visible;}
-
-        function newIdx(visibleIndices, orig, dim) {
-            var origIndex = orig.indexOf(dim);
-            var currentIndex = visibleIndices.indexOf(origIndex);
-            if(currentIndex === -1) {
-                // invisible dimensions initially go to the end
-                currentIndex += orig.length;
-            }
-            return currentIndex;
-        }
-
-        function sorter(orig) {
-            return function sorter(d1, d2) {
-                var i1 = newIdx(visibleIndices, orig, d1);
-                var i2 = newIdx(visibleIndices, orig, d2);
-                return i1 - i2;
-            };
-        }
-
-        // drag&drop sorting of the visible dimensions
-        var orig = sorter(gdDimensionsOriginalOrder[i].filter(visible));
-        gdDimensions[i].sort(orig);
-
-        // invisible dimensions are not interpreted in the context of drag&drop sorting as an invisible dimension
-        // cannot be dragged; they're interspersed into their original positions by this subsequent merging step
-        gdDimensionsOriginalOrder[i].filter(function(d) {return !visible(d);})
-             .sort(function(d) {
-                 // subsequent splicing to be done left to right, otherwise indices may be incorrect
-                 return gdDimensionsOriginalOrder[i].indexOf(d);
-             })
-            .forEach(function(d) {
-                gdDimensions[i].splice(gdDimensions[i].indexOf(d), 1); // remove from the end
-                gdDimensions[i].splice(gdDimensionsOriginalOrder[i].indexOf(d), 0, d); // insert at original index
-            });
-
-        gd.emit('plotly_restyle');
-    };
-
     render(
-        root,
         svg,
         calcData,
         {
@@ -110,9 +39,7 @@ module.exports = function plot(gd, calcData) {
             }
         },
         {
-            filterChanged: filterChanged,
             hover: hover,
-            unhover: unhover,
-            axesMoved: axesMoved
+            unhover: unhover
         });
 };
