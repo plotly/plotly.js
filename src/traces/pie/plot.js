e08f727903b87edf5098407e49bce94c26c46ab8
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-03-21 13:31:48 +0000

    pie: emit `plotly_hover` even if hoverinfo is none
    Fixes https://github.com/plotly/plotly.js/issues/902

--- src/traces/pie/plot.js
+++ src/traces/pie/plot.js
@@ -86,6 +86,8 @@ module.exports = function plot(gd, cdpie) {
                     hasHoverData = false;
 
                 function handleMouseOver(evt) {
+                    evt.originalEvent = d3.event;
+
                     // in case fullLayout or fullData has changed without a replot
                     var fullLayout2 = gd._fullLayout,
                         trace2 = gd._fullData[trace.index],
@@ -97,6 +99,7 @@ module.exports = function plot(gd, cdpie) {
                     // or if hover is turned off
                     if(gd._dragging || fullLayout2.hovermode === false ||
                             hoverinfo === 'none' || hoverinfo === 'skip' || !hoverinfo) {
+                        Fx.hover(gd, evt, 'pie');
                         return;
                     }
 
@@ -126,7 +129,6 @@ module.exports = function plot(gd, cdpie) {
                         outerContainer: fullLayout2._paper.node()
                     });
 
-                    evt.originalEvent = d3.event;
                     Fx.hover(gd, evt, 'pie');
 
                     hasHoverData = true;
