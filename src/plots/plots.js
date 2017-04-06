5bd40955e240f6c714a2c10f73d3743255404758
hy9be_uva <hy9be@virginia.edu>
2017-04-06 09:52:05 -0400

    Handle showSource config
    Made changes according to code review feedback.

--- src/plots/plots.js
+++ src/plots/plots.js
@@ -257,7 +257,7 @@ plots.previousPromises = function(gd) {
  * Add source links to your graph inside the 'showSources' config argument.
  */
 plots.addLinks = function(gd) {
-    if(gd._context.showLink === false) return;
+    if(!gd._context.showLink || !gd._context.showSources) return;
 
     var fullLayout = gd._fullLayout;
 
