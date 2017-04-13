153f17046c48acaf7da67acb3785531e82d2d9ae
Robert Monfera <monfera.robert@gmail.com>
2017-04-14 00:06:38 +0200

    Doc string update

--- src/traces/sankey/index.js
+++ src/traces/sankey/index.js
@@ -24,7 +24,7 @@ Plot.meta = {
     description: [
         'Sankey plots for network flow data analysis.',
         'The nodes are specified in `nodes` and the links between sources and targets in `links`.',
-        'The colors are set in `line.color`.'
+        'The colors are set in `nodes[i].color` and `links[i].color`; otherwise defaults are used.'
     ].join(' ')
 };
 
