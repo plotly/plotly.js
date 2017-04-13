db6f31ea5b53207bb78b90f454171700682e27be
Robert Monfera <monfera.robert@gmail.com>
2017-04-14 00:05:27 +0200

    Updated Plot.categories

--- src/traces/sankey/index.js
+++ src/traces/sankey/index.js
@@ -19,7 +19,7 @@ Plot.colorbar = require('./colorbar');
 Plot.moduleType = 'trace';
 Plot.name = 'sankey';
 Plot.basePlotModule = require('./base_plot');
-Plot.categories = ['gl'];
+Plot.categories = [];
 Plot.meta = {
     description: [
         'Sankey plots for network flow data analysis.',
