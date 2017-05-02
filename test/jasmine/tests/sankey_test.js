a2a416e300cdb7bc4bc2fcdb62bfa50ca1f0dae2
Robert Monfera <monfera.robert@gmail.com>
2017-05-02 16:42:16 +0200

    jasmine tests for Sankey

---
+++ test/jasmine/tests/sankey_test.js
@@ -0,0 +1,178 @@
+var Lib = require('@src/lib');
+var Sankey = require('@src/traces/sankey');
+var attributes = require('@src/traces/sankey/attributes');
+
+describe('sankey initialization tests', function() {
+
+    'use strict';
+
+    function _supply(traceIn) {
+        var traceOut = { visible: true },
+            defaultColor = '#444',
+            layout = { };
+
+        Sankey.supplyDefaults(traceIn, traceOut, defaultColor, layout);
+
+        return traceOut;
+    }
+
+    function _supplyWithLayout(traceIn, layout) {
+        var traceOut = { visible: true },
+            defaultColor = '#444';
+
+        Sankey.supplyDefaults(traceIn, traceOut, defaultColor, layout);
+
+        return traceOut;
+    }
+
+    describe('don\'t remove nodes if encountering no circularity', function() {
+
+        it('removing a single self-pointing node', function() {
+            var fullTrace = _supply({
+                node: {
+                    label: ['a', 'b']
+                },
+                link: {
+                    value: [1],
+                    source: [1],
+                    target: [0]
+                }
+            });
+
+            expect(fullTrace.node.label).toEqual(['a', 'b'], 'node labels retained');
+            expect(fullTrace.link.value).toEqual([1], 'link value(s) retained');
+            expect(fullTrace.link.source).toEqual([1], 'link source(s) retained');
+            expect(fullTrace.link.target).toEqual([0], 'link target(s) retained');
+        });
+    });
+
+    describe('remove nodes if encountering circularity', function() {
+
+        it('removing a single self-pointing node', function() {
+            var fullTrace = _supply({
+                node: {
+                    label: ['a']
+                },
+                link: {
+                    value: [1],
+                    source: [0],
+                    target: [0]
+                }
+            });
+
+            expect(fullTrace.node.label).toEqual([], 'node label(s) removed');
+            expect(fullTrace.link.value).toEqual([], 'link value(s) removed');
+            expect(fullTrace.link.source).toEqual([], 'link source(s) removed');
+            expect(fullTrace.link.target).toEqual([], 'link target(s) removed');
+
+        });
+
+        it('removing everything if detecting a circle', function() {
+            var fullTrace = _supply({
+                node: {
+                    label: ['a', 'b', 'c', 'd', 'e']
+                },
+                link: {
+                    value: [1, 1, 1, 1, 1, 1, 1, 1],
+                    source: [0, 1, 2, 3],
+                    target: [1, 2, 0, 4]
+                }
+            });
+
+            expect(fullTrace.node.label).toEqual([], 'node label(s) removed');
+            expect(fullTrace.link.value).toEqual([], 'link value(s) removed');
+            expect(fullTrace.link.source).toEqual([], 'link source(s) removed');
+            expect(fullTrace.link.target).toEqual([], 'link target(s) removed');
+
+        });
+
+    });
+
+    describe('sankey defaults', function() {
+
+        it('\'Sankey\' specification should have proper arrays where mandatory',
+            function() {
+
+                var fullTrace = _supply({});
+
+                expect(fullTrace.node.label)
+                    .toEqual([], 'presence of node label array is guaranteed');
+
+                expect(fullTrace.link.value)
+                    .toEqual([], 'presence of link value array is guaranteed');
+
+                expect(fullTrace.link.source)
+                    .toEqual([], 'presence of link source array is guaranteed');
+
+                expect(fullTrace.link.target)
+                    .toEqual([], 'presence of link target array is guaranteed');
+
+            });
+
+        it('\'Sankey\' specification should have proper types',
+            function() {
+
+                var fullTrace = _supply({});
+
+                expect(fullTrace.orientation)
+                    .toEqual(attributes.orientation.dflt, 'use orientation by default');
+
+                expect(fullTrace.valueformat)
+                    .toEqual(attributes.valueformat.dflt, 'valueformat by default');
+
+                expect(fullTrace.valuesuffix)
+                    .toEqual(attributes.valuesuffix.dflt, 'valuesuffix by default');
+
+                expect(fullTrace.arrangement)
+                    .toEqual(attributes.arrangement.dflt, 'arrangement by default');
+
+                expect(fullTrace.domain.x)
+                    .toEqual(attributes.domain.x.dflt, 'x domain by default');
+
+                expect(fullTrace.domain.y)
+                    .toEqual(attributes.domain.y.dflt, 'y domain by default');
+            });
+
+        it('\'Sankey\' layout dependent specification should have proper types',
+            function() {
+
+                var fullTrace = _supplyWithLayout({}, {font: {family: 'Arial'}});
+                expect(fullTrace.textfont)
+                    .toEqual({family: 'Arial'}, 'textfont is defined');
+            });
+
+        it('\'line\' specifications should yield the default values',
+            function() {
+
+                var fullTrace = _supply({});
+
+                expect(fullTrace.node.line.color)
+                    .toEqual('#444', 'default node line color');
+                expect(fullTrace.node.line.width)
+                    .toEqual(0.5, 'default node line thickness');
+
+                expect(fullTrace.link.line.color)
+                    .toEqual('#444', 'default link line color');
+                expect(fullTrace.link.line.width)
+                    .toEqual(0, 'default link line thickness');
+            });
+
+        it('fills \'node\' colors if not specified', function() {
+
+            var fullTrace = _supply({
+                node: {
+                    label: ['a', 'b']
+                },
+                link: {
+                    source: [0],
+                    target: [1],
+                    value: [1]
+                }
+            });
+
+            expect(Lib.isArray(fullTrace.node.color)).toBe(true, 'set up color array');
+
+        });
+
+    });
+});
