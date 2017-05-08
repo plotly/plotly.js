8ac49aec0a5e6939af0b31ca7e7ea47d677847f5
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-05-08 13:04:59 -0400

    add 'recalc' to list of compatible attribute keys

--- test/jasmine/tests/plotschema_test.js
+++ test/jasmine/tests/plotschema_test.js
@@ -76,7 +76,7 @@ describe('plot schema', function() {
                     var valObject = valObjects[attr.valType],
                         opts = valObject.requiredOpts
                             .concat(valObject.otherOpts)
-                            .concat(['valType', 'description', 'role']);
+                            .concat(['valType', 'description', 'role', 'recalc']);
 
                     Object.keys(attr).forEach(function(key) {
                         expect(opts.indexOf(key) !== -1).toBe(true, key, attr);
