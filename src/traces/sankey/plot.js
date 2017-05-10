3d9fcf2807ac106dc161c35d464b8a97152ad0d9
Robert Monfera <monfera.robert@gmail.com>
2017-05-10 19:40:11 +0200

    fix hover tooltip position with non-original div position

--- src/traces/sankey/plot.js
+++ src/traces/sankey/plot.js
@@ -125,13 +125,14 @@ module.exports = function plot(gd, calcData) {
 
     var linkHoverFollow = function(element, d) {
 
+        var rootBBox = gd.getBoundingClientRect();
         var boundingBox = element.getBoundingClientRect();
         var hoverCenterX = boundingBox.left + boundingBox.width / 2;
         var hoverCenterY = boundingBox.top + boundingBox.height / 2;
 
         var tooltip = Fx.loneHover({
-            x: hoverCenterX + window.scrollX,
-            y: hoverCenterY + window.scrollY,
+            x: hoverCenterX - rootBBox.left + window.scrollX,
+            y: hoverCenterY - rootBBox.top + window.scrollY,
             name: d3.format(d.valueFormat)(d.link.value) + d.valueSuffix,
             text: [
                 d.link.label,
@@ -173,10 +174,11 @@ module.exports = function plot(gd, calcData) {
     var nodeHoverFollow = function(element, d) {
 
         var nodeRect = d3.select(element).select('.nodeRect');
+        var rootBBox = gd.getBoundingClientRect();
         var boundingBox = nodeRect.node().getBoundingClientRect();
-        var hoverCenterX0 = boundingBox.left - 2;
-        var hoverCenterX1 = boundingBox.right + 2;
-        var hoverCenterY = boundingBox.top + boundingBox.height / 4;
+        var hoverCenterX0 = boundingBox.left - 2 - rootBBox.left;
+        var hoverCenterX1 = boundingBox.right + 2 - rootBBox.left;
+        var hoverCenterY = boundingBox.top + boundingBox.height / 4 - rootBBox.top;
 
         var tooltip = Fx.loneHover({
             x0: hoverCenterX0 + window.scrollX,
