bcbee49697f1682ce024589a615bd0337bd49d5a
alexcjohnson <alex@plot.ly>
2017-06-02 00:02:58 -0400

    no really, initInteractions! fix #1044... again I don't understand why we need to initInteractions twice, but it seems necessary to do both before and after finalDraw

--- src/plot_api/plot_api.js
+++ src/plot_api/plot_api.js
@@ -383,6 +383,7 @@ Plotly.plot = function(gd, data, layout, config) {
         drawFramework,
         marginPushers,
         marginPushersAgain,
+        initInteractions,
         positionAndAutorange,
         subroutines.layoutStyles,
         drawAxes,
