d46cae7e7332e385c3a9a498136344f8a071d02d
Etienne Tétreault-Pinard <etienne@plot.ly>
2017-07-07 12:01:57 -0400

    merge custom shared assertions into single assets/ module

--- test/jasmine/assets/custom_assertions.js
+++ test/jasmine/assets/custom_assertions.js
@@ -2,7 +2,22 @@
 
 var d3 = require('d3');
 
-module.exports = function assertStyle(dims, color, opacity) {
+exports.assertDims = function(dims) {
+    var traces = d3.selectAll('.trace');
+
+    expect(traces.size())
+        .toEqual(dims.length, 'to have correct number of traces');
+
+    traces.each(function(_, i) {
+        var trace = d3.select(this);
+        var points = trace.selectAll('.point');
+
+        expect(points.size())
+            .toEqual(dims[i], 'to have correct number of pts in trace ' + i);
+    });
+};
+
+exports.assertStyle = function(dims, color, opacity) {
     var N = dims.reduce(function(a, b) {
         return a + b;
     });
