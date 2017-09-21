482cca1fa6fb9cb3c9725c01f96e9c66e3a44ef0
Etienne Tétreault-Pinard <etienne@plot.ly>
2017-09-21 19:17:42 -0400

    merge geo axis attrs module into 'main' layout attr module
    - no need for separate files.

--- src/plots/geo/layout/layout_attributes.js
+++ src/plots/geo/layout/layout_attributes.js
@@ -10,9 +10,58 @@
 
 var colorAttrs = require('../../../components/color/attributes');
 var constants = require('../constants');
-var geoAxesAttrs = require('./axis_attributes');
 var overrideAll = require('../../../plot_api/edit_types').overrideAll;
 
+var geoAxesAttrs = {
+    range: {
+        valType: 'info_array',
+        role: 'info',
+        items: [
+            {valType: 'number'},
+            {valType: 'number'}
+        ],
+        description: [
+            'Sets the range of this axis (in degrees),',
+        ].join(' ')
+    },
+    showgrid: {
+        valType: 'boolean',
+        role: 'info',
+        dflt: false,
+        description: 'Sets whether or not graticule are shown on the map.'
+    },
+    tick0: {
+        valType: 'number',
+        role: 'info',
+        description: [
+            'Sets the graticule\'s starting tick longitude/latitude.'
+        ].join(' ')
+    },
+    dtick: {
+        valType: 'number',
+        role: 'info',
+        description: [
+            'Sets the graticule\'s longitude/latitude tick step.'
+        ].join(' ')
+    },
+    gridcolor: {
+        valType: 'color',
+        role: 'style',
+        dflt: colorAttrs.lightLine,
+        description: [
+            'Sets the graticule\'s stroke color.'
+        ].join(' ')
+    },
+    gridwidth: {
+        valType: 'number',
+        role: 'style',
+        min: 0,
+        dflt: 1,
+        description: [
+            'Sets the graticule\'s stroke width (in px).'
+        ].join(' ')
+    }
+};
 
 module.exports = overrideAll({
     domain: {
