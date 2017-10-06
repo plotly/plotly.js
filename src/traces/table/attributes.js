61bc258190a916b4693abb3a7faee661b1fd511f
Robert Monfera <monfera.robert@gmail.com>
2017-10-06 23:08:04 +0200

    other use of `font` - thanks Étienne for the catch!

--- src/traces/table/attributes.js
+++ src/traces/table/attributes.js
@@ -214,36 +214,6 @@ module.exports = overrideAll({
             }
         },
 
-        font: {
-            family: {
-                valType: 'string',
-                arrayOk: true,
-                role: 'style',
-                noBlank: true,
-                strict: true,
-                description: [
-                    'HTML font family - the typeface that will be applied by the web browser.',
-                    'The web browser will only be able to apply a font if it is available on the system',
-                    'which it operates. Provide multiple font families, separated by commas, to indicate',
-                    'the preference in which to apply fonts if they aren\'t available on the system.',
-                    'The plotly service (at https://plot.ly or on-premise) generates images on a server,',
-                    'where only a select number of',
-                    'fonts are installed and supported.',
-                    'These include *Arial*, *Balto*, *Courier New*, *Droid Sans*,, *Droid Serif*,',
-                    '*Droid Sans Mono*, *Gravitas One*, *Old Standard TT*, *Open Sans*, *Overpass*,',
-                    '*PT Sans Narrow*, *Raleway*, *Times New Roman*.'
-                ].join(' ')
-            },
-            size: {
-                valType: 'number',
-                arrayOk: true,
-                role: 'style'
-            },
-            color: {
-                valType: 'color',
-                arrayOk: true,
-                role: 'style'
-            }
-        }
+        font: extendFlat({}, fontAttrs({arrayOk: true}))
     }
 }, 'calc', 'from-root');
