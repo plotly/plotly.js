789121ca9ef42d74f4d343254b59a52c4e6081a3
Etienne Tétreault-Pinard <etienne@plot.ly>
2017-11-01 13:24:19 -0400

    update violin layout attr descriptions

--- src/traces/violin/layout_attributes.js
+++ src/traces/violin/layout_attributes.js
@@ -9,10 +9,29 @@
 'use strict';
 
 var boxLayoutAttrs = require('../box/layout_attributes');
+var extendFlat = require('../../lib').extendFlat;
 
-// TODO update descriptions
 module.exports = {
-    violinmode: boxLayoutAttrs.boxmode,
-    violingap: boxLayoutAttrs.boxgap,
-    violingroupgap: boxLayoutAttrs.boxgroupgap
+    violinmode: extendFlat({}, boxLayoutAttrs.boxmode, {
+        description: [
+            'Determines how violins at the same location coordinate',
+            'are displayed on the graph.',
+            'If *group*, the violins are plotted next to one another',
+            'centered around the shared location.',
+            'If *overlay*, the violins are plotted over one another,',
+            'you might need to set *opacity* to see them multiple violins.'
+        ].join(' ')
+    }),
+    violingap: extendFlat({}, boxLayoutAttrs.boxgap, {
+        description: [
+            'Sets the gap (in plot fraction) between violins of',
+            'adjacent location coordinates.'
+        ].join(' ')
+    }),
+    violingroupgap: extendFlat({}, boxLayoutAttrs.boxgroupgap, {
+        description: [
+            'Sets the gap (in plot fraction) between violins of',
+            'the same location coordinate.'
+        ].join(' ')
+    })
 };
