6903d31ff5a4f03b1e74b7c8af934ba6548ee177
Etienne Tétreault-Pinard <etienne@plot.ly>
2017-11-02 15:48:34 -0400

    remove we won't do in this PR
    - namely, hoveredpoints,
      marker.size and marker.lin selected/unselected attributes.

--- src/traces/scatter/attributes.js
+++ src/traces/scatter/attributes.js
@@ -378,82 +378,61 @@ module.exports = {
         colorAttributes('marker')
     ),
     selected: {
-        marker: extendFlat({
+        marker: {
             opacity: {
                 valType: 'number',
                 min: 0,
                 max: 1,
-                arrayOk: true,
                 role: 'style',
                 editType: 'style',
                 description: 'Sets the marker opacity of selected points.'
             },
-            size: {
-                valType: 'number',
-                min: 0,
-                dflt: 6,
-                arrayOk: true,
+            color: {
+                valType: 'color',
                 role: 'style',
-                editType: 'calcIfAutorange',
-                description: 'Sets the marker size (in px) of selected points.'
+                editType: 'style',
+                description: 'Sets the marker color of selected points.'
             },
-            line: extendFlat({
-                width: {
-                    valType: 'number',
-                    min: 0,
-                    arrayOk: true,
-                    role: 'style',
-                    editType: 'style',
-                    description: 'Sets the width (in px) of the lines bounding selected marker points.'
-                },
-                editType: 'calc'
-            },
-                colorAttributes('marker.line')
-            ),
-            editType: 'calc'
+            editType: 'style'
         },
-            colorAttributes('marker')
-        )
+        textfont: {
+            color: {
+                valType: 'color',
+                role: 'style',
+                editType: 'style',
+                description: 'Sets the text font color of selected points.'
+            },
+            editType: 'style'
+        },
+        editType: 'style'
     },
     unselected: {
-        marker: extendFlat({
+        marker: {
             opacity: {
                 valType: 'number',
                 min: 0,
                 max: 1,
-                dflt: 0.5,
-                arrayOk: true,
                 role: 'style',
                 editType: 'style',
                 description: 'Sets the marker opacity of unselected points, applied only when a selection exists.'
             },
-            size: {
-                valType: 'number',
-                min: 0,
-                dflt: 6,
-                arrayOk: true,
+            color: {
+                valType: 'color',
                 role: 'style',
-                editType: 'calcIfAutorange',
-                description: 'Sets the marker size (in px) of unselected points.'
+                editType: 'style',
+                description: 'Sets the marker color of unselected points, applied only when a selection exists.'
             },
-            line: extendFlat({
-                width: {
-                    valType: 'number',
-                    min: 0,
-                    arrayOk: true,
-                    role: 'style',
-                    editType: 'style',
-                    description: 'Sets the width (in px) of the lines bounding unselected marker points.'
-                },
-                editType: 'calc'
-            },
-                colorAttributes('marker.line')
-            ),
-            editType: 'calc'
+            editType: 'style'
         },
-            colorAttributes('marker')
-        )
-    },
+        textfont: {
+            color: {
+                valType: 'color',
+                role: 'style',
+                editType: 'style',
+                description: 'Sets the text font color of unselected points, applied only when a selection exists.'
+            },
+            editType: 'style'
+        },
     selectedpoints: {
         valType: 'any',
         role: 'info',
@@ -466,18 +445,9 @@ module.exports = {
         editType: 'calc',
         description: 'Array containing `ids` of selected points.',
     },
-    hoverpoints: {
-        valType: 'any',
-        role: 'info',
-        editType: 'calc',
-        description: 'Array containing integer indices of hovered points.',
+        editType: 'style'
     },
-    hoverids: {
-        valType: 'any',
-        role: 'info',
-        editType: 'calc',
-        description: 'Array containing `ids` of hovered points.',
-    },
+
     textposition: {
         valType: 'enumerated',
         values: [
