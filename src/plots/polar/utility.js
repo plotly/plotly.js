eaad8b2960775be8817282042402aad6a09223ab
ROMAXTECHNOLOGY\Oliver.Gee.Admin <oliver.gee@romaxtech>
2017-11-13 12:02:32 +0000

    removed addtional test

--- src/plots/polar/utility.js
+++ src/plots/polar/utility.js
@@ -5,8 +5,8 @@
 * This source code is licensed under the MIT license found in the
 * LICENSE file in the root directory of this source tree.
 */
-'use strict';
 
+'use strict';
 var µ = module.exports = { version: '0.2.2' };
 µ.util = {};
 var d3 = require('d3');
