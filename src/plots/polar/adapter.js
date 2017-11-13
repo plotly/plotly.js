30c9f866d69b9f3ffe092594d88dd69d54e5a19b
ROMAXTECHNOLOGY\Oliver.Gee.Admin <oliver.gee@romaxtech>
2017-11-13 10:10:19 +0000

    added headers

--- src/plots/polar/adapter.js
+++ src/plots/polar/adapter.js
@@ -1,3 +1,12 @@
+/**
+* Copyright 2012-2017, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+/* eslint-disable new-cap */
 
 'use strict';
 var utility = require('./utility');
@@ -5,7 +14,6 @@ var Lib = require('../../lib');
 var d3 = require('d3');
 var extendDeepAll = Lib.extendDeepAll;
 var µ = module.exports = { version: '0.2.2' };
-
 µ.adapter = {};
 µ.adapter.plotly = function module() {
     var exports = {};
