b45e91d25ab79c7fe80721f0e8dab6fd25a118db
Etienne Tétreault-Pinard <etienne@plot.ly>
2018-01-17 12:31:37 -0500

    try re-balancing tests in the 2 containers to reduce exec time

--- tasks/ci_test.sh
+++ tasks/ci_test.sh
@@ -8,7 +8,6 @@ case $CIRCLE_NODE_INDEX in
         npm run test-image      || EXIT_STATE=$?
         npm run test-image-gl2d || EXIT_STATE=$?
         npm run test-bundle     || EXIT_STATE=$?
-        npm run test-syntax     || EXIT_STATE=$?
         npm run lint            || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
@@ -16,6 +15,7 @@ case $CIRCLE_NODE_INDEX in
     1)
         npm run test-jasmine || EXIT_STATE=$?
         npm run test-export  || EXIT_STATE=$?
+        npm run test-syntax  || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
 
