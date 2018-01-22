f90efdca5b340234223a31c26294570a0423db60
Tom Demulielr--Chevret <tdemulier@itlink.fr>
2018-01-22 11:06:58 +0100

    Fix locale-en.js header syntax

--- src/locale-en.js
+++ src/locale-en.js
@@ -1,10 +1,10 @@
 /**
- * Copyright 2012-2018, Plotly, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+* Copyright 2012-2018, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
 
 'use strict';
 
