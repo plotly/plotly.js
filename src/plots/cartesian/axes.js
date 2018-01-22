eda678d2c131587bfae7f7f33e1b3439587375e1
Nicolas Kruchten <nicolas@datacratic.com>
2018-01-22 11:14:29 -0500

    proper fix for top/right axes

--- src/plots/cartesian/axes.js
+++ src/plots/cartesian/axes.js
@@ -2107,7 +2107,10 @@ axes.doTicks = function(gd, axid, skipTitle) {
 
             if(!ax._marginPush || ax._marginPush < marginPush) {
                 ax._marginPush = marginPush;
-                var pushParams = {x: 0, y: 0, r: 0, l: 0, t: 0, b: 0};
+                var pushParams = {
+                    x: ax.side[0] === 'r' ? 1 : 0,
+                    y: ax.side[0] === 't' ? 1 : 0,
+                    r: 0, l: 0, t: 0, b: 0};
                 pushParams[ax.side[0]] = marginPush;
                 Plots.autoMargin(gd, ax._name, pushParams);
             }
