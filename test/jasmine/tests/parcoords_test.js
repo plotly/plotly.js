bcf59d7469b21db5de8de5bcdd4c1e76c09fd303
etienne <etienne@plot.ly>
2018-02-20 16:15:06 -0500

    replace Lib.isArray -> Array.isArray in tests w/o typed arrays

--- test/jasmine/tests/parcoords_test.js
+++ test/jasmine/tests/parcoords_test.js
@@ -615,7 +615,7 @@ describe('@gl parcoords', function() {
             function restyleDimension(key, setterValue) {
 
                 // array values need to be wrapped in an array; unwrapping here for value comparison
-                var value = Lib.isArray(setterValue) ? setterValue[0] : setterValue;
+                var value = Array.isArray(setterValue) ? setterValue[0] : setterValue;
 
                 return function() {
                     return Plotly.restyle(gd, 'dimensions[2].' + key, setterValue).then(function() {
@@ -875,7 +875,7 @@ describe('@gl parcoords', function() {
             Plotly.plot(gd, mockCopy.data, mockCopy.layout);
 
             function restyleDimension(key, dimIndex, setterValue) {
-                var value = Lib.isArray(setterValue) ? setterValue[0] : setterValue;
+                var value = Array.isArray(setterValue) ? setterValue[0] : setterValue;
                 return function() {
                     return Plotly.restyle(gd, 'dimensions[' + dimIndex + '].' + key, setterValue).then(function() {
                         expect(gd.data[0].dimensions[dimIndex][key]).toEqual(value, 'for dimension attribute \'' + key + '\'');
