aaf7249a775b01ac151a760f6267c1806d82f85c
etienne <etienne@plot.ly>
2018-05-17 16:34:31 -0400

    mv gl3d_cone-* out of `npm run test-image` into noci_test.sh
    .. so that we test gl3d_cone-* mocks using orca, not
       our old nw.js imagetest container which doesn't generate
       cones properly for some reason.

--- tasks/noci_test.sh
+++ tasks/noci_test.sh
@@ -13,8 +13,11 @@ npm run test-jasmine -- --tags=noCI --nowatch || EXIT_STATE=$?
 # since the update to mapbox-gl@0.44.0, we must use orca
 # as mapbox-gl versions >0.22.1 aren't supported on nw.js@0.12 used in the
 # 'old' image server
+#
+# cone traces don't render correctly in the imagetest container
 $root/../orca/bin/orca.js graph \
     $root/test/image/mocks/mapbox_* \
+    $root/test/image/mocks/gl3d_cone* \
     --plotly $root/build/plotly.js \
     --mapbox-access-token "pk.eyJ1IjoiZXRwaW5hcmQiLCJhIjoiY2luMHIzdHE0MGFxNXVubTRxczZ2YmUxaCJ9.hwWZful0U2CQxit4ItNsiQ" \
     --output-dir $root/test/image/baselines/ \
