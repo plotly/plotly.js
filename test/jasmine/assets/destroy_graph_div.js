dcb8313a18cad39622543469dd1d273625e0d022
simone_rubiu <simone.rubiu@incaseofengineering.com>
2018-08-10 09:47:22 +0200

    removed test/*

--- test/jasmine/assets/destroy_graph_div.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function destroyGraphDiv() {
-    var gd = document.getElementById('graph');
-
-    if(gd) document.body.removeChild(gd);
-};
--- test/jasmine/assets/hover.js
+++
@@ -1,5 +0,0 @@
-var mouseEvent = require('./mouse_event');
-
-module.exports = function hover(x, y) {
-    mouseEvent('mousemove', x, y);
-};
