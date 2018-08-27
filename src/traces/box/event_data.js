e7c2240bc563dece9296354d505a0e17b5d1bafc
Robert Möstl <robert.moestl@scalingcurve.at>
2018-08-27 09:23:53 +0200

    Clean up box' custom eventData function [1852]

--- src/traces/box/event_data.js
+++ src/traces/box/event_data.js
@@ -9,18 +9,16 @@
 'use strict';
 
 module.exports = function eventData(out, pt) {
+
+    // Note: hoverOnBox property is needed for click-to-select
+    // to ignore when a box was clicked. This is the reason box
+    // implements this custom eventData function.
     if(pt.hoverOnBox) out.hoverOnBox = pt.hoverOnBox;
 
-    // TODO Clean up
     if('xVal' in pt) out.x = pt.xVal;
-    else if('x' in pt) out.x = pt.x;
-
     if('yVal' in pt) out.y = pt.yVal;
-    else if('y' in pt) out.y = pt.y;
-
     if(pt.xa) out.xaxis = pt.xa;
     if(pt.ya) out.yaxis = pt.ya;
-    if(pt.zLabelVal !== undefined) out.z = pt.zLabelVal;
 
     return out;
 };
