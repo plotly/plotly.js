a8598721c6fd0e8921ef5c83332a2189a7dc56c1
Étienne Tétreault-Pinard <etienne@plot.ly>
2018-09-07 14:38:30 -0400

    add info about polar.hole in setConvert jsdoc

--- src/plots/polar/set_convert.js
+++ src/plots/polar/set_convert.js
@@ -34,7 +34,7 @@ var rad2deg = Lib.rad2deg;
  *
  * Radial axis coordinate systems:
  * - d, c and l: same as for cartesian axes
- * - g: like calcdata but translated about `radialaxis.range[0]`
+ * - g: like calcdata but translated about `radialaxis.range[0]` & `polar.hole`
  *
  * Angular axis coordinate systems:
  * - d: data, in whatever form it's provided
