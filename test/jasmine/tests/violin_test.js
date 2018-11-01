8366c884504ccb9dedb90a016b731ee5e56b0e67
Kully <adam@plot.ly>
2018-11-01 15:54:15 -0400

    more testing with jasmine

--- test/jasmine/tests/violin_test.js
+++ test/jasmine/tests/violin_test.js
@@ -522,8 +522,8 @@ describe('Test violin hover:', function() {
         it('should show in two-sided base case', function(done) {
             Plotly.plot(gd, fig).then(function() {
                 mouseEvent('mousemove', 250, 250);
-                // assertViolinHoverLine([299.35, 250, 200.65, 250]);
-                assertViolinHoverLine([178.67823028564453, 250, 80, 250]);
+                assertViolinHoverLine([299.35, 250, 200.65, 250]);
+                // assertViolinHoverLine([178.67823028564453, 250, 80, 250]);
             })
             .catch(failTest)
             .then(done);
