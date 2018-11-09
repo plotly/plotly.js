3cb86311316cbb2c5cf3e223fd82830781b44860
Antoine Roy-Gobeil <antoine@plot.ly>
2018-11-09 14:35:28 -0500

    compare test images with baselines

--- .circleci/orca_generate.sh
+++ .circleci/orca_generate.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
-cd build/test_images
-ls ../../test/image/mocks/*.json | awk '!/mapbox/' | shuf | xargs -P1 -n20 xvfb-run -a orca graph --verbose
+ROOT=$(dirname $0)/..
+MOCKS=$ROOT/test/image/mocks
+BASELINES=$ROOT/test/image/baselines
+
+TEST_IMAGES=$ROOT/build/test_images
+DIFF_IMAGES=$ROOT/build/test_images_diff
+
+echo "Generating test images"
+ls $MOCKS/*.json | awk '!/mapbox/' | \
+    # Shuffle to distribute randomly slow and fast mocks
+    # shuf | \
+    # head -n 10 | \
+    # Split in chunks of 20
+    xargs -P1 -n20 xvfb-run -a orca graph --verbose --output-dir $TEST_IMAGES
+
+# Call compare.sh#
+echo "Comparing with baselines"
+find $TEST_IMAGES -type f -name "*.png" -printf "%f\n" | \
+  # shuf | \
+  # sort | \
+  # head -n 20 | \
+  # xargs -n1 -P16 -I {} bash -c "echo {} && ./node_modules/.bin/pixelmatch $1/{} $2/{} diff/{} 0 true" | tee results.txt
+  xargs -n1 -P8 -I {} bash -c "compare -verbose -metric AE $TEST_IMAGES/{} $BASELINES/{} $DIFF_IMAGES/{} 2> $DIFF_IMAGES/{}.txt"
+
+CODE=$(grep -R "all: [^0]" $DIFF_IMAGES/ | wc -l)
+grep -l -R "all: [^0]" $DIFF_IMAGES/ | gawk '{match($0, /diff\/([^.]*)/, arr); print arr[1]}'
+echo "$CODE different images"
+exit $CODE
