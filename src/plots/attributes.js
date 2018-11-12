7ab8630689019e6162cbbc49a374668390713774
alexcjohnson <alex@plot.ly>
2018-11-12 12:24:21 -0500

    comment on uirevision<->uid in trace.uirevision attribute description

--- src/plots/attributes.js
+++ src/plots/attributes.js
@@ -174,7 +174,12 @@ module.exports = {
             'interactions like `selectedpoints` and type-specific ones such as',
             '`constraintrange` in `parcoords` traces, as well as some',
             '`editable: true` modifications such as `name` and `colorbar.title`.',
-            'Defaults to `layout.uirevision`.'
+            'Defaults to `layout.uirevision`.',
+            'Trace changes are tracked by `uid`, which only falls back on trace',
+            'index if no `uid` is provided. So if your app can add/remove traces',
+            'before the end of the `data` array, such that the same trace has a',
+            'different index, you can still preserve user-driven changes if you',
+            'give each trace a `uid` that stays with it as it moves.'
         ].join(' ')
     }
 };
