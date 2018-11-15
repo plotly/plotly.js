70ea7518e9817b44e4d233baca950b1a95e42edd
Antoine Roy-Gobeil <antoine@plot.ly>
2018-11-15 13:15:16 -0500

    update baselines with pixelRatio forced to 2

--- src/lib/prepare_regl.js
+++ src/lib/prepare_regl.js
@@ -42,7 +42,7 @@ module.exports = function prepareRegl(gd, extensions) {
                     antialias: !d.pick,
                     preserveDrawingBuffer: true
                 },
-                pixelRatio: gd._context.plotGlPixelRatio || global.devicePixelRatio,
+                pixelRatio: 2 || gd._context.plotGlPixelRatio || global.devicePixelRatio,
                 extensions: extensions || []
             });
         } catch(e) {
