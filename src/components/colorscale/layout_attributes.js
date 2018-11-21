2460fc3a83cc190c5530364dddb591c7c6e1397f
Antoine Roy-Gobeil <antoine@plot.ly>
2018-11-21 14:20:30 -0500

    fix lint

--- src/components/colorscale/layout_attributes.js
+++ src/components/colorscale/layout_attributes.js
@@ -1,15 +1,12 @@
 /**
- * Copyright 2012-2018, Plotly, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
+* Copyright 2012-2018, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
 
 'use strict';
-
-var scales = require('./scales');
 
 var msg = 'Note that `autocolorscale` must be true for this attribute to work.';
 module.exports = {
