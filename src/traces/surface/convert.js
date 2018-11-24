0c1e2b2610bb7fa03ccdefb0ecb3a1143da9cb22
archmoj <mojtaba@plot.ly>
2018-11-24 17:53:09 -0500

    improved logic needed to reduce high-res cases

--- src/traces/surface/convert.js
+++ src/traces/surface/convert.js
@@ -115,7 +115,17 @@ var shortPrimes = [
     601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
     701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
     809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
-    907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997
+    907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
+    1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097,
+    1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193,
+    1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297,
+    1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399,
+    1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499,
+    1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597,
+    1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699,
+    1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789,
+    1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889,
+    1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999
 ];
 
 function getPow(a, b) {
@@ -140,12 +150,14 @@ function getFactors(a) {
 
 function smallestDivisor(a) {
     var A = getFactors(a);
+    var result = a;
     for(var i = 0; i < shortPrimes.length; i++) {
         if(A[i] > 0) {
-            return shortPrimes[i];
+            result = shortPrimes[i];
+            break;
         }
     }
-    return a;
+    return result;
 }
 
 function leastCommonMultiple(a, b) {
@@ -227,18 +239,18 @@ proto.estimateScale = function(width, height) {
     var resDst = 1 + leastCommonMultiple(xLCM, yLCM);
 
     // console.log("BEFORE: resDst=", resDst);
-
     while(resDst < MIN_RESOLUTION) {
         resDst *= 2;
     }
     while(resDst > MAX_RESOLUTION) {
-
+        resDst--;
         resDst /= smallestDivisor(resDst);
+        resDst++;
 
-        // option 2:
-        // resDst = MAX_RESOLUTION;
+        if (resDst < MIN_RESOLUTION) {
+            resDst = MIN_RESOLUTION;
+        }
     }
-
     // console.log("AFTER: resDst=", resDst);
 
     var scale = resDst / resSrc;
