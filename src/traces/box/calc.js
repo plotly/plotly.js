c802d321327bd0eeab257db82719930de555dfe1
Étienne Tétreault-Pinard <etienne@plot.ly>
2018-12-06 14:25:36 -0500

    fix box/violin x0/y0 multi-category edge case

--- src/traces/box/calc.js
+++ src/traces/box/calc.js
@@ -178,8 +178,10 @@ function getPos(trace, posLetter, posAxis, val, num) {
         pos0 = num;
     }
 
-    // TODO what to do on multicategory posAxis?
-    var pos0c = posAxis.d2c(pos0, 0, trace[posLetter + 'calendar']);
+    var pos0c = posAxis.type === 'multicategory' ?
+        posAxis.r2c(pos0) :
+        posAxis.d2c(pos0, 0, trace[posLetter + 'calendar']);
+
     return val.map(function() { return pos0c; });
 }
 
