7955cba0fc8ce9b233af67525ef436f39d74ed07
archmoj <mojtaba@plot.ly>
2018-12-10 01:20:38 -0500

    fix for pie scatterpolar and scattergeo cases

--- src/components/fx/hover.js
+++ src/components/fx/hover.js
@@ -944,7 +944,7 @@ function createHoverText(hoverData, opts, gd) {
             yLetter = gd._fullLayout.scene.yaxis.title.text || 'y';
             zLetter = gd._fullLayout.scene.zaxis.title.text || 'z';
         }
-        else if(!gd._fullLayout.ternary) {
+        else if(!gd._fullLayout.ternary && !gd._fullLayout.title) {
             xLetter = gd._fullLayout.xaxis.title.text || 'x';
             yLetter = gd._fullLayout.yaxis.title.text || 'y';
         }
