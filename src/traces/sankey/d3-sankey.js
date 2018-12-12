2504dc3b6910fcc0ad335794c675a9a23b5923d6
Antoine Roy-Gobeil <antoine@plot.ly>
2018-12-12 17:59:57 -0500

    fix header for d3-sankey.js

--- src/traces/sankey/d3-sankey.js
+++ src/traces/sankey/d3-sankey.js
@@ -1,4 +1,12 @@
 /**
+* Copyright 2012-2018, Plotly, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+/**
 Copyright 2015, Mike Bostock
 All rights reserved.
 
