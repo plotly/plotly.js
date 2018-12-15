1ad958662804895ffbc76cbe3d6d00e927576153
Antoine Roy-Gobeil <antoine@plot.ly>
2018-12-14 22:36:00 -0500

    updating to d3-sankey 0.7.1

--- src/traces/sankey/render.js
+++ src/traces/sankey/render.js
@@ -13,7 +13,8 @@ var d3 = require('d3');
 var tinycolor = require('tinycolor2');
 var Color = require('../../components/color');
 var Drawing = require('../../components/drawing');
-var d3sankey = require('./d3-sankey.js');
+var d3sankey = require('d3-sankey');
+var d3sankeyLinkHorizontal = require('d3').sankeyLinkHorizontal;
 var d3Force = require('d3-force');
 var Lib = require('../../lib');
 var gup = require('../../lib/gup');
@@ -78,13 +79,14 @@ function sankeyModel(layout, d, traceIndex) {
     var nodes = calcData._nodes;
     var links = calcData._links;
 
-    var sankey = d3sankey()
+    var sankey = d3sankey
+        .sankey()
+        .iterations(c.sankeyIterations)
         .size(horizontal ? [width, height] : [height, width])
         .nodeWidth(nodeThickness)
         .nodePadding(nodePad)
         .nodes(nodes)
-        .links(links)
-        .layout(c.sankeyIterations);
+        .links(links);
 
     if(sankey.nodePadding() < nodePad) {
         Lib.warn('node.pad was reduced to ', sankey.nodePadding(), ' to fit within the figure.');
@@ -111,8 +113,6 @@ function sankeyModel(layout, d, traceIndex) {
         translateY: layout.height - domain.y[1] * layout.height + layout.margin.t,
         dragParallel: horizontal ? height : width,
         dragPerpendicular: horizontal ? width : height,
-        nodes: nodes,
-        links: links,
         arrangement: trace.arrangement,
         sankey: sankey,
         forceLayouts: {},
@@ -123,20 +123,21 @@ function sankeyModel(layout, d, traceIndex) {
     };
 }
 
-function linkModel(uniqueKeys, d, l) {
+function linkModel(d, l, i) {
     var tc = tinycolor(l.color);
     var basicKey = l.source.label + '|' + l.target.label;
-    var foundKey = uniqueKeys[basicKey];
-    uniqueKeys[basicKey] = (foundKey || 0) + 1;
-    var key = basicKey + '__' + uniqueKeys[basicKey];
+    // var foundKey = uniqueKeys[basicKey];
+    // uniqueKeys[basicKey] = (foundKey || 0) + 1;
+    // var key = basicKey + '__' + uniqueKeys[basicKey];
 
     // for event data
     l.trace = d.trace;
     l.curveNumber = d.trace.index;
 
     return {
-        key: key,
+        key: basicKey,
         traceId: d.key,
+        pointNumber: l.pointNumber,
         link: l,
         tinyColorHue: Color.tinyRGB(tc),
         tinyColorAlpha: tc.getAlpha(),
@@ -149,25 +150,38 @@ function linkModel(uniqueKeys, d, l) {
     };
 }
 
-function nodeModel(uniqueKeys, d, n) {
+function linkPath() {
+    return d3sankey.sankeyLinkHorizontal()
+      .source(function(d) { return [d.link.source.x1, d.link.y0];})
+      .target(function(d) { return [d.link.target.x0, d.link.y1];});
+}
+
+function nodeModel(d, n, i) {
     var tc = tinycolor(n.color),
         zoneThicknessPad = c.nodePadAcross,
         zoneLengthPad = d.nodePad / 2,
-        visibleThickness = n.dx,
-        visibleLength = Math.max(0.5, n.dy);
+        visibleThickness = n.x1 - n.x0,
+        visibleLength = Math.max(0.5, (n.y1 - n.y0));
 
     var basicKey = n.label;
-    var foundKey = uniqueKeys[basicKey];
-    uniqueKeys[basicKey] = (foundKey || 0) + 1;
-    var key = basicKey + '__' + uniqueKeys[basicKey];
+    // var foundKey = uniqueKeys[basicKey];
+    // uniqueKeys[basicKey] = (foundKey || 0) + 1;
+    // var key = basicKey + '__' + uniqueKeys[basicKey];
 
     // for event data
     n.trace = d.trace;
     n.curveNumber = d.trace.index;
 
+    // additionnal coordinates
+    n.dx = n.x1 - n.x0;
+    n.dy = n.y1 - n.y0;
     return {
-        key: key,
-        traceId: d.key,
+        x0: n.x0,
+        x1: n.x1,
+        y0: n.y0,
+        y1: n.y1,
+        key: basicKey,
+        traceId: basicKey,
         node: n,
         nodePad: d.nodePad,
         nodeLineColor: d.nodeLineColor,
@@ -192,7 +206,7 @@ function nodeModel(uniqueKeys, d, n) {
         valueSuffix: d.valueSuffix,
         sankey: d.sankey,
         arrangement: d.arrangement,
-        uniqueNodeLabelPathId: [d.guid, d.key, key].join(' '),
+        uniqueNodeLabelPathId: [d.guid, d.key].join(' '),
         interactionState: d.interactionState
     };
 }
@@ -202,16 +216,8 @@ function nodeModel(uniqueKeys, d, n) {
 function updateNodePositions(sankeyNode) {
     sankeyNode
         .attr('transform', function(d) {
-            return 'translate(' + d.node.x.toFixed(3) + ', ' + (d.node.y - d.node.dy / 2).toFixed(3) + ')';
+            return 'translate(' + d.x0.toFixed(3) + ', ' + (d.y0).toFixed(3) + ')';
         });
-}
-
-function linkPath(d) {
-    var nodes = d.sankey.nodes();
-    switchToSankeyFormat(nodes);
-    var result = d.sankey.link()(d.link);
-    switchToForceFormat(nodes);
-    return result;
 }
 
 function updateNodeShapes(sankeyNode) {
@@ -220,15 +226,18 @@ function updateNodeShapes(sankeyNode) {
 
 function updateShapes(sankeyNode, sankeyLink) {
     sankeyNode.call(updateNodeShapes);
-    sankeyLink.attr('d', linkPath);
+    sankeyLink.attr('d', linkPath());
 }
 
 function sizeNode(rect) {
-    rect.attr('width', function(d) {return d.visibleWidth;})
-        .attr('height', function(d) {return d.visibleHeight;});
+    rect
+      // .attr('x', function(d) {return d.x0;})
+      // .attr('y', function(d) {return d.y0;})
+      .attr('width', function(d) {return d.x1 - d.x0;})
+      .attr('height', function(d) {return d.y1 - d.y0;});
 }
 
-function salientEnough(d) {return d.link.dy > 1 || d.linkLineWidth > 0;}
+function salientEnough(d) {return (d.y0 - d.y1) > 1 || d.linkLineWidth > 0;}
 
 function sankeyTransform(d) {
     var offset = 'translate(' + d.translateX + ',' + d.translateY + ')';
@@ -250,139 +259,139 @@ function textFlip(d) {return d.horizontal ? 'scale(1 1)' : 'scale(-1 1)';}
 function nodeTextColor(d) {return d.darkBackground && !d.horizontal ? 'rgb(255,255,255)' : 'rgb(0,0,0)';}
 function nodeTextOffset(d) {return d.horizontal && d.left ? '100%' : '0%';}
 
-// event handling
-
-function attachPointerEvents(selection, sankey, eventSet) {
-    selection
-        .on('.basic', null) // remove any preexisting handlers
-        .on('mouseover.basic', function(d) {
-            if(!d.interactionState.dragInProgress) {
-                eventSet.hover(this, d, sankey);
-                d.interactionState.hovered = [this, d];
-            }
-        })
-        .on('mousemove.basic', function(d) {
-            if(!d.interactionState.dragInProgress) {
-                eventSet.follow(this, d);
-                d.interactionState.hovered = [this, d];
-            }
-        })
-        .on('mouseout.basic', function(d) {
-            if(!d.interactionState.dragInProgress) {
-                eventSet.unhover(this, d, sankey);
-                d.interactionState.hovered = false;
-            }
-        })
-        .on('click.basic', function(d) {
-            if(d.interactionState.hovered) {
-                eventSet.unhover(this, d, sankey);
-                d.interactionState.hovered = false;
-            }
-            if(!d.interactionState.dragInProgress) {
-                eventSet.select(this, d, sankey);
-            }
-        });
-}
-
-function attachDragHandler(sankeyNode, sankeyLink, callbacks) {
-
-    var dragBehavior = d3.behavior.drag()
-
-        .origin(function(d) {return d.node;})
-
-        .on('dragstart', function(d) {
-            if(d.arrangement === 'fixed') return;
-            Lib.raiseToTop(this);
-            d.interactionState.dragInProgress = d.node;
-            saveCurrentDragPosition(d.node);
-            if(d.interactionState.hovered) {
-                callbacks.nodeEvents.unhover.apply(0, d.interactionState.hovered);
-                d.interactionState.hovered = false;
-            }
-            if(d.arrangement === 'snap') {
-                var forceKey = d.traceId + '|' + Math.floor(d.node.originalX);
-                if(d.forceLayouts[forceKey]) {
-                    d.forceLayouts[forceKey].alpha(1);
-                } else { // make a forceLayout iff needed
-                    attachForce(sankeyNode, forceKey, d);
-                }
-                startForce(sankeyNode, sankeyLink, d, forceKey);
-            }
-        })
-
-        .on('drag', function(d) {
-            if(d.arrangement === 'fixed') return;
-            var x = d3.event.x;
-            var y = d3.event.y;
-            if(d.arrangement === 'snap') {
-                d.node.x = x;
-                d.node.y = y;
-            } else {
-                if(d.arrangement === 'freeform') {
-                    d.node.x = x;
-                }
-                d.node.y = Math.max(d.node.dy / 2, Math.min(d.size - d.node.dy / 2, y));
-            }
-            saveCurrentDragPosition(d.node);
-            if(d.arrangement !== 'snap') {
-                d.sankey.relayout();
-                updateShapes(sankeyNode.filter(sameLayer(d)), sankeyLink);
-            }
-        })
-
-        .on('dragend', function(d) {
-            d.interactionState.dragInProgress = false;
-        });
-
-    sankeyNode
-        .on('.drag', null) // remove possible previous handlers
-        .call(dragBehavior);
-}
-
-function attachForce(sankeyNode, forceKey, d) {
-    var nodes = d.sankey.nodes().filter(function(n) {return n.originalX === d.node.originalX;});
-    d.forceLayouts[forceKey] = d3Force.forceSimulation(nodes)
-        .alphaDecay(0)
-        .force('collide', d3Force.forceCollide()
-            .radius(function(n) {return n.dy / 2 + d.nodePad / 2;})
-            .strength(1)
-            .iterations(c.forceIterations))
-        .force('constrain', snappingForce(sankeyNode, forceKey, nodes, d))
-        .stop();
-}
-
-function startForce(sankeyNode, sankeyLink, d, forceKey) {
-    window.requestAnimationFrame(function faster() {
-        for(var i = 0; i < c.forceTicksPerFrame; i++) {
-            d.forceLayouts[forceKey].tick();
-        }
-        d.sankey.relayout();
-        updateShapes(sankeyNode.filter(sameLayer(d)), sankeyLink);
-        if(d.forceLayouts[forceKey].alpha() > 0) {
-            window.requestAnimationFrame(faster);
-        }
-    });
-}
-
-function snappingForce(sankeyNode, forceKey, nodes, d) {
-    return function _snappingForce() {
-        var maxVelocity = 0;
-        for(var i = 0; i < nodes.length; i++) {
-            var n = nodes[i];
-            if(n === d.interactionState.dragInProgress) { // constrain node position to the dragging pointer
-                n.x = n.lastDraggedX;
-                n.y = n.lastDraggedY;
-            } else {
-                n.vx = (n.originalX - n.x) / c.forceTicksPerFrame; // snap to layer
-                n.y = Math.min(d.size - n.dy / 2, Math.max(n.dy / 2, n.y)); // constrain to extent
-            }
-            maxVelocity = Math.max(maxVelocity, Math.abs(n.vx), Math.abs(n.vy));
-        }
-        if(!d.interactionState.dragInProgress && maxVelocity < 0.1 && d.forceLayouts[forceKey].alpha() > 0) {
-            d.forceLayouts[forceKey].alpha(0);
-        }
-    };
-}
+// // event handling
+//
+// function attachPointerEvents(selection, sankey, eventSet) {
+//     selection
+//         .on('.basic', null) // remove any preexisting handlers
+//         .on('mouseover.basic', function(d) {
+//             if(!d.interactionState.dragInProgress) {
+//                 eventSet.hover(this, d, sankey);
+//                 d.interactionState.hovered = [this, d];
+//             }
+//         })
+//         .on('mousemove.basic', function(d) {
+//             if(!d.interactionState.dragInProgress) {
+//                 eventSet.follow(this, d);
+//                 d.interactionState.hovered = [this, d];
+//             }
+//         })
+//         .on('mouseout.basic', function(d) {
+//             if(!d.interactionState.dragInProgress) {
+//                 eventSet.unhover(this, d, sankey);
+//                 d.interactionState.hovered = false;
+//             }
+//         })
+//         .on('click.basic', function(d) {
+//             if(d.interactionState.hovered) {
+//                 eventSet.unhover(this, d, sankey);
+//                 d.interactionState.hovered = false;
+//             }
+//             if(!d.interactionState.dragInProgress) {
+//                 eventSet.select(this, d, sankey);
+//             }
+//         });
+// }
+//
+// function attachDragHandler(sankeyNode, sankeyLink, callbacks) {
+//
+//     var dragBehavior = d3.behavior.drag()
+//
+//         .origin(function(d) {return d.node;})
+//
+//         .on('dragstart', function(d) {
+//             if(d.arrangement === 'fixed') return;
+//             Lib.raiseToTop(this);
+//             d.interactionState.dragInProgress = d.node;
+//             saveCurrentDragPosition(d.node);
+//             if(d.interactionState.hovered) {
+//                 callbacks.nodeEvents.unhover.apply(0, d.interactionState.hovered);
+//                 d.interactionState.hovered = false;
+//             }
+//             if(d.arrangement === 'snap') {
+//                 var forceKey = d.traceId + '|' + Math.floor(d.node.originalX);
+//                 if(d.forceLayouts[forceKey]) {
+//                     d.forceLayouts[forceKey].alpha(1);
+//                 } else { // make a forceLayout iff needed
+//                     attachForce(sankeyNode, forceKey, d);
+//                 }
+//                 startForce(sankeyNode, sankeyLink, d, forceKey);
+//             }
+//         })
+//
+//         .on('drag', function(d) {
+//             if(d.arrangement === 'fixed') return;
+//             var x = d3.event.x;
+//             var y = d3.event.y;
+//             if(d.arrangement === 'snap') {
+//                 d.node.x = x;
+//                 d.node.y = y;
+//             } else {
+//                 if(d.arrangement === 'freeform') {
+//                     d.node.x = x;
+//                 }
+//                 d.node.y = Math.max(d.node.dy / 2, Math.min(d.size - d.node.dy / 2, y));
+//             }
+//             saveCurrentDragPosition(d.node);
+//             if(d.arrangement !== 'snap') {
+//                 d.sankey.relayout();
+//                 updateShapes(sankeyNode.filter(sameLayer(d)), sankeyLink);
+//             }
+//         })
+//
+//         .on('dragend', function(d) {
+//             d.interactionState.dragInProgress = false;
+//         });
+//
+//     sankeyNode
+//         .on('.drag', null) // remove possible previous handlers
+//         .call(dragBehavior);
+// }
+//
+// function attachForce(sankeyNode, forceKey, d) {
+//     var nodes = d.sankey.nodes().filter(function(n) {return n.originalX === d.node.originalX;});
+//     d.forceLayouts[forceKey] = d3Force.forceSimulation(nodes)
+//         .alphaDecay(0)
+//         .force('collide', d3Force.forceCollide()
+//             .radius(function(n) {return n.dy / 2 + d.nodePad / 2;})
+//             .strength(1)
+//             .iterations(c.forceIterations))
+//         .force('constrain', snappingForce(sankeyNode, forceKey, nodes, d))
+//         .stop();
+// }
+//
+// function startForce(sankeyNode, sankeyLink, d, forceKey) {
+//     window.requestAnimationFrame(function faster() {
+//         for(var i = 0; i < c.forceTicksPerFrame; i++) {
+//             d.forceLayouts[forceKey].tick();
+//         }
+//         d.sankey.relayout();
+//         updateShapes(sankeyNode.filter(sameLayer(d)), sankeyLink);
+//         if(d.forceLayouts[forceKey].alpha() > 0) {
+//             window.requestAnimationFrame(faster);
+//         }
+//     });
+// }
+//
+// function snappingForce(sankeyNode, forceKey, nodes, d) {
+//     return function _snappingForce() {
+//         var maxVelocity = 0;
+//         for(var i = 0; i < nodes.length; i++) {
+//             var n = nodes[i];
+//             if(n === d.interactionState.dragInProgress) { // constrain node position to the dragging pointer
+//                 n.x = n.lastDraggedX;
+//                 n.y = n.lastDraggedY;
+//             } else {
+//                 n.vx = (n.originalX - n.x) / c.forceTicksPerFrame; // snap to layer
+//                 n.y = Math.min(d.size - n.dy / 2, Math.max(n.dy / 2, n.y)); // constrain to extent
+//             }
+//             maxVelocity = Math.max(maxVelocity, Math.abs(n.vx), Math.abs(n.vy));
+//         }
+//         if(!d.interactionState.dragInProgress && maxVelocity < 0.1 && d.forceLayouts[forceKey].alpha() > 0) {
+//             d.forceLayouts[forceKey].alpha(0);
+//         }
+//     };
+// }
 
 // scene graph
 module.exports = function(svg, calcData, layout, callbacks) {
@@ -420,18 +429,15 @@ module.exports = function(svg, calcData, layout, callbacks) {
         .style('fill', 'none');
 
     var sankeyLink = sankeyLinks.selectAll('.' + c.cn.sankeyLink)
-        .data(function(d) {
-            var uniqueKeys = {};
-            return d.sankey.links()
+          .data(function(d) {
+              return d.sankey().links
                 .filter(function(l) {return l.value;})
-                .map(linkModel.bind(null, uniqueKeys, d));
-        }, keyFun);
-
-    sankeyLink.enter()
-        .append('path')
-        .classed(c.cn.sankeyLink, true)
-        .attr('d', linkPath)
-        .call(attachPointerEvents, sankey, callbacks.linkEvents);
+                .map(linkModel.bind(null, d));
+          }, keyFun)
+    sankeyLink
+          .enter().append('path')
+          .attr('d', linkPath());
+          // .call(attachPointerEvents, sankey, callbacks.linkEvents);
 
     sankeyLink
         .style('stroke', function(d) {
@@ -445,8 +451,8 @@ module.exports = function(svg, calcData, layout, callbacks) {
         .style('fill-opacity', function(d) {return d.tinyColorAlpha;});
 
     sankeyLink.transition()
-        .ease(c.ease).duration(c.duration)
-        .attr('d', linkPath);
+       .ease(c.ease).duration(c.duration)
+       .attr('d', linkPath());
 
     sankeyLink.exit().transition()
         .ease(c.ease).duration(c.duration)
@@ -471,22 +477,22 @@ module.exports = function(svg, calcData, layout, callbacks) {
 
     var sankeyNode = sankeyNodeSet.selectAll('.' + c.cn.sankeyNode)
         .data(function(d) {
-            var nodes = d.sankey.nodes();
+            var nodes = d.sankey().nodes;
             var uniqueKeys = {};
             persistOriginalPlace(nodes);
             return nodes
                 .filter(function(n) {return n.value;})
-                .map(nodeModel.bind(null, uniqueKeys, d));
+                .map(nodeModel.bind(null, d));
         }, keyFun);
 
     sankeyNode.enter()
         .append('g')
         .classed(c.cn.sankeyNode, true)
         .call(updateNodePositions)
-        .call(attachPointerEvents, sankey, callbacks.nodeEvents);
+        //.call(attachPointerEvents, sankey, callbacks.nodeEvents);
 
     sankeyNode
-        .call(attachDragHandler, sankeyLink, callbacks); // has to be here as it binds sankeyLink
+        //.call(attachDragHandler, sankeyLink, callbacks); // has to be here as it binds sankeyLink
 
     sankeyNode.transition()
         .ease(c.ease).duration(c.duration)
