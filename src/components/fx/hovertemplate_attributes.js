d25b7eb3a402c2860d9f254646afc6cf156421ef
Étienne Tétreault-Pinard <etienne@plot.ly>
2019-02-15 12:33:31 -0500

    add `hovertemplate` and `line.hovertemplate` to parcats traces

--- src/components/fx/hovertemplate_attributes.js
+++ src/components/fx/hovertemplate_attributes.js
@@ -31,7 +31,6 @@ module.exports = function(opts, extra) {
         valType: 'string',
         role: 'info',
         dflt: '',
-        arrayOk: true,
         editType: opts.editType || 'none',
         description: [
             'Template string used for rendering the information that appear on hover box.',
@@ -45,6 +44,10 @@ module.exports = function(opts, extra) {
             'Anything contained in tag `<extra>` is displayed in the secondary box, for example "<extra>{fullData.name}</extra>".'
         ].join(' ')
     };
+
+    if(opts.arrayOk !== false) {
+        hovertemplate.arrayOk = true;
+    }
 
     return hovertemplate;
 };
