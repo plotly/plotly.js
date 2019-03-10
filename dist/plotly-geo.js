e75f8a00b5e1cc6954c8a7516f9a77d20cfc357a
nitaking <10850034+nitaking@users.noreply.github.com>
2019-03-10 15:07:24 +0900

    add bundle

--- dist/plotly-geo.js
+++ dist/plotly-geo.js
@@ -1,3 +1,9 @@
+/**
+* plotly.js (geo) v1.45.2-fork.2
+* Copyright 2012-2019, Plotly, Inc.
+* All rights reserved.
+* Licensed under the MIT license
+*/
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Plotly = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(_dereq_,module,exports){
 'use strict';
 
@@ -23602,7 +23608,18 @@ function _hover(gd, evt, subplot, noHoverEvent) {
                 createSpikelines(spikePoints, spikelineOpts);
             }
         }
-        return result;
+        console.debug('return end hoverdata.length is end', result, xval, yval)
+        gd.emit('plotly_hover', {
+            event: evt,
+            points: gd._hoverdata,
+            xaxes: xaArray,
+            yaxes: yaArray,
+            xvals: xvalArray,
+            yvals: yvalArray,
+            hasPoints: !!gd._hoverdata,
+
+        });
+        return { result, xval, yval };
     }
 
     if(hasCartesian) {
@@ -34619,7 +34636,7 @@ exports.svgAttrs = {
 'use strict';
 
 // package version injected by `npm run preprocess`
-exports.version = '1.45.2';
+exports.version = '1.45.2-fork.2';
 
 // inject promise polyfill
 _dereq_('es6-promise').polyfill();
