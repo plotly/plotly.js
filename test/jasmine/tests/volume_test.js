320485f09a58e36dde6de446c32200477ade081f
archmoj <mojtaba@plot.ly>
2019-03-29 16:14:28 -0400

    add hovertext support

--- test/jasmine/tests/volume_test.js
+++ test/jasmine/tests/volume_test.js
@@ -399,6 +399,27 @@ describe('Test volume', function() {
                     ].join('\n')
                 });
             })
+            .then(function() {
+                return Plotly.restyle(gd, 'hovertext', [
+                    fig.data[0].value.map(function(v) { return '!! ' + v + ' !!'; })
+                ]);
+            })
+            .then(delay(20))
+            .then(_hover4)
+            .then(function() {
+                assertHoverLabelContent({
+                    nums: [
+                        'x: 0.4',
+                        'y: 0.001',
+                        'z: −2',
+                        'value: −1.26',
+                        '!! -1.26 !!'
+                    ].join('\n')
+                });
+            })
+            .then(function() {
+                return Plotly.restyle(gd, 'hovertemplate', '%{value}<br>(%{x},%{y},%{z})<extra>!!</extra>');
+            })
             .catch(failTest)
             .then(done);
         });
