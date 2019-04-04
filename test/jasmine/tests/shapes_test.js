e81942a05511e7f927611f615ad74d9b9a3f1650
Frank Jordi <frank.jordi@m-f.ch>
2019-04-04 14:50:19 +0200

    update tests

--- test/jasmine/tests/shapes_test.js
+++ test/jasmine/tests/shapes_test.js
@@ -458,7 +458,7 @@ describe('shapes axis reference changes', function() {
     }
 
     it('draws the right number of objects and updates clip-path correctly', function(done) {
-        expect(getShape(0).attr('clip-path') || '').toMatch(/x\)$/);
+        expect(getShape(0).attr('clip-path') || '').toMatch(/x\'\)$/);
 
         Plotly.relayout(gd, {
             'shapes[0].xref': 'paper',
@@ -475,7 +475,7 @@ describe('shapes axis reference changes', function() {
             });
         })
         .then(function() {
-            expect(getShape(0).attr('clip-path') || '').toMatch(/^[^x]+y2\)$/);
+            expect(getShape(0).attr('clip-path') || '').toMatch(/^[^x]+y2\'\)$/);
 
             return Plotly.relayout(gd, {
                 'shapes[0].xref': 'x',
@@ -484,7 +484,7 @@ describe('shapes axis reference changes', function() {
             });
         })
         .then(function() {
-            expect(getShape(0).attr('clip-path') || '').toMatch(/xy2\)$/);
+            expect(getShape(0).attr('clip-path') || '').toMatch(/xy2\'\)$/);
         })
         .catch(failTest)
         .then(done);
