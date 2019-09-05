41a5a83a6d2c5b82c31a9a7edd23000bd1763187
Étienne Tétreault-Pinard <etienne@plot.ly>
2019-09-05 16:41:12 -0400

    fix #4153 - reset loop variable in old/new transition prepare loops
    - this commits fixes both the `Plotly.animate` and
      the `Plotly.react` case.

--- test/jasmine/tests/animate_test.js
+++ test/jasmine/tests/animate_test.js
@@ -708,7 +708,6 @@ describe('Animate API details', function() {
 });
 
 describe('Animating multiple axes', function() {
-    'use strict';
     var gd;
 
     beforeEach(function() {
@@ -747,6 +746,74 @@ describe('Animating multiple axes', function() {
             expect(gd._fullLayout.yaxis.range).toEqual([2, 3]);
             expect(gd._fullLayout.yaxis2.range).toEqual([1, 2]);
         })
+        .catch(failTest)
+        .then(done);
+    });
+
+    it('should not leak axis update from subplot to subplot', function(done) {
+        function _animate(frameLayout) {
+            return function() {
+                return Plotly.animate(gd, {layout: frameLayout}, {
+                    frame: {redraw: false, duration: 10},
+                    transition: {duration: 10}
+                });
+            };
+        }
+
+        function _assert(msg, exp) {
+            return function() {
+                var fullLayout = gd._fullLayout;
+                for(var k in exp) {
+                    expect(fullLayout[k].range).toBeCloseToArray(exp[k], 2, msg + '| ' + k);
+                }
+            };
+        }
+
+        Plotly.plot(gd, [{
+            x: [0.1, 0.2, 0.3],
+            y: [0.4, 0.5, 0.6],
+        }, {
+            x: [0.2, 0.3, 0.4],
+            y: [0.5, 0.6, 0.7],
+            xaxis: 'x2',
+            yaxis: 'y2',
+        }, {
+            x: [0.3, 0.5, 0.7],
+            y: [0.7, 0.2, 0.2],
+            xaxis: 'x3',
+            yaxis: 'y3',
+        }], {
+            grid: {rows: 1, columns: 3, pattern: 'independent'},
+            showlegend: false
+        })
+        .then(_assert('base', {
+            xaxis: [0.0825, 0.3174], xaxis2: [0.1825, 0.417], xaxis3: [0.265, 0.7349],
+            yaxis: [0.385, 0.614], yaxis2: [0.485, 0.714], yaxis3: [0.163, 0.7366]
+        }))
+        .then(_animate({
+            xaxis: {range: [-10, 10]},
+            yaxis: {range: [-10, 10]}
+        }))
+        .then(_assert('after xy range animate', {
+            xaxis: [-10, 10], xaxis2: [0.1825, 0.417], xaxis3: [0.265, 0.7349],
+            yaxis: [-10, 10], yaxis2: [0.485, 0.714], yaxis3: [0.163, 0.7366]
+        }))
+        .then(_animate({
+            xaxis2: {range: [-20, 20]},
+            yaxis2: {range: [-20, 20]}
+        }))
+        .then(_assert('after x2y2 range animate', {
+            xaxis: [-10, 10], xaxis2: [-20, 20], xaxis3: [0.265, 0.7349],
+            yaxis: [-10, 10], yaxis2: [-20, 20], yaxis3: [0.163, 0.7366]
+        }))
+        .then(_animate({
+            xaxis3: {range: [-30, 30]},
+            yaxis3: {range: [-30, 30]}
+        }))
+        .then(_assert('after x3y3 range animate', {
+            xaxis: [-10, 10], xaxis2: [-20, 20], xaxis3: [-30, 30],
+            yaxis: [-10, 10], yaxis2: [-20, 20], yaxis3: [-30, 30]
+        }))
         .catch(failTest)
         .then(done);
     });
