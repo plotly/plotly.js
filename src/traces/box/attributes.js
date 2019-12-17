73d845ced42ec7a326f331df5dbf7da420441b3c
Étienne Tétreault-Pinard <etienne@plot.ly>
2019-12-17 17:31:53 -0500

    fix confidence interval formula in notched attr description

--- src/traces/box/attributes.js
+++ src/traces/box/attributes.js
@@ -73,7 +73,7 @@ module.exports = {
         description: [
             'Determines whether or not notches are drawn.',
             'Notches displays a confidence interval around the median.',
-            'We compute the confidence interval as median +/- 1.57 / IQR * sqrt(N),',
+            'We compute the confidence interval as median +/- 1.57 * IQR / sqrt(N),',
             'where IQR is the interquartile range and N is the sample size.',
             'If two boxes\' notches do not overlap there is 95% confidence their medians differ.',
             'See https://sites.google.com/site/davidsstatistics/home/notched-box-plots for more info.'
