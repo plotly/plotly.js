c71ca70be71e1dd4935d89d6c70e91b152c1d7cc
archmoj <mojtaba@plot.ly>
2020-02-11 09:42:09 -0500

    should not force x1=x0 and y1=y0

--- src/traces/bar/plot.js
+++ src/traces/bar/plot.js
@@ -149,13 +149,12 @@ function plot(gd, plotinfo, cdModule, traceLayer, opts, makeOnCompleteCallback) 
             );
 
             // display zeros if line.width > 0
-            if(isBlank && shouldDisplayZeros && helpers.getLineWidth(trace, di) && (isHorizontal ? x1 - x0 === 0 : y1 - y0 === 0)) {
+            if(isBlank && shouldDisplayZeros &&
+                helpers.getLineWidth(trace, di) &&
+                (isHorizontal ? x0 === x1 : y0 === y1)) {
                 isBlank = false;
             }
             di.isBlank = isBlank;
-
-            if(isBlank && isHorizontal) x1 = x0;
-            if(isBlank && !isHorizontal) y1 = y0;
 
             // in waterfall mode `between` we need to adjust bar end points to match the connector width
             if(adjustPixel && !isBlank) {
@@ -182,9 +181,9 @@ function plot(gd, plotinfo, cdModule, traceLayer, opts, makeOnCompleteCallback) 
                 mc = di.mc || trace.marker.color;
             }
 
-            var offset = d3.round((lw / 2) % 1, 2);
-
             function roundWithLine(v) {
+                var offset = d3.round((lw / 2) % 1, 2);
+
                 // if there are explicit gaps, don't round,
                 // it can make the gaps look crappy
                 return (opts.gap === 0 && opts.groupgap === 0) ?
