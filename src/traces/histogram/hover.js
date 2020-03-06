6b1b034848808bab907090028ee6ca778fc8743b
Emmanuelle Gouillart <emma@plot.ly>
2020-03-06 14:25:34 -0500

    Corrected typo in histogram hover

--- src/traces/histogram/hover.js
+++ src/traces/histogram/hover.js
@@ -27,7 +27,7 @@ module.exports = function hoverPoints(pointData, xval, yval, hovermode) {
         pointData[posLetter + 'Label'] = hoverLabelText(pointData[posLetter + 'a'], di.ph0, di.ph1);
     }
 
-    if(trace.hovermplate) pointData.hovertemplate = trace.hovertemplate;
+    if(trace.hovertemplate) pointData.hovertemplate = trace.hovertemplate;
 
     return pts;
 };
