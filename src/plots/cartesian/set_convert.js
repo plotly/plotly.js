53196e59d38f574416d8be2dc9877d265f84d30d
archmoj <mojtaba@plot.ly>
2020-03-09 09:21:25 -0400

    simplify logic - breaks are on date axes only

--- src/plots/cartesian/set_convert.js
+++ src/plots/cartesian/set_convert.js
@@ -693,16 +693,11 @@ module.exports = function setConvert(ax, fullLayout) {
         var breaksOut = [];
         if(!ax.breaks) return breaksOut;
 
-        var breaksIn;
-        if(ax.type === 'date') {
-            breaksIn = ax.breaks.slice().sort(function(a, b) {
-                if(a.pattern === '%w' && b.pattern === '%H') return -1;
-                else if(b.pattern === '%w' && a.pattern === '%H') return 1;
-                return 0;
-            });
-        } else {
-            breaksIn = ax.breaks;
-        }
+        var breaksIn = ax.breaks.slice().sort(function(a, b) {
+            if(a.pattern === '%w' && b.pattern === '%H') return -1;
+            else if(b.pattern === '%w' && a.pattern === '%H') return 1;
+            return 0;
+        });
 
         var addBreak = function(min, max) {
             min = Lib.constrain(min, r0, r1);
