c43873a6a6eacef71764861f937a0810bb06f077
Antoine Roy-Gobeil <antoine@plot.ly>
2020-03-10 12:48:52 -0400

    sankey: test multi-dimensional (node|link).customdata in hovertemplate

--- test/jasmine/tests/sankey_test.js
+++ test/jasmine/tests/sankey_test.js
@@ -817,9 +817,9 @@ describe('sankey tests', function() {
             var gd = createGraphDiv();
             var mockCopy = Lib.extendDeep({}, mock);
             mockCopy.data[0].node.customdata = [];
-            mockCopy.data[0].node.customdata[4] = 'nodeCustomdata';
+            mockCopy.data[0].node.customdata[4] = ['nodeCustomdata0', 'nodeCustomdata1'];
             mockCopy.data[0].link.customdata = [];
-            mockCopy.data[0].link.customdata[61] = 'linkCustomdata';
+            mockCopy.data[0].link.customdata[61] = ['linkCustomdata0', 'linkCustomdata1'];
 
             Plotly.plot(gd, mockCopy).then(function() {
                 _hover(404, 302);
@@ -840,15 +840,15 @@ describe('sankey tests', function() {
             // Test (node|link).hovertemplate
             .then(function() {
                 return Plotly.restyle(gd, {
-                    'node.hovertemplate': 'hovertemplate<br>%{value}<br>%{value:0.2f}<br>%{customdata}<extra>%{fullData.name}</extra>',
-                    'link.hovertemplate': 'hovertemplate<br>source: %{source.label}<br>target: %{target.label}<br>size: %{value:0.0f}TWh<br>%{customdata}<extra>%{fullData.name}</extra>'
+                    'node.hovertemplate': 'hovertemplate<br>%{value}<br>%{value:0.2f}<br>%{customdata[0]}/%{customdata[1]}<extra>%{fullData.name}</extra>',
+                    'link.hovertemplate': 'hovertemplate<br>source: %{source.label}<br>target: %{target.label}<br>size: %{value:0.0f}TWh<br>%{customdata[1]}<extra>%{fullData.name}</extra>'
                 });
             })
             .then(function() {
                 _hover(404, 302);
 
                 assertLabel(
-                    [ 'hovertemplate', '447TWh', '447.48', 'nodeCustomdata', 'trace 0'],
+                    [ 'hovertemplate', '447TWh', '447.48', 'nodeCustomdata0/nodeCustomdata1', 'trace 0'],
                     ['rgb(148, 103, 189)', 'rgb(255, 255, 255)', 13, 'Arial', 'rgb(255, 255, 255)']
                 );
             })
@@ -856,7 +856,7 @@ describe('sankey tests', function() {
                 _hover(450, 300);
 
                 assertLabel(
-                    ['hovertemplate', 'source: Solid', 'target: Industry', 'size: 46TWh', 'linkCustomdata', 'trace 0'],
+                    ['hovertemplate', 'source: Solid', 'target: Industry', 'size: 46TWh', 'linkCustomdata1', 'trace 0'],
                     ['rgb(0, 0, 96)', 'rgb(255, 255, 255)', 13, 'Arial', 'rgb(255, 255, 255)']
                 );
             })
