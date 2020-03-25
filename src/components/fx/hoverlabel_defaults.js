4ef8205120ced0858f0f2e7b2afc9a2347975f1d
Antoine Roy-Gobeil <antoine@plot.ly>
2020-03-25 18:27:19 -0400

    unified hover: remove extendFlat to handle falsy value for hoverlabel

--- src/components/fx/hoverlabel_defaults.js
+++ src/components/fx/hoverlabel_defaults.js
@@ -19,7 +19,12 @@ module.exports = function handleHoverLabelDefaults(contIn, contOut, coerce, opts
         if(!opts.bgcolor && contIn.legend) opts.bgcolor = contIn.legend.bgcolor;
         if(!opts.bordercolor && contIn.legend) opts.bordercolor = contIn.legend.bordercolor;
         // Merge in decreasing order of importance layout.font, layout.legend.font and hoverlabel.font
-        opts.font = Lib.extendFlat({}, contIn.font, contIn.legend ? contIn.legend.font : {}, opts.font);
+
+        var l = contIn.legend;
+        if(!opts.font) opts.font = {};
+        if(!opts.font.size) opts.font.size = l && l.size ? l.size : contIn.font.size;
+        if(!opts.font.family) opts.font.family = l && l.family ? l.family : contIn.font.family;
+        if(!opts.font.color) opts.font.color = l && l.color ? l.color : contIn.font.color;
     }
 
     coerce('hoverlabel.bgcolor', opts.bgcolor);
