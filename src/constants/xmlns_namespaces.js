793bbeee22effcd0c1443b05a8545d349e4465d0
archmoj <mojtaba@plot.ly>
2020-04-09 20:37:47 -0400

    refactor xmlns_namespaces

--- src/constants/xmlns_namespaces.js
+++ src/constants/xmlns_namespaces.js
@@ -1,13 +1,18 @@
 'use strict';
 
 
-exports.xmlns = 'http://www.w3.org/2000/xmlns/';
-exports.svg = 'http://www.w3.org/2000/svg';
-exports.xlink = 'http://www.w3.org/1999/xlink';
+var xmlns = 'http://www.w3.org/2000/xmlns/';
+var svg = 'http://www.w3.org/2000/svg';
+var xlink = 'http://www.w3.org/1999/xlink';
+
+exports.xmlns = xmlns;
+exports.svg = svg;
+exports.xlink = xlink;
+
 
 // the 'old' d3 quirk got fix in v3.5.7
 // https://github.com/mbostock/d3/commit/a6f66e9dd37f764403fc7c1f26be09ab4af24fed
 exports.svgAttrs = {
-    xmlns: exports.svg,
-    'xmlns:xlink': exports.xlink
+    xmlns: svg,
+    'xmlns:xlink': xlink
 };
