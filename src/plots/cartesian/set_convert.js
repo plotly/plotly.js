2087766a28159136f9828d80e9c617758d6945c7
archmoj <mojtaba@plot.ly>
2020-06-03 15:22:41 -0400

    fix milliseconds not to depend on timezone

--- src/plots/cartesian/set_convert.js
+++ src/plots/cartesian/set_convert.js
@@ -103,7 +103,10 @@ module.exports = function setConvert(ax, fullLayout) {
                 // same logic as in Lib.ms2DateTime
                 var msecTenths = Math.floor(Lib.mod(v + 0.05, 1) * 10);
                 var msRounded = Math.round(v - msecTenths / 10);
-                ms = dateTime2ms(new Date(msRounded)) + msecTenths / 10;
+
+                var d = new Date(msRounded);
+                ms = dateTime2ms(d) + msecTenths / 10;
+                ms += d.getTimezoneOffset() * ONEMIN;
             } else return BADNUM;
         }
         return ms;
