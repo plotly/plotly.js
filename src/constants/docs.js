eafc956207fea66bf0d3cc5da3f900bea4d6a21a
archmoj <mojtaba@plot.ly>
2020-07-24 19:16:40 -0400

    update doc links for d3 formatters

--- src/constants/docs.js
+++ src/constants/docs.js
@@ -9,6 +9,6 @@
 'use strict';
 
 module.exports = {
-    FORMAT_LINK: 'https://github.com/d3/d3-3.x-api-reference/blob/master/Formatting.md#d3_format',
-    DATE_FORMAT_LINK: 'https://github.com/d3/d3-3.x-api-reference/blob/master/Time-Formatting.md#format'
+    FORMAT_LINK: 'https://github.com/d3/d3-format#format',
+    DATE_FORMAT_LINK: 'https://github.com/d3/d3-time-format#locale_format'
 };
