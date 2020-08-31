8c736f76b89adad42b31d04d0198a323ee4dc612
Antoine Roy-Gobeil <antoine@plot.ly>
2020-08-31 12:24:30 -0400

    Update description of attribute source in image traces

--- src/traces/image/attributes.js
+++ src/traces/image/attributes.js
@@ -29,7 +29,7 @@ module.exports = extendFlat({
         editType: 'calc',
         description: [
             'Specifies the data URI of the image to be visualized.',
-            'The URI consists of "data:[<media type>][;base64],<data>"'
+            'The URI consists of "data:image/[<media subtype>][;base64],<data>"'
         ].join(' ')
     },
     z: {
