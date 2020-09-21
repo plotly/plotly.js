74070e0c273726ecf9fe14f3f05e8e95eda1d0be
archmoj <mojtaba@plot.ly>
2020-09-21 10:48:31 -0400

    refactor instanceOrPeriod function

--- src/plots/cartesian/instance_or_period.js
+++ src/plots/cartesian/instance_or_period.js
@@ -11,24 +11,23 @@
 var alignPeriod = require('./align_period');
 
 module.exports = function instanceOrPeriod(d, trace, axLetter) {
-    var ax = d[axLetter + 'a'];
+    var ins = [
+        d[axLetter + 'LabelVal']
+    ];
     if(trace[axLetter + 'period']) {
-        var vals = [
-            d[axLetter + 'LabelVal']
-        ];
-
+        var ax = d[axLetter + 'a'];
         var mockTrace = {};
         mockTrace[axLetter + 'period'] = trace[axLetter + 'period'];
         mockTrace[axLetter + 'period0'] = trace[axLetter + 'period0'];
         var alignment = axLetter + 'periodalignment';
 
         mockTrace[alignment] = 'start';
-        var startTime = alignPeriod(mockTrace, ax, axLetter, vals)[0];
+        var startTime = alignPeriod(mockTrace, ax, axLetter, ins)[0];
 
         mockTrace[alignment] = 'end';
-        var endTime = alignPeriod(mockTrace, ax, axLetter, vals)[0];
+        var endTime = alignPeriod(mockTrace, ax, axLetter, ins)[0];
 
-        return [startTime, endTime];
+        return [startTime, endTime]; // period: two items
     }
-    return [d[axLetter + 'LabelVal']];
+    return ins; // instance: one item
 };
