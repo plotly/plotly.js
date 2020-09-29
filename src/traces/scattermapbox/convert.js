69d158922c88d3c0ef4f9f8989cff2d8992d26cc
Jakob Jul Elben <jakobjulelben@jakobjulelbens-macbook-pro.home>
2020-09-29 10:55:12 +0200

    Fix color and sizes

--- src/traces/scattermapbox/convert.js
+++ src/traces/scattermapbox/convert.js
@@ -90,8 +90,8 @@ module.exports = function convert(gd, calcTrace) {
                 filter: ['has', 'point_count'],
                 layout: {visibility: 'visible'},
                 paint: {
-                    'circle-color': createClusterColors(trace.marker.color, trace.marker.steps),
-                    'circle-radius': createClusterSizes(trace.marker.size, trace.marker.steps)
+                    'circle-color': createClusterColors(trace.cluster.color, trace.marker.steps),
+                    'circle-radius': createClusterSizes(trace.cluster.size, trace.marker.steps)
                 },
             };
             opts.clusterCount = {
