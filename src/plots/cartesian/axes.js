plotly/plotly.js

commit a4cb02beb936e32a0cdc9b48b4cf73acc81ae856

maintain ax.tick0

src/plots/cartesian/axes.js
@@ -708,12 +708,17 @@ axes.calcTicks = function calcTicks(ax, opts) {
             }
         }
 
-        if(prevDtick !== ax.dtick) {
+        if(prevDtick !== ax.dtick && ax._tick0Init !== ax.tick0) {
+            var prevTick0 = ax.tick0;
+
             // move tick0 back
             ax.tick0 = axes.tickIncrement(ax.tick0, prevDtick, !axrev, ax.calendar);
 
             // redo first tick
             ax._tmin = axes.tickFirst(ax, opts);
+
+            // maintain tick0 value
+            ax.tick0 = prevTick0;
         }
     }
 

commit ecb506729d2e29c9c9ba212a941287f5056d1fb6

fix tick0 bug introduced in previous commit a4cb02b

src/plots/cartesian/axes.js
@@ -709,16 +709,11 @@ axes.calcTicks = function calcTicks(ax, opts) {
         }
 
         if(prevDtick !== ax.dtick && ax._tick0Init !== ax.tick0) {
-            var prevTick0 = ax.tick0;
-
             // move tick0 back
             ax.tick0 = axes.tickIncrement(ax.tick0, prevDtick, !axrev, ax.calendar);
 
             // redo first tick
             ax._tmin = axes.tickFirst(ax, opts);
-
-            // maintain tick0 value
-            ax.tick0 = prevTick0;
         }
     }
 