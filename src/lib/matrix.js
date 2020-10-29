c409a28fd91d27ae4d845dc5f4d64919a420e36b
archmoj <mojtaba@plot.ly>
2020-10-29 09:48:02 -0400

    fixup matrix for translate

--- src/lib/matrix.js
+++ src/lib/matrix.js
@@ -150,9 +150,9 @@ exports.inverseTransformMatrix = function(m) {
     var out = [];
     mat4X4.invert(out, m);
     return [
-        [out[0], out[4], out[8], out[12]],
-        [out[1], out[5], out[9], out[13]],
-        [out[2], out[6], out[10], out[14]],
-        [out[3], out[7], out[11], out[15]],
+        [out[0], out[1], out[2], out[3]],
+        [out[4], out[5], out[6], out[7]],
+        [out[8], out[9], out[10], out[11]],
+        [out[12], out[13], out[14], out[15]]
     ];
 };
