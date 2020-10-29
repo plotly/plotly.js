f4d03e4e7e29e1f1eeb1914942f1a94156b41b70
J Paterson <jpaterson@nautilusdigital.com>
2020-10-29 14:27:27 -0700

    update values passed to assertEvent use non-blocking assert syntax

--- test/jasmine/tests/gl3d_plot_interact_test.js
+++ test/jasmine/tests/gl3d_plot_interact_test.js
@@ -942,14 +942,14 @@ describe('Test gl3d drag and wheel interactions', function() {
         var relayoutCallback = jasmine.createSpy('relayoutCallback');
 
         function assertEvent(e, passive) {
-            expect(e.defaultPrevented).toEqual(!passive);
+            expect(e.defaultPrevented).not.toEqual(passive);
             relayoutCallback();
         }
 
         gd.addEventListener('touchend', function(e) { assertEvent(e, true); });
         gd.addEventListener('touchstart', function(e) { assertEvent(e, true); });
-        gd.addEventListener('touchmove', function(e) { assertEvent(e, true); });
-        gd.addEventListener('wheel', function(e) { assertEvent(e, true); });
+        gd.addEventListener('touchmove', function(e) { assertEvent(e, false); });
+        gd.addEventListener('wheel', function(e) { assertEvent(e, false); });
 
         Plotly.plot(gd, mock)
         .then(function() {
