d87320aab8b68c964ceccd203daed092678e28af
archmoj <mojtaba@plot.ly>
2020-12-08 14:34:19 -0500

    test inside ticklabels render with matches and scaleanchor

--- test/jasmine/tests/mock_test.js
+++ test/jasmine/tests/mock_test.js
@@ -84,6 +84,8 @@ var list = [
     'axes_category_null',
     'axes_chain_scaleanchor_matches',
     'axes_chain_scaleanchor_matches2',
+    'axes_chain_scaleanchor_matches_inside-ticklabels',
+    'axes_chain_scaleanchor_matches2_inside-ticklabels',
     'axes_custom-ticks_log-date',
     'axes_enumerated_ticks',
     'axes_free_default',
@@ -1172,6 +1174,8 @@ figs['axes_category_categoryarray_truncated_tails'] = require('@mocks/axes_categ
 figs['axes_category_null'] = require('@mocks/axes_category_null');
 figs['axes_chain_scaleanchor_matches'] = require('@mocks/axes_chain_scaleanchor_matches');
 figs['axes_chain_scaleanchor_matches2'] = require('@mocks/axes_chain_scaleanchor_matches2');
+figs['axes_chain_scaleanchor_matches_inside-ticklabels'] = require('@mocks/axes_chain_scaleanchor_matches_inside-ticklabels');
+figs['axes_chain_scaleanchor_matches2_inside-ticklabels'] = require('@mocks/axes_chain_scaleanchor_matches2_inside-ticklabels');
 figs['axes_custom-ticks_log-date'] = require('@mocks/axes_custom-ticks_log-date');
 figs['axes_enumerated_ticks'] = require('@mocks/axes_enumerated_ticks');
 figs['axes_free_default'] = require('@mocks/axes_free_default');
