646cb2ce1f59e225dbd5d13a82bdd1d633f34286
archmoj <mojtaba@plot.ly>
2021-01-04 12:57:44 -0500

    Revert "drop strange flag"
    This reverts commit bbe25efe84d6e91b7cf699e50c1cdc6688120435.

--- test/jasmine/tests/plot_api_react_test.js
+++ test/jasmine/tests/plot_api_react_test.js
@@ -19,7 +19,7 @@ var delay = require('../assets/delay');
 
 var MAPBOX_ACCESS_TOKEN = require('@build/credentials.json').MAPBOX_ACCESS_TOKEN;
 
-describe('Plotly.react', function() {
+describe('@noCIdep Plotly.react', function() {
     var mockedMethods = [
         'doTraceStyle',
         'doColorBars',
