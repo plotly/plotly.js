ed0195b44ce3d06ede6cdb940c467f5dd4f7e3d6
archmoj <mojtaba@plot.ly>
2021-01-20 16:47:01 -0500

    refactor compress attrs

--- tasks/compress_attributes.js
+++ tasks/compress_attributes.js
@@ -8,29 +8,30 @@ var through = require('through2');
 
 // one line string with or without trailing comma
 function makeStringRegex(attr) {
-    return attr + ': \'.*\'' + ',?';
+    return makeRegex(
+        attr + ': \'.*\'' + ',?'
+    );
 }
 
 // joined array of strings with or without trailing comma
 function makeJoinedArrayRegex(attr) {
-    return attr + ': \\[[\\s\\S]*?\\]' + '\\.join\\(.*' + ',?';
+    return makeRegex(
+        attr + ': \\[[\\s\\S]*?\\]' + '\\.join\\(.*' + ',?'
+    );
 }
 
 // array with or without trailing comma
 function makeArrayRegex(attr) {
-    return attr + ': \\[[\\s\\S]*?\\]' + ',?';
+    return makeRegex(
+        attr + ': \\[[\\s\\S]*?\\]' + ',?'
+    );
 }
 
-// ref: http://www.regexr.com/3cmac
-var regexStr = [
-    makeStringRegex('description'),
-    makeJoinedArrayRegex('description'),
-    makeArrayRegex('requiredOpts'),
-    makeArrayRegex('otherOpts'),
-    makeStringRegex('hrName')
-].join('|');
-
-var regex = new RegExp(regexStr, 'g');
+function makeRegex(regexStr) {
+    return (
+        new RegExp(regexStr, 'g')
+    );
+}
 
 module.exports = function() {
     var allChunks = [];
@@ -38,7 +39,15 @@ module.exports = function() {
         allChunks.push(chunk);
         next();
     }, function(done) {
-        this.push(Buffer.concat(allChunks).toString().replace(regex, ''));
+        var str = Buffer.concat(allChunks).toString('utf-8');
+        this.push(
+            str
+                .replace(makeStringRegex('description'), '')
+                .replace(makeJoinedArrayRegex('description'), '')
+                .replace(makeArrayRegex('requiredOpts'), '')
+                .replace(makeArrayRegex('otherOpts'), '')
+                .replace(makeStringRegex('hrName'), '')
+        );
         done();
     });
 };
