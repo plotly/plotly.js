4e09afdc88507f7ddd4a8980732500afcbd8444f
archmoj <mojtaba@plot.ly>
2021-02-02 21:18:44 -0500

    add major version after latest for CDN to leave v1 unchanged

--- tasks/stats.js
+++ tasks/stats.js
@@ -7,6 +7,10 @@ var prettySize = require('prettysize');
 var common = require('./util/common');
 var constants = require('./util/constants');
 var pkgVersion = require('../package.json').version;
+var majorVersion = pkgVersion.split('.')[0];
+var theLatest = 'latest' + (
+    (majorVersion === '1') ? '' : ('-v' + majorVersion)
+);
 
 var pathDistREADME = path.join(constants.pathToDist, 'README.md');
 var cdnRoot = 'https://cdn.plot.ly/plotly-';
@@ -74,7 +78,7 @@ function getInfoContent() {
         'Plotly.js defaults to US English (en-US) and includes British English (en) in the standard bundle.',
         'Many other localizations are available - here is an example using Swiss-German (de-CH),',
         'see the contents of this directory for the full list.',
-        'They are also available on our CDN as ' + cdnRoot + 'locale-de-ch-' + 'latest' + '.js OR ' + cdnRoot + 'locale-de-ch-' + pkgVersion + '.js',
+        'They are also available on our CDN as ' + cdnRoot + 'locale-de-ch-' + theLatest + '.js OR ' + cdnRoot + 'locale-de-ch-' + pkgVersion + '.js',
         'Note that the file names are all lowercase, even though the region is uppercase when you apply a locale.',
         '',
         '*After* the plotly.js script tag, add:',
@@ -109,12 +113,12 @@ function getMainBundleInfo() {
         '',
         'It be can imported as minified javascript',
         '- using dist file `dist/plotly.min.js`',
-        '- using CDN URL ' + cdnRoot + 'latest' + MINJS + ' OR ' + cdnRoot + pkgVersion + MINJS,
+        '- using CDN URL ' + cdnRoot + theLatest + MINJS + ' OR ' + cdnRoot + pkgVersion + MINJS,
         '',
         'or as raw javascript:',
         '- using the `plotly.js-dist` npm package (starting in `v1.39.0`)',
         '- using dist file `dist/plotly.js`',
-        '- using CDN URL ' + cdnRoot + 'latest' + JS + ' OR ' + cdnRoot + pkgVersion + JS,
+        '- using CDN URL ' + cdnRoot + theLatest + JS + ' OR ' + cdnRoot + pkgVersion + JS,
         '- using CommonJS with `require(\'plotly.js\')`',
         '',
         'If you would like to have access to the attribute meta information ' +
@@ -185,8 +189,8 @@ function makeBundleInfo(pathObj) {
         '',
         '| Flavor | URL |',
         '| ------ | --- |',
-        '| Latest | ' + cdnRoot + name + '-' + 'latest' + JS + ' |',
-        '| Latest minified | ' + cdnRoot + name + '-' + 'latest' + MINJS + ' |',
+        '| Latest | ' + cdnRoot + name + '-' + theLatest + JS + ' |',
+        '| Latest minified | ' + cdnRoot + name + '-' + theLatest + MINJS + ' |',
         '| Tagged | ' + cdnRoot + name + '-' + pkgVersion + JS + ' |',
         '| Tagged minified | ' + cdnRoot + name + '-' + pkgVersion + MINJS + ' |',
         '',
