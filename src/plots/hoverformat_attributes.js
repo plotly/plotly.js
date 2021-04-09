8c33d50a8daabab795fc29bcd545342ebaae13c2
archmoj <mojtaba@plot.ly>
2021-04-09 15:12:57 -0400

    point to date formatting options on (x|y|z) axes

--- src/plots/hoverformat_attributes.js
+++ src/plots/hoverformat_attributes.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var FORMAT_LINK = require('../constants/docs').FORMAT_LINK;
+var docs = require('../constants/docs')
+var FORMAT_LINK = docs.FORMAT_LINK;
+var DATE_FORMAT_LINK = docs.DATE_FORMAT_LINK;
 
-module.exports = function axisHoverFormat(x, mockedAxis) {
+module.exports = function axisHoverFormat(x, noDates) {
     return {
         valType: 'string',
         dflt: '',
@@ -10,9 +12,13 @@ module.exports = function axisHoverFormat(x, mockedAxis) {
         description: [
             'Sets the hover text formatting rule for `' + x + '`',
             ' using d3 formatting mini-languages which are very similar to those in Python.',
-            'See: ' + FORMAT_LINK,
+            'See: ' + FORMAT_LINK + (
+                noDates ?
+                    '' :
+                    ' And for dates see: ' + DATE_FORMAT_LINK
+                ),
             'By default the values are formatted using ' + (
-                mockedAxis ?
+                noDates ?
                     'generic number format' :
                     ('`' + x + 'axis.hoverformat`')
             ) + '.',
