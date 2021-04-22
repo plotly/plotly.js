c4370065e54a5fd13426255768c50eb3621c5fa6
LucaVazz <luca.vazzano@gmx.net>
2021-04-22 18:10:22 +0200

    extend choropleth tests to include translate3d transform (#5525)

--- test/jasmine/tests/choropleth_test.js
+++ test/jasmine/tests/choropleth_test.js
@@ -167,7 +167,6 @@ describe('Test choropleth hover:', function() {
 
     function run(hasCssTransform, pos, fig, content, style) {
         gd = createGraphDiv();
-        var scale = 1;
 
         style = style || {
             bgcolor: 'rgb(68, 68, 68)',
@@ -177,14 +176,8 @@ describe('Test choropleth hover:', function() {
             fontFamily: 'Arial'
         };
 
-        return Plotly.newPlot(gd, fig)
-        .then(function() {
-            if(hasCssTransform) {
-                scale = 0.5;
-                transformPlot(gd, 'translate(-25%, -25%) scale(0.5)');
-            }
-
-            mouseEvent('mousemove', scale * pos[0], scale * pos[1]);
+        function assertHoverLabel(posX, posY) {
+            mouseEvent('mousemove', posX, posY);
             assertHoverLabelContent({
                 nums: content[0],
                 name: content[1]
@@ -193,6 +186,18 @@ describe('Test choropleth hover:', function() {
                 d3Select('g.hovertext'),
                 style
             );
+        }
+
+        return Plotly.newPlot(gd, fig)
+        .then(function() {
+            if(hasCssTransform) {
+                transformPlot(gd, 'translate3d(10px, 10px, 0) scale(1)');
+                assertHoverLabel(pos[0] + 10, pos[1] + 10)
+                transformPlot(gd, 'translate(-25%, -25%) scale(0.5)');
+                assertHoverLabel(0.5 * pos[0], 0.5 * pos[1])
+            } else {
+                assertHoverLabel(pos[0], pos[1])
+            }
         });
     }
 
