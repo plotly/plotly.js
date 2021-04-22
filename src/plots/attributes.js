d87ee126e88997e4e0cc44d62dc1bf9a646eebe8
archmoj <mojtaba@plot.ly>
2021-04-22 13:23:15 -0400

    update legendrank description

--- src/plots/attributes.js
+++ src/plots/attributes.js
@@ -46,8 +46,8 @@ module.exports = {
         dflt: 1000,
         editType: 'style',
         description: [
-            'Sets the legend rank for this trace in each `legendgroup`.',
-            'Items with smaller ranks would be presented on top/left side while',
+            'Sets the legend rank for this trace.',
+            'Items and groups with smaller ranks would be presented on top/left side while',
             'with `*reversed* `legend.traceorder` they would be on bottom/right side.',
             'The default legendrank is 1000,',
             'so that you can use ranks less than 1000 to place certain items before all unranked items,',
