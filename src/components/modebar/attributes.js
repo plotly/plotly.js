117e39ca81045e9231de8bd7813123415ef76caa
archmoj <mojtaba@plot.ly>
2021-05-14 15:01:27 -0400

    organize modebar attributes and defaults and register it

---
+++ src/components/modebar/attributes.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+    editType: 'modebar',
+
+    modebar: {
+        editType: 'modebar',
+
+        orientation: {
+            valType: 'enumerated',
+            values: ['v', 'h'],
+            dflt: 'h',
+            editType: 'modebar',
+            description: 'Sets the orientation of the modebar.'
+        },
+        bgcolor: {
+            valType: 'color',
+            editType: 'modebar',
+            description: 'Sets the background color of the modebar.'
+        },
+        color: {
+            valType: 'color',
+            editType: 'modebar',
+            description: 'Sets the color of the icons in the modebar.'
+        },
+        activecolor: {
+            valType: 'color',
+            editType: 'modebar',
+            description: 'Sets the color of the active or hovered on icons in the modebar.'
+        },
+        uirevision: {
+            valType: 'any',
+            editType: 'none',
+            description: [
+                'Controls persistence of user-driven changes related to the modebar,',
+                'including `hovermode`, `dragmode`, and `showspikes` at both the',
+                'root level and inside subplots. Defaults to `layout.uirevision`.'
+            ].join(' ')
+        },
+    }
+};
---
+++ src/components/modebar/defaults.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Lib = require('../../lib');
+var Color = require('../color');
+var attributes = require('./attributes');
+
+module.exports = function supplyLayoutDefaults(layoutIn, layoutOut) {
+    function coerce(attr, dflt) {
+        return Lib.coerce(layoutIn, layoutOut, attributes, attr, dflt);
+    }
+
+    coerce('modebar.orientation');
+    coerce('modebar.bgcolor', Color.addOpacity(layoutOut.paper_bgcolor, 0.5));
+    var defaultColor = Color.contrast(Color.rgb(layoutOut.modebar.bgcolor));
+    coerce('modebar.color', Color.addOpacity(defaultColor, 0.3));
+    coerce('modebar.activecolor', Color.addOpacity(defaultColor, 0.7));
+    coerce('modebar.uirevision', layoutOut.uirevision);
+};
