a9da031d22b9c77b152d4ccdb1688c548f88e0d4
LucaVazz <luca.vazzano@gmx.net>
2021-05-26 15:26:56 +0200

    Revert "extend choropleth tests to include translate3d transform (#5525)"
    This reverts commit c4370065e54a5fd13426255768c50eb3621c5fa6.

--- test/jasmine/tests/choropleth_test.js
+++ test/jasmine/tests/choropleth_test.js
@@ -167,6 +167,7 @@ describe('Test choropleth hover:', function() {
 
     function run(hasCssTransform, pos, fig, content, style) {
         gd = createGraphDiv();
+        var scale = 1;
 
         style = style || {
             bgcolor: 'rgb(68, 68, 68)',
@@ -176,8 +177,14 @@ describe('Test choropleth hover:', function() {
             fontFamily: 'Arial'
         };
 
-        function assertHoverLabel(posX, posY) {
-            mouseEvent('mousemove', posX, posY);
+        return Plotly.newPlot(gd, fig)
+        .then(function() {
+            if(hasCssTransform) {
+                scale = 0.5;
+                transformPlot(gd, 'translate(-25%, -25%) scale(0.5)');
+            }
+
+            mouseEvent('mousemove', scale * pos[0], scale * pos[1]);
             assertHoverLabelContent({
                 nums: content[0],
                 name: content[1]
@@ -186,18 +193,6 @@ describe('Test choropleth hover:', function() {
                 d3Select('g.hovertext'),
                 style
             );
-        }
-
-        return Plotly.newPlot(gd, fig)
-        .then(function() {
-            if(hasCssTransform) {
-                transformPlot(gd, 'translate3d(10px, 10px, 0) scale(1)');
-                assertHoverLabel(pos[0] + 10, pos[1] + 10);
-                transformPlot(gd, 'translate(-25%, -25%) scale(0.5)');
-                assertHoverLabel(0.5 * pos[0], 0.5 * pos[1]);
-            } else {
-                assertHoverLabel(pos[0], pos[1]);
-            }
         });
     }
 
