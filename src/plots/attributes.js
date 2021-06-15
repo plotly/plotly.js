a1f87b9dbe93a83e3f91da4f3599b22e9a4c166e
archmoj <mojtaba@plot.ly>
2021-06-15 18:26:28 -0400

    would be > are

--- src/plots/attributes.js
+++ src/plots/attributes.js
@@ -47,8 +47,8 @@ module.exports = {
         editType: 'style',
         description: [
             'Sets the legend rank for this trace.',
-            'Items and groups with smaller ranks would be presented on top/left side while',
-            'with `*reversed* `legend.traceorder` they would be on bottom/right side.',
+            'Items and groups with smaller ranks are presented on top/left side while',
+            'with `*reversed* `legend.traceorder` they are on bottom/right side.',
             'The default legendrank is 1000,',
             'so that you can use ranks less than 1000 to place certain items before all unranked items,',
             'and ranks greater than 1000 to go after all unranked items.'
