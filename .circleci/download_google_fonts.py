21e262f95dc2c84dfc6e62f0f761ad746f8dc5ea
archmoj <mojtaba@plot.ly>
2021-06-23 19:00:18 -0400

    download Roboto fonts

--- .circleci/download_google_fonts.py
+++ .circleci/download_google_fonts.py
@@ -1,20 +1,33 @@
 import requests
 
-dirOut = '.circleci/fonts/truetype/googleFonts'
+dirOut = '.circleci/fonts/truetype/googleFonts/'
 
-family = 'Raleway'
-repo = 'https://github.com/impallari/' + family + '/blob/master/fonts/v3.000%20Fontlab/TTF/'
+def download(repo, family, types) :
+    for t in types :
+        name = family + t + '.ttf'
+        url = repo + name + '?raw=true'
+        print(url)
+        req = requests.get(url, allow_redirects=True)
+        open(dirOut + name, 'wb').write(req.content)
 
-types = [
-    '-Regular',
-    '-Regular-Italic',
-    '-Bold',
-    '-Bold-Italic'
-]
+download(
+    'https://github.com/impallari/Raleway/blob/master/fonts/v3.000%20Fontlab/TTF/',
+    'Raleway',
+    [
+        '-Regular',
+        '-Regular-Italic',
+        '-Bold',
+        '-Bold-Italic'
+    ]
+)
 
-for t in types :
-    name = family + t + '.ttf'
-    url = repo + name + '?raw=true'
-    print(url)
-    req = requests.get(url, allow_redirects=True)
-    open(dirOut + name, 'wb').write(req.content)
+download(
+    'https://github.com/googlefonts/roboto/blob/main/src/hinted/',
+    'Roboto',
+    [
+        '-Regular',
+        '-Italic',
+        '-Bold',
+        '-BoldItalic'
+    ]
+)
