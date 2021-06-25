36d0bf045f0c1ebdf4a07057699216672e1a42a4
archmoj <mojtaba@plot.ly>
2021-06-25 10:51:51 -0400

    download Dosis font for image test

--- .circleci/download_google_fonts.py
+++ .circleci/download_google_fonts.py
@@ -11,6 +11,15 @@ def download(repo, family, types) :
         open(dirOut + name, 'wb').write(req.content)
 
 download(
+    'https://github.com/eliheuer/dosis-vf/blob/master/fonts/static-fonts/',
+    'Dosis',
+    [
+        '-Regular',
+        '-Bold'
+    ]
+)
+
+download(
     'https://github.com/googlefonts/noto-fonts/blob/main/hinted/ttf/NotoSansMono/',
     'NotoSansMono',
     [
