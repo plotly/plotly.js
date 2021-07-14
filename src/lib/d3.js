109c23440e6811dc1b4c047dcc7fdc66e8942467
archmoj <mojtaba@plot.ly>
2021-07-14 11:21:18 -0400

    add d3.round to d3 object

--- src/lib/d3.js
+++ src/lib/d3.js
@@ -1,3 +1,9 @@
 'use strict';
 
-module.exports = {};
+var d3 = require('d3');
+
+d3.round = function(x, n) { // copy of d3.round from v3
+    return n ? Math.round(x * (n = Math.pow(10, n))) / n : Math.round(x);
+};
+
+module.exports = d3;
