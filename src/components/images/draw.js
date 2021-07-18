d1755e88180e47ab3382395c92b85581c56933de
archmoj <mojtaba@plot.ly>
2021-07-17 21:09:23 -0400

    layout images enter and exit adjustment

--- src/components/images/draw.js
+++ src/components/images/draw.js
@@ -204,12 +204,13 @@ module.exports = function draw(gd) {
     }
 
     var imagesBelow = fullLayout._imageLowerLayer.selectAll('image')
-        .data(imageDataBelow);
+        .data(imageDataBelow)
+        .enter()
+        .append('image');
     var imagesAbove = fullLayout._imageUpperLayer.selectAll('image')
-        .data(imageDataAbove);
-
-    imagesBelow.enter().append('image');
-    imagesAbove.enter().append('image');
+        .data(imageDataAbove)
+        .enter()
+        .append('image');
 
     imagesBelow.exit().remove();
     imagesAbove.exit().remove();
@@ -235,9 +236,10 @@ module.exports = function draw(gd) {
         var imagesOnSubplot = subplotObj.imagelayer.selectAll('image')
             // even if there are no images on this subplot, we need to run
             // enter and exit in case there were previously
-            .data(imageDataSubplot[subplot] || []);
+            .data(imageDataSubplot[subplot] || [])
+            .enter()
+            .append('image');
 
-        imagesOnSubplot.enter().append('image');
         imagesOnSubplot.exit().remove();
 
         imagesOnSubplot.each(function(d) {
