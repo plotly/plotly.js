86b94997183bce9d27169de05795cbc32cacb60e
archmoj <mojtaba@plot.ly>
2021-07-17 21:11:27 -0400

    titles enter and exit adjustment

--- src/components/titles/index.js
+++ src/components/titles/index.js
@@ -101,18 +101,22 @@ function draw(gd, titleClass, options) {
     }
 
     var el = group.selectAll('text')
-        .data(elShouldExist ? [0] : []);
-    el.enter().append('text');
-    el.text(txt)
+        .data(elShouldExist ? [0] : [])
+        .enter()
+        .append('text');
+
+    el.exit().remove();
+
+    if(!elShouldExist) return group;
+
+    el
+        .text(txt)
         // this is hacky, but convertToTspans uses the class
         // to determine whether to rotate mathJax...
         // so we need to clear out any old class and put the
         // correct one (only relevant for colorbars, at least
         // for now) - ie don't use .classed
         .attr('class', titleClass);
-    el.exit().remove();
-
-    if(!elShouldExist) return group;
 
     function titleLayout(titleEl) {
         Lib.syncOrAsync([drawTitle, scootTitle], titleEl);
@@ -142,16 +146,16 @@ function draw(gd, titleClass, options) {
             opacity: opacity * Color.opacity(fontColor),
             'font-weight': Plots.fontWeight
         })
-        .attr(attributes)
-        .call(svgTextUtils.convertToTspans, gd);
+        .attrs(attributes)
+        .each(function() { svgTextUtils.convertToTspans(d3.select(this), gd); });
 
         return Plots.previousPromises(gd);
     }
 
     function scootTitle(titleElIn) {
-        var titleGroup = d3.select(titleElIn.node().parentNode);
-
         if(avoid && avoid.selection && avoid.side && txt) {
+            var titleGroup = d3.select(titleElIn.node().parentNode);
+
             titleGroup.attr('transform', null);
 
             // move toward avoid.side (= left, right, top, bottom) if needed
