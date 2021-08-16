600eee1a19e97b993117f565a549326535c59e07
Roni Choudhury <roni.choudhury@kitware.com>
2021-08-16 15:29:15 -0400

    Remove unnecessary constants

--- src/plots/smith/constants.js
+++ src/plots/smith/constants.js
@@ -18,17 +18,4 @@ module.exports = {
         'angular-axis',
         'radial-axis'
     ],
-
-    radialDragBoxSize: 50,
-    angularDragBoxSize: 30,
-    cornerLen: 25,
-    cornerHalfWidth: 2,
-
-    // pixels to move mouse before you stop clamping to starting point
-    MINDRAG: 8,
-    // smallest radial distance [px] allowed for a zoombox
-    MINZOOM: 20,
-    // distance [px] off (r=0) or (r=radius) where we transition
-    // from single-sided to two-sided radial zoom
-    OFFEDGE: 20
 };
