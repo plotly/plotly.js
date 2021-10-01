2a4a8a342d7d4a2054809b16e48e5f39c2230c64
archmoj <mojtaba@plot.ly>
2021-10-01 18:38:40 -0400

    clear copied TODO comment from polar

--- src/traces/scattersmith/attributes.js
+++ src/traces/scattersmith/attributes.js
@@ -63,11 +63,6 @@ module.exports = {
     }),
     fillcolor: scatterAttrs.fillcolor,
 
-    // TODO error bars
-    // https://stackoverflow.com/a/26597487/4068492
-    // error_x (error_r, error_imag)
-    // error_y
-
     hoverinfo: extendFlat({}, baseAttrs.hoverinfo, {
         flags: ['real', 'imag', 'text', 'name']
     }),
