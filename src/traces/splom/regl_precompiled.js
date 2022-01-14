c5624ee8ebb43d11f98315070c7b8711438ff435
Yujin Wu <yujinwunz+code@gmail.com>
2022-01-14 18:25:33 +1100

    uncommitted generated traces

--- src/traces/splom/regl_precompiled.js
+++ src/traces/splom/regl_precompiled.js
@@ -1,1 +1,19 @@
-module.exports = {};
+var v0 = require('../../generated/regl-codegen/7a5bb1985cc7db0a7544e23c660c9dc27a2e4053572e95d457a7e52fd4980997');
+var v1 = require('../../generated/regl-codegen/8a43b073e4f3e9c0e499c8ac9c253f2aa1e3d3de2febfccc6526b52295dbf770');
+var v2 = require('../../generated/regl-codegen/ff8495670417d5d0e4caa9942ad63b4dff0242a390ac1cb807c78ca326d6f3ce');
+var v3 = require('../../generated/regl-codegen/13c0ae156483f2bcbd7ff29404f0abfd8689ff43f41791a6c7469868690a4260');
+var v4 = require('../../generated/regl-codegen/19769c875db736c08a744c0a6aabe28276ed06aa24fdb7c36506a9f4c1f56f13');
+var v5 = require('../../generated/regl-codegen/21cec01aa93887c70e86d7f1bc84d6837da0b1f5c1ff4cadd42ac6eb37f9f316');
+var v6 = require('../../generated/regl-codegen/f9448a214a3e3cd439b767559aa71a4d1ccf5a8f39b8b756973e71acd33ff956');
+var v7 = require('../../generated/regl-codegen/0919c57b995304312da30a5af7873a319bfb7b7e22ff6b4fa203ecbd5774ebfc');
+
+module.exports = {
+  "7a5bb1985cc7db0a7544e23c660c9dc27a2e4053572e95d457a7e52fd4980997": v0,
+  "8a43b073e4f3e9c0e499c8ac9c253f2aa1e3d3de2febfccc6526b52295dbf770": v1,
+  "ff8495670417d5d0e4caa9942ad63b4dff0242a390ac1cb807c78ca326d6f3ce": v2,
+  "13c0ae156483f2bcbd7ff29404f0abfd8689ff43f41791a6c7469868690a4260": v3,
+  "19769c875db736c08a744c0a6aabe28276ed06aa24fdb7c36506a9f4c1f56f13": v4,
+  "21cec01aa93887c70e86d7f1bc84d6837da0b1f5c1ff4cadd42ac6eb37f9f316": v5,
+  "f9448a214a3e3cd439b767559aa71a4d1ccf5a8f39b8b756973e71acd33ff956": v6,
+  "0919c57b995304312da30a5af7873a319bfb7b7e22ff6b4fa203ecbd5774ebfc": v7
+};
