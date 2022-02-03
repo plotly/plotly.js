cd654978fb0e387477f1be5bb3d172b80c00b297
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2022-02-03 15:44:25 -0500

    no effect when array tickmode

--- src/plots/cartesian/layout_attributes.js
+++ src/plots/cartesian/layout_attributes.js
@@ -423,7 +423,7 @@ module.exports = {
             'A value of 2 means shows every 2nd level.',
             'A larger value n means only every nth tick is labeled.',
             '`tick0` determines which labels are shown.',
-            'Not implemented for an axis with `type` of *log* or *multicategory*.'
+            'Not implemented for an axis with `type` of *log* or *multicategory* and when `tickmode` is *array*.'
         ].join(' ')
     },
     tickvals: {
