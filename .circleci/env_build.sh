9be5d47e2832c461147b92c74431a71d2802957c
Yujin Wu <yujinwunz+code@gmail.com>
2022-02-11 02:16:26 +1100

    [temporary] try without npm ls

--- .circleci/env_build.sh
+++ .circleci/env_build.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-sudo apt-add-repository universe
-sudo apt update
-sudo apt install python2-minimal
-
-echo python2 version: `python2 -V`
-
 export NODE_OPTIONS='--max-old-space-size=4096' && \
 echo "node version: $(node --version)" && \
 echo "npm version: $(npm --version)" && \
 npm ci && \
-npm ls --prod --all
+echo "temporarily not do `npm ls`"
+# npm ls --prod --all
