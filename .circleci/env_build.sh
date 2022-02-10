75893821df0b00531eb8e94684504900a81f1a18
Yujin Wu <yujinwunz+code@gmail.com>
2022-02-11 00:30:50 +1100

    .

--- .circleci/env_build.sh
+++ .circleci/env_build.sh
@@ -3,7 +3,7 @@ sudo apt-add-repository universe
 sudo apt update
 sudo apt install python2-minimal
 
-echo python2 version: `python2 -v`
+echo python2 version: `python2 -V`
 
 export NODE_OPTIONS='--max-old-space-size=4096' && \
 echo "node version: $(node --version)" && \
