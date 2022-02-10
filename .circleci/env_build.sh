b1b2866688bab912bfdd50c7c11794a32b8ae337
Yujin Wu <yujinwunz+code@gmail.com>
2022-02-11 00:16:09 +1100

    try installing python2.7 first

--- .circleci/env_build.sh
+++ .circleci/env_build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+sudo apt-add-repository universe
+sudo apt update
+sudo apt install python2-minimal
+
+echo python2 version: `python2 -v`
+
 export NODE_OPTIONS='--max-old-space-size=4096' && \
 echo "node version: $(node --version)" && \
 echo "npm version: $(npm --version)" && \
