300a2c045892c172d49c8812d104d0e159b314b1
Yujin Wu <yujinwunz+code@gmail.com>
2022-03-08 20:00:12 +1100

    adjusted package.lock versioning to work with npm 16

--- .circleci/env_build.sh
+++ .circleci/env_build.sh
@@ -3,5 +3,4 @@ export NODE_OPTIONS='--max-old-space-size=4096' && \
 echo "node version: $(node --version)" && \
 echo "npm version: $(npm --version)" && \
 npm ci && \
-echo "temporarily not do `npm ls`"
 npm ls --prod --all
