550707649fe18125abe1da70db1dffbfa36c7b9c
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2022-07-08 09:26:09 -0400

    adjust controllers for tiny rect selections

--- src/components/shapes/display_outlines.js
+++ src/components/shapes/display_outlines.js
@@ -250,8 +250,8 @@ module.exports = function displayOutlines(polygons, outlines, dragOptions, nCall
                     var dx = nextPoint[1] - x;
                     var dy = nextPoint[2] - y;
 
-                    var width = dy ? 5 : 25;
-                    var height = dy ? 25 : 5;
+                    var width = dy ? 5 : Math.max(Math.min(25, Math.abs(dx) - 5), 5);
+                    var height = dx ? 5 : Math.max(Math.min(25, Math.abs(dy) - 5), 5);
 
                     vertex.classed(dy ? 'cursor-ew-resize' : 'cursor-ns-resize', true)
                         .attr('width', width)
