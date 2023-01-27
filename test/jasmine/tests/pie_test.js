3dae10955bf5da8fa8a8fb65ce48c040853ad7a7
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2023-01-27 10:33:45 -0500

    adjust pie test

--- test/jasmine/tests/pie_test.js
+++ test/jasmine/tests/pie_test.js
@@ -972,7 +972,7 @@ describe('Pie traces', function() {
             textposition: [['outside', 'outside', 'inside', 'inside', 'outside']],
             'outsidetextfont.size': 12
         }, {
-            t: '~=', l: 'shrunk',
+            t: 'grew', l: 'shrunk',
             b: 'grew', r: 'grew'
         }))
         .then(check('automargin:false', {automargin: false}, {
