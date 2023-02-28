2df73c437b9bc47155d913b09fb81a08837fbd2e
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2023-02-28 16:42:15 -0500

    add b64 image test

---
+++ test/image/convert_b64.py
@@ -0,0 +1,99 @@
+from sqlite3 import NotSupportedError
+import numpy
+import base64
+
+plotlyjsTypes = {
+    'int8': 'i1',
+    'uint8': 'u1',
+    'int16': 'i2',
+    'uint16': 'u2',
+    'int32': 'i4',
+    'uint32': 'u4',
+    'float32': 'f4',
+    'float64': 'f8'
+}
+
+# List mainly including keys with type of 'info_array'
+# Simply having two items the b64 option is not supported by plotly.js
+# This list also includes cases of type 'any' that we don't to be converted
+# e.g. in mapbox layers or geojson
+rangeKeys = [
+    'geojson',
+    'layers',
+    'subplots',
+    'xaxes',
+    'yaxes',
+    'domain',
+    'range',
+    'dtickrange',
+    'constraintrange',
+    'bounds',
+    'parallels',
+    'sector',
+    'span',
+    'zmin',
+    'zmax',
+    'args',
+    'args2',
+    'values',
+    'groups',
+    'selectedpoints',
+    'opacityscale',
+    'cameraposition' # deprecated
+]
+
+def arraysToB64(obj, newObj) :
+    for key, val in obj.items() :
+        if key in rangeKeys :
+            newObj[key] = val
+        elif isinstance(val, dict) :
+            newObj[key] = dict()
+            arraysToB64(val, newObj[key])
+        elif isinstance(val, list) :
+            try :
+                arr = numpy.array(val)
+            except Exception :
+                newObj[key] = val
+                continue
+
+            if arr.dtype == 'object' :
+                newList = list()
+                for v in val :
+                    if isinstance(v, dict) :
+                        newList.append(arraysToB64(v, dict()))
+                    else :
+                        newList.append(v)
+
+                newObj[key] = newList
+            else :
+                # skip converting arrays with 2 items or less
+                if(arr.ndim == 1 and arr.shape[0] < 3) :
+                    newObj[key] = val
+                    continue
+
+                # convert Big Ints until we could support them in plotly.js
+                if str(arr.dtype) == 'int64' :
+                    arr = arr.astype(numpy.int32)
+                elif str(arr.dtype) == 'uint64' :
+                    arr = arr.astype(numpy.uint32)
+
+                if str(arr.dtype) in plotlyjsTypes :
+                    newObj[key] = {
+                        'dtype': plotlyjsTypes[str(arr.dtype)],
+                        'bdata': base64.b64encode(arr).decode('ascii')
+                    }
+
+                    if(arr.ndim > 1) :
+                        newObj[key]['shape'] = str(arr.shape)[1:-1]
+
+                    #print(val)
+                    #print('____________________')
+                    #print(newObj[key])
+                    #print('____________________')
+                else :
+                    newObj[key] = val
+
+        else :
+            newObj[key] = val
+
+    return newObj
