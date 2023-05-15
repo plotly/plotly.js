1bb2b82e21c3858c333f0f33144c6be48f4d899f
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2023-05-15 18:58:16 +0200

    Fix heatmap/image traces without zsmooth on IOS
    Co-authored-by: Alex Johnson <alex@plot.ly>

--- src/lib/supports_pixelated_image.js
+++ src/lib/supports_pixelated_image.js
@@ -15,7 +15,7 @@ function supportsPixelatedImage() {
     if(_supportsPixelated !== null) { // only run the feature detection once
         return _supportsPixelated;
     }
-    if(Lib.isIE() || Lib.isSafari()) {
+    if(Lib.isIE() || Lib.isSafari() || lib.isIOS()) {
         // NB. Safari passes the test below but the final rendering is not pixelated
         _supportsPixelated = false;
     } else {
