fd93796c7f5a4ff28c9d7521e8883273431e2d0b
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2023-06-05 10:47:02 -0400

    fix pattern colors for pie and sunburst

--- src/traces/pie/fill_one.js
+++ src/traces/pie/fill_one.js
@@ -1,16 +1,13 @@
 'use strict';
 
 var Drawing = require('../../components/drawing');
+var Color = require('../../components/color');
 
 module.exports = function fillOne(s, pt, trace, gd) {
-    // enforce the point color, when colors (with s) & the pattern shape are missing.
-    // 'abuse' the color attribute, used in the Drawing component for bar trace type.
-    var marker = trace.marker;
-    if(marker.pattern) {
-        if(!marker.colors || !marker.pattern.shape) marker.color = pt.color;
+    var pattern = trace.marker.pattern;
+    if(pattern && pattern.shape) {
+        Drawing.pointStyle(s, trace, gd, pt);
     } else {
-        marker.color = pt.color;
+        Color.fill(s, pt.color);
     }
-
-    Drawing.pointStyle(s, trace, gd, pt);
 };
