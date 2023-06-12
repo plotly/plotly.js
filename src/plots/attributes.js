bcf7dde4bc03f5a87b72c133c5c9e44e6e0adf85
Liam Connors <connorsl@tcd.ie>
2023-06-12 10:26:48 -0400

    Update legendrank description

--- src/plots/attributes.js
+++ src/plots/attributes.js
@@ -78,7 +78,7 @@ module.exports = {
         description: [
             'Sets the legend rank for this trace.',
             'Items and groups with smaller ranks are presented on top/left side while',
-            'with `*reversed* `legend.traceorder` they are on bottom/right side.',
+            'with *reversed* `legend.traceorder` they are on bottom/right side.',
             'The default legendrank is 1000,',
             'so that you can use ranks less than 1000 to place certain items before all unranked items,',
             'and ranks greater than 1000 to go after all unranked items.'
