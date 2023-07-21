6fb9c6509385974a32c5a4a1bb79d2254ec68f1f
Alistair-Welch <alistair.welch@jdplc.com>
2023-07-21 12:43:40 +0100

    initial commit, single scatter plot

---
+++ src/plot_api/accessibility.js
@@ -0,0 +1,48 @@
+import {c2mChart} from "chart2music";
+
+export function enable(gd) {
+
+    var fullLayout = gd._fullLayout;
+    var fullData = gd._fullData;
+
+    const c2mData = {};
+
+    for(var i = 0; i < fullData.length; i++) {
+        var trace = fullData[i];
+        if(trace.type === 'scatter') {
+            c2mData[i] = trace.y;
+        }
+        else {
+            Lib.warn('Accessibility not implemented for trace type: ' + trace.type);
+            return;
+        }
+    }
+    
+    var closed_captions = document.createElement('div');
+    closed_captions.id = 'cc';
+    gd.appendChild(closed_captions);
+
+    c2mChart({
+        title: fullLayout.title.text ?? "",
+        type: "line",
+        axes: {
+            x: {
+            label: fullLayout.xaxis.title.text ?? ""
+            },
+            y: {
+            label: fullLayout.yaxis.title.text ?? ""
+            }
+        },
+        element: gd,
+        cc: closed_captions,
+        data: c2mData,
+        options: {
+            onFocusCallback: ({slice, index}) => {
+            Plotly.Fx.hover(gd, [{
+                curveNumber: slice, 
+                pointNumber: index
+            }])
+            }
+        }
+    });      
+};
