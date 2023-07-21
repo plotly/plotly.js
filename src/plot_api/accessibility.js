d37d24f04e99b8aba492a453c069bdeeb8f654ef
Alistair-Welch <alistair.welch@jdplc.com>
2023-07-21 14:10:47 +0100

    data and trace labels

--- src/plot_api/accessibility.js
+++ src/plot_api/accessibility.js
@@ -7,10 +7,21 @@ export function enable(gd) {
 
     const c2mData = {};
 
+    var labels = [];
     for(var i = 0; i < fullData.length; i++) {
         var trace = fullData[i];
         if(trace.type === 'scatter') {
-            c2mData[i] = trace.y;
+            var traceData = [];
+            for(var p = 0; p < trace.y.length; p++) {
+                traceData.push(
+                    {
+                        x: trace.x[p],
+                        y: trace.y[p],
+                        label: trace.text[p]
+                    })
+            }
+            c2mData[trace.name ?? i] = traceData;
+            labels.push(trace.name ?? i);
         }
         else {
             Lib.warn('Accessibility not implemented for trace type: ' + trace.type);
@@ -23,6 +34,8 @@ export function enable(gd) {
     closed_captions.className = 'closed_captions';
     gd.appendChild(closed_captions);
 
+    Lib.warn(fullLayout);
+    Lib.warn(fullData);
     c2mChart({
         title: fullLayout.title.text ?? "",
         type: "line",
@@ -40,7 +53,7 @@ export function enable(gd) {
         options: {
             onFocusCallback: ({slice, index}) => {
             Plotly.Fx.hover(gd, [{
-                curveNumber: slice, 
+                curveNumber: labels.indexOf(slice), 
                 pointNumber: index
             }])
             },
