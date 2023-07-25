cdcd86eb4a9a0e08230295110cf15c10e954fbec
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2023-07-25 12:46:08 -0400

    2.25.0

--- dist/plotly-geo-assets.js
+++ dist/plotly-geo-assets.js
@@ -1,5 +1,5 @@
 /**
-* plotly.js v2.24.3
+* plotly.js v2.25.0
 * Copyright 2012-2023, Plotly, Inc.
 * All rights reserved.
 * Licensed under the MIT license
@@ -24,7 +24,7 @@ return /******/ (function() { // webpackBootstrap
 
 
 // package version injected by `npm run preprocess`
-exports.version = '2.24.3';
+exports.version = '2.25.0';
 
 /***/ }),
 
