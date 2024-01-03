a788ccc50cffb50d5fcf3a1b103a8f61104afcfa
Andrew Pikul <ajpikul@gmail.com>
2024-01-03 09:47:06 -0500

    Add test for full-domain

--- test/jasmine/tests/pikul_test.js
+++ test/jasmine/tests/pikul_test.js
@@ -5,7 +5,7 @@ var createGraphDiv = require('../assets/create_graph_div');
 var destroyGraphDiv = require('../assets/destroy_graph_div');
 
 // Boilerplate taken from axes_test.js
-describe('When generating axes w/ `tickmode`:"domain array",', function() {
+describe('Generating ticks with `tickmode`,', function() {
   var gd;
 
   beforeEach(function() {
@@ -18,20 +18,25 @@ describe('When generating axes w/ `tickmode`:"domain array",', function() {
   // Passed as tickLen argument to specify a major or minor tick config
   const MAJOR = 10;
   const MINOR = 5; 
-  function generateTickConfig(tickLen){
+  function generateTickConfig(tickLen, tickmode, nticks){
+    if (tickmode === undefined) tickmode = 'domain array';
     // Intentionally configure to produce a single `(x|y)tick` class per tick
     // labels and tick marks each produce one, so one or the other
-    standardConfig = {tickmode: 'domain array', ticklen: tickLen, showticklabels: false};
+    standardConfig = {tickmode: tickmode, ticklen: tickLen, showticklabels: false};
 
     // Tick values will be random:
-    var n = Math.floor(Math.random() * 100);
-    tickVals = [];
+    if (tickmode === 'domain array') {
+      var n = Math.floor(Math.random() * 100);
+      tickVals = [];
 
-    for(let i = 0; i <= n; i++){
-      intermediate = (Math.trunc(Math.random()*150) - 25) / 100; // Number between -.25 and 1.25 w/ 2 decimals max
-      tickVals.push(Math.min(Math.max(intermediate, 0), 1)); // 2 decimal number between 0 and 1 w/ higher odds of 0 or 1
+      for(let i = 0; i <= n; i++){
+        intermediate = (Math.trunc(Math.random()*150) - 25) / 100; // Number between -.25 and 1.25 w/ 2 decimals max
+        tickVals.push(Math.min(Math.max(intermediate, 0), 1)); // 2 decimal number between 0 and 1 w/ higher odds of 0 or 1
+      }
+      standardConfig['tickvals'] = tickVals;
+    } else if (tickmode === 'full domain') {
+      standardConfig['nticks'] = nticks;
     }
-    standardConfig['tickvals'] = tickVals;
     return standardConfig;
   }
   
@@ -63,9 +68,9 @@ describe('When generating axes w/ `tickmode`:"domain array",', function() {
         { type:'date'},
         { type:'category'},
       ];
-      for (let graphTypeIndex = 0; graphTypeIndex < graphTypes.length; graphTypeIndex++) {
+      for(let graphTypeIndex = 0; graphTypeIndex < graphTypes.length; graphTypeIndex++) {
           (function(tickConfig, xGraphType) { // wrap in func or else it() can't see variable because of javascript closure scope
-              it('fraction mapping to geometries for config ' + binaryToTickType(tickConfig) , function(done) {
+              it('"domain array" and config  ' + binaryToTickType(tickConfig) , function(done) {
                   var xMajor = tickConfig & XMAJOR; // does this config include xmajor?
                   var xMinor = tickConfig & XMINOR; // does this config include xminor?
                   var yMajor = tickConfig & YMAJOR; // ... etc
@@ -188,6 +193,148 @@ describe('When generating axes w/ `tickmode`:"domain array",', function() {
                       }
                   }).then(done, done.fail);
               }); 
+              for(let nticks_param = 0; nticks_param < 5; nticks_param++) {
+                  (function(nticks_param) { 
+                      it('"full domain" and config  ' + binaryToTickType(tickConfig) , function(done) {
+                          var xMajor = tickConfig & XMAJOR; // does this config include xmajor?
+                          var xMinor = tickConfig & XMINOR; // does this config include xminor?
+                          var yMajor = tickConfig & YMAJOR; // ... etc
+                          var yMinor = tickConfig & YMINOR;
+                          ticksOff = {ticklen: 0, showticklabels: false};
+                          var xMajorConfig = xMajor ? generateTickConfig(MAJOR, 'full domain', nticks_param) : ticksOff; // generate configs
+                          var xMinorConfig = xMinor ? generateTickConfig(MINOR, 'full domain', nticks_param) : ticksOff;
+                          var yMajorConfig = yMajor ? generateTickConfig(MAJOR, 'full domain', nticks_param) : ticksOff;
+                          var yMinorConfig = yMinor ? generateTickConfig(MINOR, 'full domain', nticks_param) : ticksOff;
+                          var configInfo = "" // for debugging
+                          configInfo += xMajor ? "\n " + `xMajor nticks: ${xMajorConfig['nticks']}` : "";
+                          configInfo += xMinor ? "\n " + `xMinor nticks: ${xMinorConfig['nticks']}` : "";
+                          configInfo += yMajor ? "\n " + `yMajor nticks: ${yMajorConfig['nticks']}` : "";
+                          configInfo += yMinor ? "\n " + `yMinor nticks: ${yMinorConfig['nticks']}` : "";
+
+                          // stolen from axes_test.js
+                          Plotly.newPlot(gd, {
+                              data: [{
+                                  x: [0, 1],
+                                  y: [0, 1]
+                              }],
+                              layout: {
+                                  width: 400,
+                                  height: 400,
+                                  margin: { t: 40, b: 40, l: 40, r: 40, },
+                                  ...xGraphType,
+                                  xaxis: {
+                                      autorange: true,
+                                      ...xMajorConfig, // explode config into this key
+                                      minor: xMinorConfig, // set config to this key
+                                  },
+                                  yaxis: { // same as above
+                                      autorange: true,
+                                      ...yMajorConfig,
+                                      minor: yMinorConfig,
+                                  },
+                          }}).then(function() {
+                              // This regex is for extracting geometric position of a tick
+                              // regex: `.source` converts to string, laid out this way to make for easier reading
+                              const funcName = "translate" + /\(/.source; // literally simplest way to regex '('
+                              const integerPart = /\d+/.source; // numbers left of decimal
+                              const fractionalPart = /(?:\.\d+)?/.source; // decimal + numbers to right
+                              const floatNum = integerPart + fractionalPart; // all together
+                              const any = /.+/.source;
+                              const close = /\)/.source;
+                              const reX = new RegExp(funcName + '(' + floatNum + '),' + any + close); // parens () are capture not fn()
+                              const reY = new RegExp(funcName + any + ',(' + floatNum + ')' + close);
+
+                              for(let runNumber = 0b1; runNumber <= 0b1000; runNumber <<= 0b1) { // Check all ticks on all axes ☺
+                                  var runInfo = "\n Checking: " + binaryToTickType(runNumber);
+                                  var elementName = "";
+                                  var targetConfig;
+                                  var re;
+                                  if(runNumber & xMajor) { // ie. (this run wants xMajor) & (xMajor was set in config above)
+                                      elementName = "xtick";
+                                      targetConfig = xMajorConfig;
+                                      re = reX;
+                                  } else if(runNumber & xMinor) {
+                                      elementName = "xtick";
+                                      targetConfig = xMinorConfig;
+                                      re = reX;
+                                  } else if(runNumber & yMajor) {
+                                      elementName = "ytick";
+                                      targetConfig = yMajorConfig;
+                                      re = reY;
+                                  } else if(runNumber & yMinor) {
+                                      elementName = "ytick";
+                                      targetConfig = yMinorConfig;
+                                      re = reY;
+                                  } else continue; // This run would have been to check ticks that don't exist
+                                  
+                                  var tickElements = document.getElementsByClassName(elementName);
+                                  var nt = targetConfig['nticks'];
+                                  var expectedTickLen = String(targetConfig['ticklen'])
+                                  var tickValsUnique = new Array(); 
+                                  if (nt == 0) {
+                                    // pass
+                                  } else if (nt == 1) {
+                                      tickValsUnique = [0];
+                                  } else if (nt == 2) {
+                                      tickValsUnique = [0, 1];
+                                  } else {
+                                      var increment = 1/(nt-1); // (nt-2) + 1
+                                      tickValsUnique.push(0);
+                                      for (let i = 0; i < nt-2; i++) {
+                                          tickValsUnique.push((i+1)*increment);
+                                      }
+                                      tickValsUnique.push(1);
+                                  }
+                                  // Filter out major/minor and grab geometry
+                                  transformVals = []; // "transform" ie the positional property
+                                  for(let i = 0; i < tickElements.length; i++) {
+                                      if(!tickElements[i].getAttribute("d").endsWith(expectedTickLen)) continue;
+                                      var translate = tickElements[i].getAttribute("transform");
+                                      var match = translate.match(re);
+                                      if (match === null) continue;
+                                      transformVals.push(Number(match[1]));
+                                  }
+                                  
+                                  var debugInfo = "\n " + `tickElements: (${tickElements.length}) ${tickElements}` + "\n " +
+                                    `nticks: ${tickValsUnique.length}`;
+                                  
+                                  expect(transformVals.length).toBe(tickValsUnique.length, 
+                                    "filtered tick elements vs tickvals failed" + runInfo + configInfo + debugInfo);
+                                  
+                                  if(transformVals.length < 2) return; // Can't test proportions with < 2 ticks (since no fixed reference)
+
+                                  
+                                  // To test geometries without using fixed point or data values...
+                                  // we can check consistency of y = mx+b! (y is DOM position, x is proportion)
+                                  // If x = 0 then y = b, but we may not have a 0 valued x
+                                  // m = (y1 - y2) / (x1 - x2)
+                                  // b = y1 - mx1
+                                  y = transformVals;
+                                  x = tickValsUnique;
+                                  var m, b;
+                                  var b_index = x.indexOf(0);
+                                  
+                                  m = (y[0] - y[1]) / (x[0] - x[1]);
+                                  b = (b_index != -1) ? b = y[b_index] : y[0] - m*x[0];
+                                  
+                                  calculatedY = [];
+                                  for(let i = 0; i < x.length; i++) calculatedY.push(m*x[i] + b);
+
+                                  /* **** Close this comment line to manually inspect output --> 
+                                  yout = [];
+                                  ycalcout = [];
+                                  for (i = 0; i < Math.min(x.length, 10); i++) {
+                                       yout.push(Number.parseFloat(y[i]).toFixed(2));
+                                       ycalcout.push(Number.parseFloat(calculatedY[i]).toFixed(2));
+                                  }
+                                  console.log(yout);
+                                  console.log(ycalcout);/* */
+                                  expect(y).toBeCloseToArray(calculatedY, 1, `y=mx+b test failed comparing\n${y}\n${calculatedY}`);
+                              }
+                          }).then(done, done.fail);
+                      }); 
+                  })(nticks_param);
+              }
           })(tickConfig, graphTypes[graphTypeIndex]);
       }
   }
