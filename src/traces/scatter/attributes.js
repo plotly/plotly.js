9b12d3ef3ca5e58d0dbd8be5be1f567735d94c89
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2024-03-01 14:55:57 -0500

    move makeFillcolorAttr to a file

--- src/traces/scatter/attributes.js
+++ src/traces/scatter/attributes.js
@@ -13,21 +13,7 @@ var constants = require('./constants');
 
 var extendFlat = require('../../lib/extend').extendFlat;
 
-var makeFillcolorAttr = function(hasFillgradient) {
-    return {
-        valType: 'color',
-        editType: 'style',
-        anim: true,
-        description: [
-            'Sets the fill color.',
-            'Defaults to a half-transparent variant of the line color,',
-            'marker color, or marker line color, whichever is available.',
-            hasFillgradient ?
-                'If fillgradient is specified, fillcolor is ignored except for setting the background color of the hover label, if any.' :
-                ''
-        ].join(' ')
-    };
-}
+var makeFillcolorAttr = require('./fillcolor_attribute');
 
 function axisPeriod(axis) {
     return {
