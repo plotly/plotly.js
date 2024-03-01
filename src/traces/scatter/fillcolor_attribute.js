4e5adb837c9aaff39ce8e2ac068a1301869b9b22
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2024-03-01 15:08:46 -0500

    adjust fillcolor function and use it in traces

--- src/traces/scatter/fillcolor_attribute.js
+++ src/traces/scatter/fillcolor_attribute.js
@@ -8,10 +8,11 @@ module.exports = function makeFillcolorAttr(hasFillgradient) {
         description: [
             'Sets the fill color.',
             'Defaults to a half-transparent variant of the line color,',
-            'marker color, or marker line color, whichever is available.',
-            hasFillgradient ?
-                'If fillgradient is specified, fillcolor is ignored except for setting the background color of the hover label, if any.' :
-                ''
+            'marker color, or marker line color, whichever is available.' + (
+                hasFillgradient ?
+                    ' If fillgradient is specified, fillcolor is ignored except for setting the background color of the hover label, if any.' :
+                    ''
+            )
         ].join(' ')
     };
 };
