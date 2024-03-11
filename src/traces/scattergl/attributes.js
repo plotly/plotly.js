497b5f1dcaca1c2aef9552ab9e071db860996a96
Farkites <mario.rodriguez@plot.ly>
2024-03-11 16:02:54 +0000

    Remove scattergl zindex

--- src/traces/scattergl/attributes.js
+++ src/traces/scattergl/attributes.js
@@ -101,4 +101,3 @@ var attrs = module.exports = overrideAll({
 attrs.x.editType = attrs.y.editType = attrs.x0.editType = attrs.y0.editType = 'calc+clearAxisTypes';
 attrs.hovertemplate = scatterAttrs.hovertemplate;
 attrs.texttemplate = scatterAttrs.texttemplate;
-attrs.zindex = scatterAttrs.zindex
