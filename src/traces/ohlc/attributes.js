abfd1a8cd94a09392347c650a978bbd9334c74b7
Farkites <mario.rodriguez@plot.ly>
2024-03-11 17:31:48 +0000

    Add zindex for ohlc

--- src/traces/ohlc/attributes.js
+++ src/traces/ohlc/attributes.js
@@ -133,4 +133,15 @@ module.exports = {
             ].join(' ')
         }
     }),
+
+    zindex: {
+        valType: 'integer',
+        dflt: 0,
+        editType: 'calc',
+        description: [
+            'Sets the layer on which this trace is displayed, relative to ',
+            'other traces on the same axes. Traces with higher `zindex` ',
+            'appear in front of those with lower `zindex`.'
+        ].join(' ')
+    }
 };
