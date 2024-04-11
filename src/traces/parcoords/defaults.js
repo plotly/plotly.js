666b10ef1a2244cb765e294544529d1a715d6e6e
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2024-04-11 07:08:06 -0400

    parcoords

--- src/traces/parcoords/defaults.js
+++ src/traces/parcoords/defaults.js
@@ -96,6 +96,10 @@ module.exports = function supplyDefaults(traceIn, traceOut, defaultColor, layout
     // make default font size 10px (default is 12),
     // scale linearly with global font size
     var fontDflt = {
+        weight: layout.font.weight,
+        style: layout.font.style,
+        stretch: layout.font.stretch,
+        variant: layout.font.variant,
         family: layout.font.family,
         size: Math.round(layout.font.size / 1.2),
         color: layout.font.color
