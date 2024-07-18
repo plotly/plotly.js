ffba61e588ca6d133f5ae4ab4c0e436204a5bcc8
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2024-07-18 09:01:44 -0400

    rename new mocks

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -102,7 +102,7 @@ case $1 in
         ;;
 
     make-baselines-mathjax3)
-        python3 test/image/make_baseline.py mathjax3    legend_mathjax_title_and_items mathjax parcats_grid_subplots table_latex_multitrace_scatter table_plain_birds table_wrapped_birds ternary-mathjax zz-ternary-mathjax-title-place-subtitle || EXIT_STATE=$?
+        python3 test/image/make_baseline.py mathjax3    legend_mathjax_title_and_items mathjax parcats_grid_subplots table_latex_multitrace_scatter table_plain_birds table_wrapped_birds ternary-mathjax ternary-mathjax-title-place-subtitle || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
 
