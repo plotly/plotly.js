935be965cd93a7315c11cdc5589dd3a851a35048
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2024-08-21 18:50:30 -0400

    Revert "Revert "inline d3.round in drawing/symbol_defs.js""
    This reverts commit d7fd65cc648725ef90840c5e3adb1e5625e270fc.

--- src/components/drawing/symbol_defs.js
+++ src/components/drawing/symbol_defs.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var parseSvgPath = require('parse-svg-path');
-var round = require('@plotly/d3').round;
+var round = // require('@plotly/d3').round;
+    function(x, n) {
+        return n ? Math.round(x * (n = Math.pow(10, n))) / n : Math.round(x);
+    };
 
 /** Marker symbol definitions
  * users can specify markers either by number or name
