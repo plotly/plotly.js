001457c188fdf0688bf60f09442478f0a67ae23b
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2024-09-26 11:05:00 -0400

    Revert "draw grids only in the mainplot"
    This reverts commit 653fe773a23dffd03e42c42003a381ed273b1c57.

--- src/plots/cartesian/index.js
+++ src/plots/cartesian/index.js
@@ -568,7 +568,7 @@ function makeSubplotLayer(gd, plotinfo) {
     var yLayer = constants.layerValue2layerClass[plotinfo.yaxis.layer];
     var hasOnlyLargeSploms = fullLayout._hasOnlyLargeSploms;
 
-    if(!plotinfo.mainplot) {
+    if(!plotinfo.mainplot || fullLayout._zindices.length > 1) {
         if(hasOnlyLargeSploms) {
             // TODO could do even better
             // - we don't need plot (but we would have to mock it in lsInner
