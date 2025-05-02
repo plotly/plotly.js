f7f5a03e3da32b496b618d5248717d63b1c8526f
Emily KL <4672118+emilykl@users.noreply.github.com>
2025-05-02 16:47:58 -0400

    Update Plotly version

--- .circleci/env_image.sh
+++ .circleci/env_image.sh
@@ -5,6 +5,6 @@ sudo python3 .circleci/download_google_fonts.py && \
 sudo cp -r .circleci/fonts/ /usr/share/ && \
 sudo fc-cache -f && \
 # install kaleido & plotly
-sudo python3 -m pip install kaleido==1.0.0rc13 plotly==5.5.0 --progress-bar off
+sudo python3 -m pip install "plotly[kaleido]==6.1.0rc0" --progress-bar off
 # install numpy i.e. to convert arrays to typed arrays
 sudo python3 -m pip install numpy==1.24.2
