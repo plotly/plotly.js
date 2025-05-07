66328b431708dc7be543c1bd427d1ebfbe2fae19
My-Tien Nguyen <my-tien@users.noreply.github.com>
2025-05-07 13:04:57 +0200

    Add support for `ticklabelposition` "inside"/"outside" for category axes with `tickson` set to "boundaries".
    "top", "bottom", "left", "right" are ignored if `tickson` is set to "boundaries".

--- src/plots/cartesian/layout_attributes.js
+++ src/plots/cartesian/layout_attributes.js
@@ -663,12 +663,13 @@ module.exports = {
         dflt: 'outside',
         editType: 'calc',
         description: [
-            'Determines where tick labels are drawn with respect to the axis',
+            'Determines where tick labels are drawn with respect to the axis.',
             'Please note that',
-            'top or bottom has no effect on x axes or when `ticklabelmode` is set to *period*.',
-            'Similarly',
-            'left or right has no effect on y axes or when `ticklabelmode` is set to *period*.',
-            'Has no effect on *multicategory* axes or when `tickson` is set to *boundaries*.',
+            'top or bottom has no effect on x axes or when `ticklabelmode` is set to *period*',
+            'or when `tickson` is set to *boundaries*. Similarly,',
+            'left or right has no effect on y axes or when `ticklabelmode` is set to *period*',
+            'or when `tickson` is set to *boundaries*.',
+            'Has no effect on *multicategory* axes.',
             'When used on axes linked by `matches` or `scaleanchor`,',
             'no extra padding for inside labels would be added by autorange,',
             'so that the scales could match.'
