ebb7b1210856c5f7b6fc9e0ea4ec256bfea54d37
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-28 14:00:48 -0500

    Add custom CSS class "modebar--custom"

--- src/components/modebar/modebar.js
+++ src/components/modebar/modebar.js
@@ -45,7 +45,7 @@ proto.update = function(graphInfo, buttons) {
     this.element.setAttribute('id', modeBarId);
     this.element.setAttribute('role', 'toolbar');
     this._uid = modeBarId;
-    this.element.className = 'modebar';
+    this.element.className = 'modebar modebar--custom';
     if(context.displayModeBar === 'hover') this.element.className += ' modebar--hover ease-bg';
 
     if(fullLayout.modebar.orientation === 'v') {
