14298d85891f6c2f9f496b83263acce4b29710d8
Emily KL <4672118+emilykl@users.noreply.github.com>
2025-05-29 15:38:48 -0400

    typo

--- .circleci/env_image.sh
+++ .circleci/env_image.sh
@@ -10,6 +10,6 @@ sudo fc-cache -f
 # Once new Kaleido and Plotly versions are released, uncomment the line above, update the Plotly version,
 # and delete the two lines below.
 sudo python3 -m pip install "git+https://github.com/plotly/plotly.py.git@6837831" --progress-bar off
-sudo python3 -m pip install "'git+https://github.com/plotly/Kaleido.git@2a4bfa2#subdirectory=src/py'" --progress-bar off
+sudo python3 -m pip install "git+https://github.com/plotly/Kaleido.git@2a4bfa2#subdirectory=src/py" --progress-bar off
 # install numpy i.e. to convert arrays to typed arrays
 sudo python3 -m pip install numpy==1.24.2
