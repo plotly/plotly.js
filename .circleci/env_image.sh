fae9f08a63a53d9df88cc408acaaf3106e7bb3ad
Emily KL <4672118+emilykl@users.noreply.github.com>
2025-05-30 12:37:08 -0400

    try python3 without sudo

--- .circleci/env_image.sh
+++ .circleci/env_image.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
+python3 --version
 # install required fonts
 sudo apt-get install fonts-liberation2 fonts-open-sans fonts-noto-cjk fonts-noto-color-emoji
-python .circleci/download_google_fonts.py
+python3 .circleci/download_google_fonts.py
 sudo cp -r .circleci/fonts/ /usr/share/
 sudo fc-cache -f
 # install kaleido & plotly
 # python -m pip install "plotly[kaleido]==6.1.2" --progress-bar off
 # Once new Kaleido and Plotly versions are released, uncomment the line above, update the Plotly version,
 # and delete the two lines below.
-python -m pip install "git+https://github.com/plotly/plotly.py.git@6837831" --progress-bar off
-python -m pip install "git+https://github.com/plotly/Kaleido.git@2a4bfa2#subdirectory=src/py" --progress-bar off
+python3 -m pip install "git+https://github.com/plotly/plotly.py.git@6837831" --progress-bar off
+python3 -m pip install "git+https://github.com/plotly/Kaleido.git@2a4bfa2#subdirectory=src/py" --progress-bar off
 # install numpy i.e. to convert arrays to typed arrays
-python -m pip install numpy==1.24.2
+python3 -m pip install numpy==1.24.2
