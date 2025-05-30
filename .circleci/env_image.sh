ecb2c0c5d90456ac49e370ae5843e7251d092f1a
Emily KL <4672118+emilykl@users.noreply.github.com>
2025-05-30 11:32:29 -0400

    Revert "use python instead of python3"
    This reverts commit 279af99d47030a57b7a902638181534bf6818be5.

--- .circleci/env_image.sh
+++ .circleci/env_image.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
 # install required fonts
-python --version
 sudo apt-get install fonts-liberation2 fonts-open-sans fonts-noto-cjk fonts-noto-color-emoji
-sudo python .circleci/download_google_fonts.py
+sudo python3 .circleci/download_google_fonts.py
 sudo cp -r .circleci/fonts/ /usr/share/
 sudo fc-cache -f
 # install kaleido & plotly
-# sudo python -m pip install "plotly[kaleido]==6.1.2" --progress-bar off
+# sudo python3 -m pip install "plotly[kaleido]==6.1.2" --progress-bar off
 # Once new Kaleido and Plotly versions are released, uncomment the line above, update the Plotly version,
 # and delete the two lines below.
-sudo python -m pip install "git+https://github.com/plotly/plotly.py.git@6837831" --progress-bar off
-sudo python -m pip install "git+https://github.com/plotly/Kaleido.git@2a4bfa2#subdirectory=src/py" --progress-bar off
+sudo python3 -m pip install "git+https://github.com/plotly/plotly.py.git@6837831" --progress-bar off
+sudo python3 -m pip install "git+https://github.com/plotly/Kaleido.git@2a4bfa2#subdirectory=src/py" --progress-bar off
 # install numpy i.e. to convert arrays to typed arrays
-sudo python -m pip install numpy==1.24.2
+sudo python3 -m pip install numpy==1.24.2
