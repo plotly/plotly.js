12f73ad05098a32e8c2c8057fcc552ca7df84cc1
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2025-06-20 10:38:13 -0400

    download CSV file

--- test/jasmine/performance_tests/assets/post_process.js
+++ test/jasmine/performance_tests/assets/post_process.js
@@ -1,15 +1,27 @@
-'use strict';
-
 exports.writeRawDataAsCSV = function(traceName, allTests) {
+    var str = '';
     for(var k = 0; k < allTests.length; k++) {
         var test = allTests[k];
 
-        var str = traceName + ',' + test.n + '\n';
+        str += traceName + ',' + test.n + '\n';
         str += 'id,time(ms)\n';
         for(var i = 0; i < test.raw.length; i++) {
             str += i + ',' + test.raw[i] + '\n';
         }
+        str += '\n';
 
         console.log(str);
     }
+
+    // download a CSV file
+    var a = document.createElement('a');
+    var myBlob = new Blob([str], {type: 'text/plain'})
+    var url = window.URL.createObjectURL(myBlob);
+    a.href = url;
+    a.download = traceName + '.csv';
+    a.style.display = 'none';
+    document.body.append(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
 };
