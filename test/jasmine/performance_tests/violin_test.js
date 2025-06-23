94b2fe039a2636d3db145eba2f1b37b03693b191
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2025-06-23 10:53:48 -0400

    add violin tests

---
+++ test/jasmine/performance_tests/violin_test.js
@@ -0,0 +1,87 @@
+var createGraphDiv = require('../assets/create_graph_div');
+var delay = require('../assets/delay');
+var d3SelectAll = require('../../strict-d3').selectAll;
+var Plotly = require('../../../lib/core');
+var PlotlyViolin = require('../../../lib/violin');
+var writeRawDataAsCSV = require('./assets/post_process').writeRawDataAsCSV;
+
+var gd = createGraphDiv();
+
+var tests = [{
+    n: 1000
+}, {
+    n: 2000
+}, {
+    n: 4000
+}, {
+    n: 8000
+}, {
+    n: 16000
+}, {
+    n: 32000
+}, {
+    n: 64000
+}];
+
+tests.forEach(function(spec, index) {
+    describe('Performance test violin | size:' + spec.n, function() {
+        'use strict';
+
+        Plotly.register(PlotlyViolin);
+
+        const samples = Array.from({ length: 9 }, (_, i) => i);
+        const nTimes = samples.length - 1;
+
+        var y = Float64Array.from({ length: spec.n }, (_, i) => i * Math.cos(Math.sqrt(i)));
+
+        var mock = {
+            data: [{
+                type: 'violin',
+                points: 'all',
+                y: y
+            }],
+            layout: {
+                width: 900,
+                height: 400
+            }
+        };
+
+        var startTime, endTime;
+
+        beforeEach(function(done) {
+            startTime = performance.now();
+
+            // Wait for actual rendering to complete
+            requestAnimationFrame(function() {
+                requestAnimationFrame(function() {
+                    endTime = performance.now();
+                    done();
+                });
+            });
+
+            Plotly.newPlot(gd, mock);
+        });
+
+        afterEach(function(done) {
+            delay(100)().then(done);
+        });
+
+        samples.forEach(function(t) {
+            it('should graph violin traces | turn: ' + t, function() {
+                var delta = endTime - startTime;
+
+                if(t === 0) {
+                    tests[index].raw = [];
+                }
+                tests[index].raw[t] = delta;
+
+                var nodes = d3SelectAll('g.trace.violins');
+                expect(nodes.size()).toEqual(1);
+
+                if(t === nTimes && index === tests.length - 1) {
+                    writeRawDataAsCSV('violin', tests);
+                }
+            });
+        });
+    });
+});
