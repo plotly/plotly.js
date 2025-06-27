d92d35647198426f6b19205eae9f2c26a88e7888
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2025-06-26 20:23:53 -0400

    test extra points

--- test/jasmine/performance_tests/assets/test_cases.js
+++ test/jasmine/performance_tests/assets/test_cases.js
@@ -1,7 +1,7 @@
 var tests = [];
 
 for(let traceType of ['image', 'heatmap', 'contour']) {
-    for(let m of [10, 20, 40, 80, 160, 320, 640]) {
+    for(let m of [10, 20, 40, 80, 160, 320, 640, 1280]) {
         let nx = 5 * m;
         let ny = 2 * m;
         tests.push({
@@ -16,10 +16,13 @@ for(let traceType of ['image', 'heatmap', 'contour']) {
     }
 }
 
+var allN = [/*1000, 2000, 4000, 8000, 16000, */32000, 64000, 128000];
+var allNTraces = [1/*, 10, 100*/]
+
 for(let traceType of ['box', 'violin']) {
     for(let mode of ['no_points', 'all_points']) {
-        for(let nTraces of [1, 10, 100]) {
-            for(let n of [1000, 2000, 4000, 8000, 16000, 32000]) {
+        for(let nTraces of allNTraces) {
+            for(let n of allN) {
                 tests.push({
                     n:n,
                     nTraces: nTraces,
@@ -38,8 +41,8 @@ for(let traceType of ['box', 'violin']) {
 
 for(let traceType of ['scatter', 'scattergl', 'scattergeo']) {
     for(let mode of ['markers', 'lines', 'markers+lines']) {
-        for(let nTraces of [1, 10, 100]) {
-            for(let n of [1000, 2000, 4000, 8000, 16000, 32000]) {
+        for(let nTraces of allNTraces) {
+            for(let n of allN) {
                 tests.push({
                     n:n,
                     nTraces: nTraces,
@@ -57,8 +60,8 @@ for(let traceType of ['scatter', 'scattergl', 'scattergeo']) {
 
 for(let traceType of ['bar', 'histogram']) {
     for(let mode of ['group', 'stack', 'overlay']) {
-        for(let nTraces of [1, 10, 100]) {
-            for(let n of [1000, 2000, 4000, 8000, 16000, 32000]) {
+        for(let nTraces of allNTraces) {
+            for(let n of allN) {
                 tests.push({
                     n:n,
                     nTraces: nTraces,
