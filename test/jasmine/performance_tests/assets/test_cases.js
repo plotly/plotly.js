ae35d0095393d312dce70a278d021b8d5db08f9a
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2025-07-02 14:12:00 -0400

    test data points between 64K to 128K with 1K step

--- test/jasmine/performance_tests/assets/test_cases.js
+++ test/jasmine/performance_tests/assets/test_cases.js
@@ -1,5 +1,6 @@
 var tests = [];
 
+/*
 for(let traceType of ['image', 'heatmap', 'contour']) {
     for(let m of [10, 20, 40, 80, 160, 320, 640, 1280]) {
         let nx = 5 * m;
@@ -15,12 +16,23 @@ for(let traceType of ['image', 'heatmap', 'contour']) {
         });
     }
 }
+*/
 
-var allN = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000];
-var allNTraces = [1, /*10, */100]
+function fillArrayFromToBy(start, end, step) {
+  const result = [];
+  for (let i = start; i <= end; i += step) {
+    result.push(i);
+  }
+  return result;
+}
 
-for(let traceType of ['box', 'violin']) {
-    for(let mode of ['no_points', 'all_points']) {
+//var allN = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000];
+var allN = fillArrayFromToBy(64000, 128000, 1000);
+//var allNTraces = [1, /*10, */100]
+var allNTraces = [1]
+
+for(let traceType of [/*'box', */'violin']) {
+    for(let mode of [/*'no_points', */'all_points']) {
         for(let nTraces of allNTraces) {
             for(let n of allN) {
                 tests.push({
@@ -40,7 +52,7 @@ for(let traceType of ['box', 'violin']) {
 }
 
 for(let traceType of ['scatter', 'scattergl', 'scattergeo']) {
-    for(let mode of ['markers', 'lines', 'markers+lines']) {
+    for(let mode of [/*'markers', 'lines', */'markers+lines']) {
         for(let nTraces of allNTraces) {
             for(let n of allN) {
                 tests.push({
@@ -58,8 +70,8 @@ for(let traceType of ['scatter', 'scattergl', 'scattergeo']) {
     }
 }
 
-for(let traceType of ['bar', 'histogram']) {
-    for(let mode of ['group', 'stack', 'overlay']) {
+for(let traceType of ['bar'/*, 'histogram'*/]) {
+    for(let mode of ['group'/*, 'stack', 'overlay'*/]) {
         for(let nTraces of allNTraces) {
             for(let n of allN) {
                 tests.push({
