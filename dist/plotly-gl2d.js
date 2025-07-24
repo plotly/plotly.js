6ac01bf058d716c0d3092a4c6c7e219fe861ddba
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2025-07-24 09:38:51 -0400

    Revert "3.0.3"
    This reverts commit 2bb6513d465e00de47bda96f292c3d2086c750e1.

--- dist/plotly-gl2d.js
+++ dist/plotly-gl2d.js
@@ -1,5 +1,5 @@
 /**
-* plotly.js (gl2d) v3.0.3
+* plotly.js (gl2d) v3.0.2
 * Copyright 2012-2025, Plotly, Inc.
 * All rights reserved.
 * Licensed under the MIT license
@@ -52,7 +52,7 @@ var Plotly = (() => {
   var require_version = __commonJS({
     "src/version.js"(exports) {
       "use strict";
-      exports.version = "3.0.3";
+      exports.version = "3.0.2";
     }
   });
 
