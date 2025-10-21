0d30a842fbda938a2fe955fbe441440297e65955
John Degner <johnbdegner@gmail.com>
2025-10-21 13:56:52 -0700

    Add scatterquiver trace type

---
+++ src/traces/scatterquiver/style.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var d3 = require('@plotly/d3');
+
+var Drawing = require('../../components/drawing');
+var Lib = require('../../lib');
+
+module.exports = function style(gd, calcTrace) {
+    if(!calcTrace || !calcTrace.length || !calcTrace[0]) return;
+    
+    var trace = calcTrace[0].trace;
+    var s = d3.select(gd).selectAll('g.trace' + trace.uid);
+
+    s.selectAll('path.js-line')
+        .call(Drawing.lineGroupStyle, trace.line || {});
+};
