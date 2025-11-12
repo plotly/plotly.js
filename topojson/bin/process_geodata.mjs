6cc62f2002ecc04f2f7dc0e6a149bccc2b86801c
Cameron DeCoster <cameron.decoster@gmail.com>
2025-11-12 11:38:31 -0700

    Update attributes for disputed territories

--- topojson/bin/process_geodata.mjs
+++ topojson/bin/process_geodata.mjs
@@ -377,9 +377,15 @@ const commandsCountries50m = [
     // Subtract Caspian Sea from country polygons
     `-filter 'globalid === "{BBBEF27F-A6F4-4FBC-9729-77B3A8739409}"' target=all_features + name=caspian_sea`,
     '-erase source=caspian_sea target=all_features',
-    // Update country codes for disputed territories at Egypt/Sudan border: https://en.wikipedia.org/wiki/Egypt%E2%80%93Sudan_border
-    `-each 'if (globalid === "{CA12D116-7A19-41D1-9622-17C12CCC720D}") iso3cd = "XHT"'`, // Halaib Triangle
-    `-each 'if (globalid === "{9FD54A50-0BFB-4385-B342-1C3BDEE5ED9B}") iso3cd = "XBT"'`, // Bir Tawil
+    // Update country codes, names for disputed territories
+    // https://en.wikipedia.org/wiki/Ilemi_Triangle
+    `-each 'if (globalid === "{CAB4B11D-5D1D-495E-AC9C-8A18A5A4370B}") { iso3cd = "XIT"; nam_en = "Ilemi Triangle"; }'`,
+    // https://en.wikipedia.org/wiki/Egypt%E2%80%93Sudan_border
+    `-each 'if (globalid === "{CA12D116-7A19-41D1-9622-17C12CCC720D}") { iso3cd = "XHT"; nam_en = "Halaib Triangle"; }'`,
+    `-each 'if (globalid === "{9FD54A50-0BFB-4385-B342-1C3BDEE5ED9B}") { iso3cd = "XBT"; nam_en = "Bir Tawil"; }'`,
+    // https://en.wikipedia.org/wiki/Sino-Indian_border_dispute
+    `-each 'if (globalid === "{9AB8E07B-A251-47AB-9B0C-F969DBE07558}") nam_en = "Aksai Chin"'`,
+    `-each 'if (globalid === "{F180660F-073C-402E-AF75-1E448B4C30F1}") nam_en = "Arunachal Pradesh"'`,
     `-each 'if (iso3cd) iso3cd = iso3cd.toUpperCase()'`,
     `-filter '${filters.countries}'`,
     // Snap polygons to clean up land, coastlines layers
