7ba03e3dc227ecabd0df32398e587997df7694f8
John Degner <johnbdegner@gmail.com>
2025-11-17 08:58:02 -0800

    Add quiver jasmine and image tests

--- src/traces/quiver/event_data.js
+++ src/traces/quiver/event_data.js
@@ -3,8 +3,8 @@
 module.exports = function eventData(out, pt, trace, cd, pointNumber) {
     out.x = pt.x;
     out.y = pt.y;
-    out.u = trace.u[pointNumber];
-    out.v = trace.v[pointNumber];
-    out.pointNumber = pointNumber;
-    out.trace = trace;
+    out.u = trace.u ? trace.u[pointNumber] : undefined;
+    out.v = trace.v ? trace.v[pointNumber] : undefined;
+    
+    return out;
 };
