8035cf98e0d9ef426cea7b8f7f166d9424b97bb6
Cameron DeCoster <cameron.decoster@gmail.com>
2026-09-01 07:10:26 -0600

    Update docstring

--- src/types/core/data.internal.d.ts
+++ src/types/core/data.internal.d.ts
@@ -2,7 +2,7 @@
  * Internal data/trace types (not in public API)
  *
  * These are runtime-resolved versions of trace data with internal state
- * properties. For public trace types, see data.d.ts.
+ * properties. For public trace types, see generated/schema.d.ts.
  */
 
 import type { Data } from '../generated/schema';
