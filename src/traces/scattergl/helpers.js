38dc4659b6ee944520ed071a5e338aa03766f12a
Ya-Fan Chen <20377719+Lexachoc@users.noreply.github.com>
2026-09-24 01:48:17 +0200

    fix numeric marker variant detection issue  in scattergl

--- src/traces/scattergl/helpers.js
+++ src/traces/scattergl/helpers.js
@@ -3,13 +3,11 @@
 var constants = require('./constants');
 
 exports.isOpenSymbol = function(symbol) {
-    return (typeof symbol === 'string') ?
-        constants.OPEN_RE.test(symbol) :
-        symbol % 200 > 100;
+    return (typeof symbol === 'string' && constants.OPEN_RE.test(symbol)) ||
+        symbol % 200 >= 100;
 };
 
 exports.isDotSymbol = function(symbol) {
-    return (typeof symbol === 'string') ?
-        constants.DOT_RE.test(symbol) :
-        symbol > 200;
+    return (typeof symbol === 'string' && constants.DOT_RE.test(symbol)) ||
+        symbol >= 200;
 };
